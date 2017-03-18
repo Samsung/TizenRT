52937b3f905187a81766ebc25f06a8d87c78b78b
Heesub Shin <heesub.shin@samsung.com>
2017-03-18 16:19:17 +0900

    s5j/sflash: add to support progmem
    This commit implements the progmem interfaces. Though S5J does not have
    on-chip flash memory, I cound not find any better design other than
    using the progmem.
    
    Change-Id: Ifc6f38b9bd0564754a2ec055e317868cd50e3bc0
    Signed-off-by: Heesub Shin <heesub.shin@samsung.com>

--- os/arch/arm/src/s5j/s5j_sflash.c
+++ os/arch/arm/src/s5j/s5j_sflash.c
@@ -62,6 +62,7 @@
 #include <tinyara/spi/spi.h>
 #include <arpa/inet.h>
 #include <tinyara/fs/mtd.h>
+#include <tinyara/progmem.h>
 
 #include "up_arch.h"
 #include "cache.h"
@@ -860,3 +861,116 @@ struct mtd_dev_s *getspiflashmtd(void)
 {
 	return (struct mtd_dev_s *)sflashdev.priv;
 }
+
+#ifdef CONFIG_MTD_PROGMEM
+static uint8_t s5j_sflash_read_status(void)
+{
+	return getreg8(rRDSR);
+}
+
+size_t up_progmem_getaddress(size_t page)
+{
+	return S5J_SFLASH_MAPPED_ADDR + up_progmem_pagesize(page) * page;
+}
+
+ssize_t up_progmem_getpage(size_t addr)
+{
+	return (addr - S5J_SFLASH_MAPPED_ADDR) / up_progmem_pagesize(0);
+}
+
+ssize_t up_progmem_erasepage(size_t page)
+{
+	size_t addr;
+	irqstate_t irqs;
+
+	if (page >= up_progmem_npages())
+		return -EFAULT;
+
+	addr = up_progmem_getaddress(page);
+
+	/* Disable IRQs while erasing sector */
+	irqs = irqsave();
+
+	s5j_qspi_disable_wp();
+
+	/* Set sector address and then send erase command */
+	putreg32(addr - S5J_SFLASH_MAPPED_ADDR, rERASE_ADDRESS);
+	putreg8(0xff, rSE);
+
+	/* Wait for the completion */
+	while (s5j_sflash_read_status() & 0x1);
+
+	s5j_qspi_enable_wp();
+
+	/* Invalidate cache */
+	arch_invalidate_dcache(addr, addr + up_progmem_pagesize(page));
+
+	/* Restore IRQs */
+	irqrestore(irqs);
+
+	return up_progmem_pagesize(page);
+}
+
+ssize_t up_progmem_ispageerased(size_t page)
+{
+	size_t addr;
+	size_t count;
+	size_t bwritten;
+
+	if (page >= up_progmem_npages())
+		return -EFAULT;
+
+	bwritten = 0;
+	addr = up_progmem_getaddress(page);
+	for (count = up_progmem_pagesize(page); count; count--) {
+		if (getreg32(addr) != 0xffffff)
+			bwritten++;
+		addr += sizeof(int);
+	}
+
+	return bwritten * sizeof(int);
+}
+
+ssize_t up_progmem_write(size_t addr, const void *buf, size_t count)
+{
+	int page;
+	int pagesize;
+	size_t remain = count;
+
+	page = up_progmem_getpage(addr + count);
+	if (page < 0)
+		return -EINVAL;
+
+	pagesize = up_progmem_pagesize(page);
+
+	while (remain) {
+		int tmp = remain;
+		irqstate_t irqs;
+
+		if (tmp > pagesize)
+			tmp = pagesize;
+
+		/* Disable IRQs */
+		irqs = irqsave();
+
+		s5j_qspi_disable_wp();
+
+		/* Load and write data */
+		memcpy((void *)addr, buf, tmp);
+
+		/* Flush cache */
+		arch_flush_dcache(addr, addr + tmp);
+
+		s5j_qspi_enable_wp();
+
+		/* Restore IRQs */
+		irqrestore(irqs);
+
+		buf    += tmp;
+		addr   += tmp;
+		remain -= tmp;
+	}
+
+	return count;
+}
+#endif /* CONFIG_MTD_PROGMEM */
