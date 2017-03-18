a2e54b19aaa6fc67e5c1a835b95ddac00904ea56
Heesub Shin <heesub.shin@samsung.com>
2017-03-18 19:35:19 +0900

    sidk_s5jt200: configure partitions at boot
    This commit introduces several Kconfig entries for configurting
    partitions and adds a function that configures flash partitions
    dynamically at boot time. However, there will be no functional
    differences yet unless we turn it on. So, don't worry.
    
    Change-Id: I3f10491c6f0c753b2c874310ebfe70596ce0e3c6
    Signed-off-by: Heesub Shin <heesub.shin@samsung.com>

--- os/arch/arm/src/sidk_s5jt200/src/s5jt200_tash.c
+++ os/arch/arm/src/sidk_s5jt200/src/s5jt200_tash.c
@@ -68,6 +68,8 @@
 #include "s5j_rtc.h"
 #include "up_internal.h"
 
+#include <tinyara/fs/mtd.h>
+
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
@@ -112,6 +114,86 @@ static void scsc_wpa_ctrl_iface_init(void)
 #endif
 }
 
+static void sidk_s5jt200_configure_partitions(void)
+{
+#if defined(CONFIG_SIDK_S5JT200_FLASH_PART)
+	int partno;
+	int partoffset;
+	const char *parts = CONFIG_SIDK_S5JT200_FLASH_PART_LIST;
+	const char *types = CONFIG_SIDK_S5JT200_FLASH_PART_TYPE;
+#if defined(CONFIG_MTD_PARTITION_NAMES)
+	const char *names = CONFIG_SIDK_S5JT200_FLASH_PART_NAME;
+#endif
+	FAR struct mtd_dev_s *mtd;
+	FAR struct mtd_geometry_s geo;
+
+	mtd = progmem_initialize();
+	if (!mtd) {
+		lldbg("ERROR: progmem_initialize failed\n");
+		return;
+	}
+
+	if (mtd->ioctl(mtd, MTDIOC_GEOMETRY, (unsigned long)&geo) < 0) {
+		lldbg("ERROR: mtd->ioctl failed\n");
+		return;
+	}
+
+	partno = 0;
+	partoffset = 0;
+
+	while (*parts) {
+		FAR struct mtd_dev_s *mtd_part;
+		int partsize;
+
+		partsize = strtoul(parts, NULL, 0) << 10;
+
+		if (partsize < geo.erasesize) {
+			lldbg("ERROR: Partition size is lesser than erasesize\n");
+			return;
+		}
+
+		if (partsize % geo.erasesize != 0) {
+			lldbg("ERROR: Partition size is not multiple of erasesize\n");
+			return;
+		}
+
+		mtd_part = mtd_partition(mtd, partoffset,
+					partsize / geo.erasesize, 0);
+		partoffset += partsize / geo.erasesize;
+
+		if (!mtd_part) {
+			lldbg("ERROR: failed to create partition.\n");
+			return;
+		}
+
+#if defined(CONFIG_MTD_SMART) && defined(CONFIG_FS_SMARTFS)
+		if (!strncmp(types, "smartfs,", 8)) {
+			char partref[4];
+			sprintf(partref, "p%d", partno);
+			smart_initialize(CONFIG_SIDK_S5JT200_FLASH_MINOR,
+					mtd_part, partref);
+		}
+#endif
+
+#if defined(CONFIG_MTD_PARTITION_NAMES)
+		if (strcmp(names, ""))
+			mtd_setpartitionname(mtd_part, names);
+
+		while (*names != ',' && *names) names++;
+		if (*names == ',') names++;
+#endif
+
+		while (*parts != ',' && *parts) parts++;
+		if (*parts == ',') parts++;
+
+		while (*types != ',' && *types) types++;
+		if (*types == ',') types++;
+
+		partno++;
+	}
+#endif /* CONFIG_SIDK_S5JT200_FLASH_PART */
+}
+
 /****************************************************************************
  * Name: board_app_initialize
  *
@@ -126,6 +208,8 @@ int board_app_initialize(void)
 #if defined(CONFIG_RTC) && defined(CONFIG_RTC_DRIVER) && defined(CONFIG_S5J_RTC)
 	FAR struct tm tp;
 #endif
+
+	sidk_s5jt200_configure_partitions();
 
 #ifdef CONFIG_S5J_I2C
 	s5j_i2c_register(0);
