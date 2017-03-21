2dfff47244cace96e33beff7e203b00d2707092d
Heesub Shin <heesub.shin@samsung.com>
2017-03-21 17:33:18 +0900

    s5j/mct: tidy up a bit
    Fixes trivial coding style issues.
    
    Change-Id: I6893a785335d7f4350647a9abcd2a163246140ec
    Signed-off-by: Heesub Shin <heesub.shin@samsung.com>

--- os/arch/arm/src/s5j/s5j_mct.h
+++ os/arch/arm/src/s5j/s5j_mct.h
@@ -49,8 +49,8 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  ****************************************************************************/
-#ifndef __S5J_MCT_H__
-#define __S5J_MCT_H__
+#ifndef __ARCH_ARM_SRC_S5J_S5J_MCT_H__
+#define __ARCH_ARM_SRC_S5J_S5J_MCT_H__
 
 /****************************************************************************
  * Included Files
@@ -64,73 +64,73 @@
  * Private Types
  ****************************************************************************/
 #ifdef __cplusplus
-static "C" {
+extern "C" {
 #endif
 
-	typedef enum {
-		MCT_G0 = 0x200,
-		MCT_G1 = 0x210,
-		MCT_G2 = 0x220,
-		MCT_G3 = 0x230,
-		MCT_L0 = 0x300,
-		MCT_L1 = 0x400,
-		MCT_L2 = 0x500,
-		MCT_L3 = 0x600,
-	} MCT_CHANNEL;
+typedef enum {
+	MCT_G0 = 0x200,
+	MCT_G1 = 0x210,
+	MCT_G2 = 0x220,
+	MCT_G3 = 0x230,
+	MCT_L0 = 0x300,
+	MCT_L1 = 0x400,
+	MCT_L2 = 0x500,
+	MCT_L3 = 0x600,
+} MCT_CHANNEL;
 
-	typedef enum {
-		LINT_NONE = 0,
-		LINT_INT = 1,
-		LINT_FRC = 2,
-		LINT_FRC_INT = 3,
-	} MCT_LINT;
+typedef enum {
+	LINT_NONE = 0,
+	LINT_INT = 1,
+	LINT_FRC = 2,
+	LINT_FRC_INT = 3,
+} MCT_LINT;
 
-	typedef enum {
-		eDIVIDER_1 = 0,
-		eDIVIDER_2,
-		eDIVIDER_4,
-		eDIVIDER_8,
-		eDIVIDER_16,
-		eDIVIDER_MAX,
-	} eMCT_DIVIDER;
+typedef enum {
+	eDIVIDER_1 = 0,
+	eDIVIDER_2,
+	eDIVIDER_4,
+	eDIVIDER_8,
+	eDIVIDER_16,
+	eDIVIDER_MAX,
+} eMCT_DIVIDER;
 
-	/****************************************************************************
-	 * Public Functions
-	 ****************************************************************************/
-	void s5j_mct_local_int_ctrl(MCT_CHANNEL mct_id, MCT_LINT flag);
-	u64 s5j_mct_global_get_comp_cnt(MCT_CHANNEL mct_id);
-	u32 s5j_mct_global_get_auto_incr(MCT_CHANNEL mct_id);
-	void s5j_mct_global_set_auto_incr(MCT_CHANNEL mct_id, u32 count);
-	void s5j_mct_local_set_int_cnt_auto(MCT_CHANNEL mct_id, u32 count);
-	void s5j_mct_local_set_int_cnt_manual(MCT_CHANNEL mct_id, u32 count);
-	void s5j_mct_local_set_tick_cnt(MCT_CHANNEL mct_id, u32 count);
-	u32 s5j_mct_local_get_int_cnt(MCT_CHANNEL mct_id);
-	u32 s5j_mct_local_get_tick_cnt(MCT_CHANNEL mct_id);
-	void s5j_mct_local_set_frc_cnt(MCT_CHANNEL mct_id, u32 count);
-	u32 s5j_mct_local_get_frc_cnt(MCT_CHANNEL mct_id);
-	bool s5j_mct_ispending(MCT_CHANNEL mct_id);
-	void s5j_mct_global_start_frc(void);
-	void s5j_mct_global_stop_frc(void);
-	void s5j_mct_global_set_frc_cnt(cycle_t count);
-	void s5j_mct_clk_rate(u32 prescaler, eMCT_DIVIDER divider);
-	void s5j_mct_local_set_interval(MCT_CHANNEL mct_id);
-	void s5j_mct_local_clear_interval(MCT_CHANNEL mct_id);
-	void s5j_mct_local_start_int(MCT_CHANNEL mct_id);
-	void s5j_mct_local_stop_int(MCT_CHANNEL mct_id);
-	void s5j_mct_local_start_timer(MCT_CHANNEL mct_id);
-	void s5j_mct_local_stop_timer(MCT_CHANNEL mct_id);
-	void s5j_mct_local_start_frc(MCT_CHANNEL mct_id);
-	void s5j_mct_local_stop_frc(MCT_CHANNEL mct_id);
-	void s5j_mct_global_enable_int(MCT_CHANNEL mct_id);
-	void s5j_mct_global_disable_int(MCT_CHANNEL mct_id);
-	void s5j_mct_global_set_comp_cnt(MCT_CHANNEL mct_id, u64 count);
-	cycle_t s5j_mct_global_get_frc_cnt(void);
-	void s5j_mct_global_disable_comp(MCT_CHANNEL mct_id);
-	void s5j_mct_global_enable_comp(MCT_CHANNEL mct_id);
-	void s5j_mct_reset(MCT_CHANNEL mct_id);
-	void s5j_mct_clear_pending(MCT_CHANNEL mct_id);
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+void s5j_mct_local_int_ctrl(MCT_CHANNEL mct_id, MCT_LINT flag);
+u64 s5j_mct_global_get_comp_cnt(MCT_CHANNEL mct_id);
+u32 s5j_mct_global_get_auto_incr(MCT_CHANNEL mct_id);
+void s5j_mct_global_set_auto_incr(MCT_CHANNEL mct_id, u32 count);
+void s5j_mct_local_set_int_cnt_auto(MCT_CHANNEL mct_id, u32 count);
+void s5j_mct_local_set_int_cnt_manual(MCT_CHANNEL mct_id, u32 count);
+void s5j_mct_local_set_tick_cnt(MCT_CHANNEL mct_id, u32 count);
+u32 s5j_mct_local_get_int_cnt(MCT_CHANNEL mct_id);
+u32 s5j_mct_local_get_tick_cnt(MCT_CHANNEL mct_id);
+void s5j_mct_local_set_frc_cnt(MCT_CHANNEL mct_id, u32 count);
+u32 s5j_mct_local_get_frc_cnt(MCT_CHANNEL mct_id);
+bool s5j_mct_ispending(MCT_CHANNEL mct_id);
+void s5j_mct_global_start_frc(void);
+void s5j_mct_global_stop_frc(void);
+void s5j_mct_global_set_frc_cnt(cycle_t count);
+void s5j_mct_clk_rate(u32 prescaler, eMCT_DIVIDER divider);
+void s5j_mct_local_set_interval(MCT_CHANNEL mct_id);
+void s5j_mct_local_clear_interval(MCT_CHANNEL mct_id);
+void s5j_mct_local_start_int(MCT_CHANNEL mct_id);
+void s5j_mct_local_stop_int(MCT_CHANNEL mct_id);
+void s5j_mct_local_start_timer(MCT_CHANNEL mct_id);
+void s5j_mct_local_stop_timer(MCT_CHANNEL mct_id);
+void s5j_mct_local_start_frc(MCT_CHANNEL mct_id);
+void s5j_mct_local_stop_frc(MCT_CHANNEL mct_id);
+void s5j_mct_global_enable_int(MCT_CHANNEL mct_id);
+void s5j_mct_global_disable_int(MCT_CHANNEL mct_id);
+void s5j_mct_global_set_comp_cnt(MCT_CHANNEL mct_id, u64 count);
+cycle_t s5j_mct_global_get_frc_cnt(void);
+void s5j_mct_global_disable_comp(MCT_CHANNEL mct_id);
+void s5j_mct_global_enable_comp(MCT_CHANNEL mct_id);
+void s5j_mct_reset(MCT_CHANNEL mct_id);
+void s5j_mct_clear_pending(MCT_CHANNEL mct_id);
 
 #ifdef __cplusplus
 }
 #endif
-#endif							//__MCT_H__
+#endif /* __ARCH_ARM_SRC_S5J_S5J_MCT_H__ */
