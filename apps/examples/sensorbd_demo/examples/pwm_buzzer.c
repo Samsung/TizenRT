c47016ec614665ca10be5ff2a4cf3151b6206a7d
Bongryul Lee <bongryul.lee@samsung.com>
2017-04-29 01:01:04 +0900

    examples/sensorbd_demo: play a simpler scale instead of a melody
    sensorbd_demo contains an example to show the PWM features S5J provides.
    It generates pulse of various frequency to make a buzzer sound or
    adjusts the brightness of light emitted by LEDs attached to it by
    changing the duty cycle.
    
    This commit changes frequencies and duty cycles for buzzer sound in the
    sense that it would be better to play a simpler scale rather than to
    play some melody.
    
    Change-Id: Ie566ade9d52101a0feca700dc9a11f02fde5590c
    Signed-off-by: Bongryul Lee <bongryul.lee@samsung.com>

--- apps/examples/sensorbd_demo/examples/pwm_buzzer.c
+++ apps/examples/sensorbd_demo/examples/pwm_buzzer.c
@@ -53,65 +53,60 @@
 #include <fcntl.h>
 #include <tinyara/pwm.h>
 
-#define c3   3822				/* PWM: 130.82 Hz, note freq: 130.81 Hz, error 0.01% */
-#define e3   3034				/* PWM: 164.80 Hz, note freq: 164.81 Hz, error 0.01% */
-#define g3x  2408				/* PWM: 207.64 Hz, note freq: 207.65 Hz, error 0.01% */
-#define a3   2273				/* PWM: 219.97 Hz, note freq: 220.00 Hz, error 0.01% */
-#define b3   2025				/* PWM: 246.91 Hz, note freq: 246.94 Hz, error 0.01% */
-#define c4   1911				/* PWM: 261.64 Hz, note freq: 261.63 Hz, error 0.01% */
-#define d4   1703				/* PWM: 293.60 Hz, note freq: 293.66 Hz, error 0.02% */
-#define d4x  1607				/* PWM: 311.14 Hz, note freq: 311.13 Hz, error 0.00% */
-#define e4   1517				/* PWM: 329.60 Hz, note freq: 329.63 Hz, error 0.01% */
-#define f4   1432				/* PWM: 349.16 Hz, note freq: 349.23 Hz, error 0.02% */
-#define g4   1276				/* PWM: 391.85 Hz, note freq: 392.00 Hz, error 0.04% */
-#define a4   1136				/* PWM: 440.14 Hz, note freq: 440.00 Hz, error 0.03% */
-#define h4   1012				/* PWM: 494.07 Hz, note freq: 493.88 Hz, error 0.04% */
-#define c5   956				/* PWM: 523.01 Hz, note freq: 523.25 Hz, error 0.05% */
-#define p    1
+#define C4   262
+#define D4   294
+#define E4   330
+#define F4   349
+#define G4   392
+#define A4   440
+#define B4   494
+#define C5   523
+#define D5   587
+#define E5   659
+#define F5   698
+#define G5   784
+#define A5   880
+#define B5   988
+#define C6   1047
 
-int octavef[] = { c4, d4, e4, f4, g4, a4, h4, c5 };
-int octaved[] = { 8, 8, 8, 8, 8, 8, 8, 8 };
-
-int furelisef[] = {
-	e4, d4x, e4, d4x, e4, b3, d4, c4, a3, p, c3, e3, a3, b3, p, e3, g3x, b3, c4, p, e3,
-	e3, d4x, e4, d4x, e4, b3, d4, c4, a3, p, c3, e3, a3, b3, p, e3, c4, b3, a3
-};
-
-int furelised[] = {
-	8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-	8, 8, 8, 4, 8, 8, 8, 8, 4, 8, 8,
-	8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
-	8, 8, 4, 8, 8, 8, 8, 4
-};
+int octavef1[8] = { C4, D4, E4, F4, G4, A4, B4, C5 };
+int octavef2[8] = { C5, D5, E5, F5, G5, A5, B5, C6 };
+int octaved[8] = { 8, 8, 8, 8, 8, 8, 8, 8 };
 
 void pwmbuzzer_main(int argc, char *argv[])
 {
-	int i, fd1, fd2;
+	int i, fd;
 	struct pwm_info_s pwm_info;
 
-	fd1 = open("/dev/pwm1", O_RDWR);
-	fd2 = open("/dev/pwm2", O_RDWR);
+	/* device 0 channel 1 */
+	fd = open("/dev/pwm1", O_RDWR);
 
 	for (i = 0; i < 8; i++) {
-		pwm_info.frequency = octavef[i];
+		pwm_info.frequency = octavef1[i];
 		pwm_info.duty = (octaved[i] * 65536) / 100;
-		ioctl(fd1, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&pwm_info));
-		ioctl(fd1, PWMIOC_START);
+		ioctl(fd, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&pwm_info));
+		ioctl(fd, PWMIOC_START);
 
 		up_mdelay(400);
 	}
 
-	for (i = 0; i < 40; i++) {
-		pwm_info.frequency = furelisef[i];
-		pwm_info.duty = (furelised[i] * 65536) / 100;
-		ioctl(fd2, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&pwm_info));
-		ioctl(fd2, PWMIOC_START);
+	ioctl(fd, PWMIOC_STOP);
+	close(fd);
+
+	up_mdelay(1000);
+
+	/* device 0 channel 2 */
+	fd = open("/dev/pwm2", O_RDWR);
+
+	for (i = 0; i < 8; i++) {
+		pwm_info.frequency = octavef2[i];
+		pwm_info.duty = (octaved[i] * 65536) / 100;
+		ioctl(fd, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&pwm_info));
+		ioctl(fd, PWMIOC_START);
 
 		up_mdelay(400);
 	}
 
-	ioctl(fd1, PWMIOC_STOP);
-	close(fd1);
-	ioctl(fd2, PWMIOC_STOP);
-	close(fd2);
+	ioctl(fd, PWMIOC_STOP);
+	close(fd);
 }
