61ea7d8ebdcb1dc4e0a1319ea440284f5483108c
ahreum.jeong <ahreum.jeong@samsung.com>
2017-05-02 13:33:40 +0900

    Update years in copyright

--- os/logm/logm.c
+++ os/logm/logm.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * Copyright 2016 Samsung Electronics All Rights Reserved.
+ * Copyright 2016-2017 Samsung Electronics All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
