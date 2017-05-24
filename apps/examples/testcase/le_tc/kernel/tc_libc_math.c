ba024e12ffd14db541764c00a74de986a7abf1d5
Lokesh B V <lokesh.bv@partner.samsung.com>
2017-05-24 16:47:16 +0530

    libc/math: Add testcases for missing math APIs
    This patch adds testcases for math APIs namely 'cbrt', 'fdim', 'famx', 'fmin' and 'hypot'.
    Change-Id: I4741578f9549b0b071fe4237de55072076e0a50f
    Signed-off-by: jc_.kim <jc_.kim@samsung.com>

--- apps/examples/testcase/le_tc/kernel/tc_libc_math.c
+++ apps/examples/testcase/le_tc/kernel/tc_libc_math.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * Copyright 2016 Samsung Electronics All Rights Reserved.
+ * Copyright 2016-2017 Samsung Electronics All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@
 #include <tinyara/compiler.h>
 #include <stdio.h>
 #include <tinyara/math.h>
+#include <tinyara/float.h>
 #include "tc_internal.h"
 
 #ifdef CONFIG_HAVE_DOUBLE
@@ -37,14 +38,40 @@
 #define VAL2 4503599627370496.5
 
 /**
-* @fn                   :tc_libc_math_fabs
-* @brief                :Returns the absolute value of parameter
-* @Scenario             :Returns the absolute value of parameter
-* API's covered         :fabs
-* Preconditions         :None
-* Postconditions        :None
-* @return               :void
-*/
+ * @fn                   :tc_libc_math_cbrt
+ * @brief                :Returns the cube root of parameter
+ * @Scenario             :Returns the cube root of parameter
+ * API's covered         :cbrt
+ * Preconditions         :None
+ * Postconditions        :None
+ * @return               :void
+ */
+static void tc_libc_math_cbrt(void)
+{
+	const double in_val[6] = { ZERO, VAL1, -VAL1, INFINITY, -INFINITY, NAN };
+	const double sol_val[6] = { ZERO, 131072.0, -131072.0, INFINITY, -INFINITY, NAN };
+	double ret_val[6];
+	int cbrt_idx;
+
+	for (cbrt_idx = 0; cbrt_idx < 6; cbrt_idx++) {
+		ret_val[cbrt_idx] = cbrt(in_val[cbrt_idx]);
+		if (!(isnan(sol_val[cbrt_idx]) && isnan(ret_val[cbrt_idx]))) {
+			TC_ASSERT_LEQ("cbrt", fabs(sol_val[cbrt_idx] - ret_val[cbrt_idx]), FLT_EPSILON);
+		}
+	}
+
+	TC_SUCCESS_RESULT();
+}
+
+/**
+ * @fn                   :tc_libc_math_fabs
+ * @brief                :Returns the absolute value of parameter
+ * @Scenario             :Returns the absolute value of parameter
+ * API's covered         :fabs
+ * Preconditions         :None
+ * Postconditions        :None
+ * @return               :void
+ */
 static void tc_libc_math_fabs(void)
 {
 	const double in_val[5] = { ZERO, VAL1, -VAL1, INFINITY, -INFINITY };
@@ -61,14 +88,41 @@ static void tc_libc_math_fabs(void)
 }
 
 /**
-* @fn                   :tc_libc_math_floor
-* @brief                :Returns the floor value of parameter
-* @Scenario             :Returns the floor value of parameter
-* API's covered         :floor
-* Preconditions         :None
-* Postconditions        :None
-* @return               :void
-*/
+ * @fn                   :tc_libc_math_fdim
+ * @brief                :Returns the positive  difference max(x-y,0),  between thier arguments
+ * @Scenario             :Returns the positive  difference max(x-y,0),  between their arguments
+ * API's covered         :fdim
+ * Preconditions         :None
+ * Postconditions        :None
+ * @return               :void
+ */
+static void tc_libc_math_fdim(void)
+{
+	const double in_val1[9] = { ZERO, -ZERO, VAL1, -VAL1, VAL2, -VAL2, INFINITY, -INFINITY, NAN };
+	const double in_val2[9] = { -INFINITY, INFINITY, -VAL2, VAL2, -VAL1, VAL1, -ZERO, ZERO, NAN };
+	const double sol_val[9] = { INFINITY, ZERO, 6755399441055744.0, ZERO, 6755399441055744.0, ZERO, INFINITY, ZERO, NAN };
+	double ret_val[9];
+	int fdim_idx;
+
+	for (fdim_idx = 0; fdim_idx < 9; fdim_idx++) {
+		ret_val[fdim_idx] = fdim(in_val1[fdim_idx], in_val2[fdim_idx]);
+		if (!(isnan(sol_val[fdim_idx]) && isnan(ret_val[fdim_idx]))) {
+			TC_ASSERT_LEQ("fdim", fabs(sol_val[fdim_idx] - ret_val[fdim_idx]), FLT_EPSILON);
+		}
+	}
+
+	TC_SUCCESS_RESULT();
+}
+
+/**
+ * @fn                   :tc_libc_math_floor
+ * @brief                :Returns the floor value of parameter
+ * @Scenario             :Returns the floor value of parameter
+ * API's covered         :floor
+ * Preconditions         :None
+ * Postconditions        :None
+ * @return               :void
+ */
 static void tc_libc_math_floor(void)
 {
 	const double in_val[8] = { ZERO, -ZERO, VAL1, -VAL1, VAL2, -VAL2, INFINITY, -INFINITY };
@@ -85,14 +139,95 @@ static void tc_libc_math_floor(void)
 }
 
 /**
-* @fn                   :tc_libc_math_pow
-* @brief                :Returns the power value of parameter
-* @Scenario             :Returns the power value of parameter
-* API's covered         :pow
-* Preconditions         :None
-* Postconditions        :None
-* @return               :void
-*/
+ * @fn                   :tc_libc_math_fmax
+ * @brief                :Returns the maximum value of argument1 and argument2
+ * @Scenario             :Returns the maximum value of argument1 and argument2
+ * API's covered         :fmax
+ * Preconditions         :None
+ * Postconditions        :None
+ * @return               :void
+ */
+static void tc_libc_math_fmax(void)
+{
+	const double in_val1[7] = { ZERO, VAL1, -VAL1, INFINITY, -INFINITY, NAN, VAL1 };
+	const double in_val2[7] = { -INFINITY, INFINITY, -VAL1, VAL1, ZERO, NAN, NAN };
+	const double sol_val[7] = { ZERO, INFINITY, -VAL1, INFINITY, ZERO, NAN, VAL1 };
+	double ret_val[7];
+	int fmax_idx;
+
+	for (fmax_idx = 0; fmax_idx < 7; fmax_idx++) {
+		ret_val[fmax_idx] = fmax(in_val1[fmax_idx], in_val2[fmax_idx]);
+		if (!(isnan(sol_val[fmax_idx]) && isnan(ret_val[fmax_idx]))) {
+			TC_ASSERT_LEQ("fmax", fabs(sol_val[fmax_idx] - ret_val[fmax_idx]), FLT_EPSILON);
+		}
+	}
+
+	TC_SUCCESS_RESULT();
+}
+
+/**
+ * @fn                   :tc_libc_math_fmin
+ * @brief                :Returns the minimum value of argument1 and argument2
+ * @Scenario             :Returns the minimum value of argument1 and argument2
+ * API's covered         :fmin
+ * Preconditions         :None
+ * Postconditions        :None
+ * @return               :void
+ */
+static void tc_libc_math_fmin(void)
+{
+	const double in_val1[7] = { ZERO, VAL1, -VAL1, INFINITY, -INFINITY, NAN, VAL1 };
+	const double in_val2[7] = { -INFINITY, INFINITY, -VAL1, VAL1, ZERO, NAN, NAN };
+	const double sol_val[7] = { -INFINITY, VAL1, -VAL1, VAL1, -INFINITY, NAN, VAL1 };
+	double ret_val[7];
+	int fmin_idx;
+
+	for (fmin_idx = 0; fmin_idx < 7; fmin_idx++) {
+		ret_val[fmin_idx] = fmin(in_val1[fmin_idx], in_val2[fmin_idx]);
+		if (!(isnan(sol_val[fmin_idx]) && isnan(ret_val[fmin_idx]))) {
+			TC_ASSERT_LEQ("fmin", fabs(sol_val[fmin_idx] - ret_val[fmin_idx]), FLT_EPSILON);
+		}
+	}
+
+	TC_SUCCESS_RESULT();
+}
+
+/**
+ * @fn                   :tc_libc_math_hypot
+ * @brief                :Returns the length of a right-angled  triangle with sides of length parameter1 and parameter2
+ * @Scenario             :Returns the length of a right-angled  triangle with sides of length parameter1 and parameter2
+ * API's covered         :hypot
+ * Preconditions         :None
+ * Postconditions        :None
+ * @return               :void
+ */
+static void tc_libc_math_hypot(void)
+{
+	const double in_val1[10] = { ZERO, -ZERO, VAL1, -VAL1, VAL2, -VAL2, INFINITY, -INFINITY, NAN, VAL1 };
+	const double in_val2[10] = { -INFINITY, INFINITY, -VAL2, VAL2, -VAL1, VAL1, -ZERO, ZERO, INFINITY, NAN };
+	const double sol_val[10] = { INFINITY, INFINITY, 5035177455121576.0, 5035177455121576.0, 5035177455121576.0, 5035177455121576.0, INFINITY, INFINITY, INFINITY, NAN };
+	double ret_val[10];
+	int hypot_idx;
+
+	for (hypot_idx = 0; hypot_idx < 10; hypot_idx++) {
+		ret_val[hypot_idx] = hypot(in_val1[hypot_idx], in_val2[hypot_idx]);
+		if (!(isnan(sol_val[hypot_idx]) && isnan(ret_val[hypot_idx]))) {
+			TC_ASSERT_LEQ("hypot", fabs(sol_val[hypot_idx] - ret_val[hypot_idx]), FLT_EPSILON);
+		}
+	}
+
+	TC_SUCCESS_RESULT();
+}
+
+/**
+ * @fn                   :tc_libc_math_pow
+ * @brief                :Returns the power value of parameter
+ * @Scenario             :Returns the power value of parameter
+ * API's covered         :pow
+ * Preconditions         :None
+ * Postconditions        :None
+ * @return               :void
+ */
 
 static void tc_libc_math_pow(void)
 {
@@ -120,8 +255,13 @@ static void tc_libc_math_pow(void)
 int libc_math_main(void)
 {
 #ifdef CONFIG_HAVE_DOUBLE
+	tc_libc_math_cbrt();
 	tc_libc_math_fabs();
+	tc_libc_math_fdim();
 	tc_libc_math_floor();
+	tc_libc_math_fmax();
+	tc_libc_math_fmin();
+	tc_libc_math_hypot();
 	tc_libc_math_pow();
 #else
 	printf("tc_libc_math will not be executed. CONFIG_HAVE_DOUBLE is not set.\n");
