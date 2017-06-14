9427a3687064a861babcffada1fd1d2a332e42d2
Shinkyu Park <shinkyu.park@samsung.com>
2017-06-14 10:45:26 +0900

    external/aws: chage user specific data
    This commit changes
        - Certification key file
        - Host name

--- external/aws/samples/TizenRT/subscribe_publish_sample/aws_iot_config.h
+++ external/aws/samples/TizenRT/subscribe_publish_sample/aws_iot_config.h
@@ -23,10 +23,10 @@
 
 // Get from console
 // =================================================
-#define AWS_IOT_MQTT_HOST              "a2w1jnvr7mzhr1.iot.ap-northeast-2.amazonaws.com" ///< Customer specific MQTT HOST. The same will be used for Thing Shadow
+#define AWS_IOT_MQTT_HOST              "" ///< Customer specific MQTT HOST. The same will be used for Thing Shadow
 #define AWS_IOT_MQTT_PORT              8883 ///< default port for MQTT/S
 #define AWS_IOT_MQTT_CLIENT_ID         "c-sdk-client-id" ///< MQTT client ID should be unique for every device
-#define AWS_IOT_MY_THING_NAME 		   "samsung_aws"
+#define AWS_IOT_MY_THING_NAME 		   "AWS-IoT-C-SDK" ///< Thing Name of the Shadow this device is associated with
 #define AWS_IOT_ROOT_CA_FILENAME       "rootCA.crt" ///< Root CA file name
 #define AWS_IOT_CERTIFICATE_FILENAME   "cert.pem" ///< device signed certificate file name
 #define AWS_IOT_PRIVATE_KEY_FILENAME   "privkey.pem" ///< Device private key filename
