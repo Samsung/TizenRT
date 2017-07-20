2e221f5ac219e688f7e73da1618f69e15d18fa6e
root <kyungse1171@naver.com>
2017-07-20 17:28:59 +0900

    Add appconfig command for making app config metafile

---
+++ apps/tools/create_appspec.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+###########################################################################
+#
+# Copyright 2017 Samsung Electronics All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
+###########################################################################
+
+KCONFIG="Kconfig"
+METAFILE=".appSpec"
+MAKEFILE="Makefile"
+OSDIR="../os/"
+APPDIR="../apps/"
+KCONFIG_ENTRY="Kconfig_ENTRY"
+DOTCONFIG=`find ../os/ -name '.config'`
+EXAMPLEDIR=`echo "$*" | sed -n 's/.\/tools\/create_appspec.sh //pg'`
+
+	USER_ENTRYPOINT=`sed -n '/CONFIG_USER_ENTRYPOINT/p' $DOTCONFIG | sed -n 's/.*=//p'`
+	echo -e "USER_ENTRYPOINT=$USER_ENTRYPOINT\n" >> $OSDIR$METAFILE
+
+# get app's info from Kconfig & Kconfig_ENTRY;
+# find CONFIG_NAME from Kconfig_ENTRY and search SELECTABLE(find select & depends on);
+
+for FILE in $EXAMPLEDIR
+do
+	APPNAME=`sed -n '/^APPNAME/p' $APPDIR$FILE$MAKEFILE | sed -n 's/APPNAME = //p'`
+	FUNCTION=`sed -n '/^FUNCNAME/p' $APPDIR$FILE$MAKEFILE | sed -n 's/FUNCNAME = //p'`
+	CONFIG_NAME=`sed -n '/depends/p' $APPDIR$FILE$KCONFIG_ENTRY | sed -n 's/.*depends on //p'`
+	CHECK=`sed -n "/# CONFIG_$CONFIG_NAME is not set/p" $DOTCONFIG`
+	if [ "$CHECK" = "" ]
+		then CONFIG_USE="y"
+	else
+		CONFIG_USE="n"
+	fi
+	DISCRIPTION=`sed -n '/bool/p' $APPDIR$FILE$KCONFIG_ENTRY | sed -n 's/.*bool //p'`
+	KCONFIG_INFO=`sed -n "/config $CONFIG_NAME$/,/^$/p" $APPDIR$FILE$KCONFIG`
+	DEPENDENCY=`echo "$KCONFIG_INFO" | sed -n '/depends on/p'`
+	SELECTFLAG=`echo "$KCONFIG_INFO" | sed -n '/select/p'`
+	if [ "$DEPENDENCY" != "" ] || [ "$SELECTFLAG" != "" ]
+		then SELECTABLE="n"
+	else
+		SELECTABLE="y"
+	fi
+	if [ "$APPNAME" != "" ]
+		then echo "{ $APPNAME, $FUNCTION, CONFIG_$CONFIG_NAME, $CONFIG_USE, $SELECTABLE, $DISCRIPTION }" >> $OSDIR$METAFILE
+	fi
+done
