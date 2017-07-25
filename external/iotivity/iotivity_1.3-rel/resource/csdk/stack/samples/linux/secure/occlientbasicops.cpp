4ba179e5a941eb3eb7c811f5f37826f939c8b006
Oleksandr Dmytrenko <o.dmytrenko@samsung.com>
2017-07-25 14:09:52 +0300

    iotivity: Stack sample client fix
    This patch comes from iotivity upstream 1.3-rel branch.
    The details are as follows.
    
    Stack sample client support old servers
    
    Change-Id: Id9fffc3b9e73c68820b99e5fd197d742d0b19418
    Signed-off-by: Oleksandr Dmytrenko <o.dmytrenko@samsung.com>
    Signed-off-by: Wonsang Ryou <wonsang.yoo@samsung.com>

--- external/iotivity/iotivity_1.3-rel/resource/csdk/stack/samples/linux/secure/occlientbasicops.cpp
+++ external/iotivity/iotivity_1.3-rel/resource/csdk/stack/samples/linux/secure/occlientbasicops.cpp
@@ -512,6 +512,28 @@ int parseClientResponse(OCClientResponse *clientResponse)
             }
         }
 
+        //old servers support
+        if (0 == coapSecureResource && res->secure)
+        {
+#ifdef __WITH_TLS__
+            if (WithTcp)
+            {
+                endpoint->flags = (OCTransportFlags)(endpoint->flags | OC_SECURE);
+                endpoint->adapter = OC_ADAPTER_TCP;
+                endpoint->port = res->tcpPort;
+                OIC_LOG_V(INFO, TAG, "TLS port: %d", endpoint->port);
+            }
+            else
+#endif
+            {
+                endpoint->port = res->port;
+                endpoint->flags = (OCTransportFlags)(endpoint->flags | OC_SECURE);
+                endpoint->adapter = OC_ADAPTER_IP;
+                OIC_LOG_V(INFO, TAG, "DTLS port: %d", endpoint->port);
+            }
+            coapSecureResource = 1;
+        }
+
         OIC_LOG_V(INFO, TAG, "Secure -- %s", coapSecureResource == 1 ? "YES" : "NO");
 
         // If we discovered a secure resource, exit from here
@@ -525,4 +547,3 @@ int parseClientResponse(OCClientResponse *clientResponse)
 
     return 0;
 }
-
