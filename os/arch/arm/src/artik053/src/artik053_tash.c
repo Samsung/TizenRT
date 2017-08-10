cb736d02821ac01e52843e4c763c507770d2dd48
Junhwan Park <junhwan.park@samsung.com>
2017-08-10 15:39:08 +0900

    artik053: add new line parameter in lldbg
    Missing new line parameters were in some lldbg. This can degrade the
    readability of the error log when abnormal.
    
    Change-Id: I1794caf247102cf9bcd720260d680e34e6270950
    Signed-off-by: Junhwan Park <junhwan.park@samsung.com>
    
    Conflicts:
    	os/arch/arm/src/artik053/src/artik053_tash.c

--- os/arch/arm/src/artik053/src/artik053_tash.c
+++ os/arch/arm/src/artik053/src/artik053_tash.c
@@ -286,7 +286,7 @@ int board_app_initialize(void)
 	/* Initialize and mount user partition (if we have) */
 	ret = mksmartfs(CONFIG_ARTIK053_AUTOMOUNT_USERFS_DEVNAME, false);
 	if (ret != OK) {
-		lldbg("ERROR: mksmartfs on %s failed", CONFIG_ARTIK053_AUTOMOUNT_USERFS_DEVNAME);
+		lldbg("ERROR: mksmartfs on %s failed\n", CONFIG_ARTIK053_AUTOMOUNT_USERFS_DEVNAME);
 	} else {
 		ret = mount(CONFIG_ARTIK053_AUTOMOUNT_USERFS_DEVNAME, CONFIG_ARTIK053_AUTOMOUNT_USERFS_MOUNTPOINT, "smartfs", 0, NULL);
 		if (ret != OK) {
