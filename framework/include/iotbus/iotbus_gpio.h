96cfd87f14c73a5d351c3ba0545a96d14326b9d9
sunghan <sh924.chang@samsung.com>
2017-08-14 17:57:15 +0900

    modify indentation for readability and beauty on iotbus
    Change-Id: Id65d58658d7d4c894596508449049e6c4906b14c
    Signed-off-by: sunghan <sh924.chang@samsung.com>

--- framework/include/iotbus/iotbus_gpio.h
+++ framework/include/iotbus/iotbus_gpio.h
@@ -42,10 +42,11 @@
  * IOTBUS_GPIO_DRIVE_PUSHPULL = 4, Push Pull\n
  */
 typedef enum {
-	IOTBUS_GPIO_DRIVE_NONE = 0, /* No set any state */
-	IOTBUS_GPIO_DRIVE_PULLUP = 1, /* Resistive High */
+	IOTBUS_GPIO_DRIVE_NONE     = 0, /* No set any state */
+	IOTBUS_GPIO_DRIVE_PULLUP   = 1, /* Resistive High */
 	IOTBUS_GPIO_DRIVE_PULLDOWN = 2, /* Resistive Low */
-	IOTBUS_GPIO_DRIVE_FLOAT = 3, IOTBUS_GPIO_DRIVE_PUSHPULL = 4,
+	IOTBUS_GPIO_DRIVE_FLOAT    = 3,
+	IOTBUS_GPIO_DRIVE_PUSHPULL = 4,
 } iotbus_gpio_drive_e;
 
 /**
@@ -57,8 +58,9 @@ typedef enum {
  * IOTBUS_GPIO_DIRECTION_IN = 2, Input Direction\n
  */
 typedef enum {
-	IOTBUS_GPIO_DIRECTION_NONE = 0, IOTBUS_GPIO_DIRECTION_OUT = 1, /* Output. A Mode can also be set */
-	IOTBUS_GPIO_DIRECTION_IN = 2, /* Input */
+	IOTBUS_GPIO_DIRECTION_NONE = 0,
+	IOTBUS_GPIO_DIRECTION_OUT  = 1,  /* Output. A Mode can also be set */
+	IOTBUS_GPIO_DIRECTION_IN   = 2,  /* Input */
 } iotbus_gpio_direction_e;
 
 /**
@@ -71,10 +73,10 @@ typedef enum {
  * IOTBUS_GPIO_EDGE_FALLING = 3, Interrupt on falling edge only\n
  */
 typedef enum {
-	IOTBUS_GPIO_EDGE_NONE = 0, /* No interrupt on Gpio */
-	IOTBUS_GPIO_EDGE_BOTH = 1, /* Interrupt on rising & falling */
-	IOTBUS_GPIO_EDGE_RISING = 2, /* Interrupt on rising only */
-	IOTBUS_GPIO_EDGE_FALLING = 3 /* Interrupt on falling only */
+	IOTBUS_GPIO_EDGE_NONE    = 0, /* No interrupt on Gpio */
+	IOTBUS_GPIO_EDGE_BOTH    = 1, /* Interrupt on rising & falling */
+	IOTBUS_GPIO_EDGE_RISING  = 2, /* Interrupt on rising only */
+	IOTBUS_GPIO_EDGE_FALLING = 3  /* Interrupt on falling only */
 } iotbus_gpio_edge_e;
 
 struct _iotbus_gpio_s;
