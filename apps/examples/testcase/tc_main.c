02f5aafe3be6a1267e73572d3a066256a9d7316c
kang <pilseob.kang@samsung.com>
2017-09-08 13:59:51 +0900

    unit testcases for mqtt

--- apps/examples/testcase/tc_main.c
+++ apps/examples/testcase/tc_main.c
@@ -50,6 +50,9 @@
 #if defined(CONFIG_EXAMPLES_TESTCASE_DM_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_DM_ITC)
 #define TC_DM_STACK  20480
 #endif
+#if defined(CONFIG_EXAMPLES_TESTCASE_MQTT_UTC)
+#define TC_MQTT_STACK 4096
+#endif
 
 sem_t tc_sem;
 int working_tc;
@@ -67,6 +70,7 @@ extern int utc_sysio_main(int argc, char *argv[]);
 extern int itc_sysio_main(int argc, char *argv[]);
 extern int utc_dm_main(int argc, char *argv[]);
 extern int itc_dm_main(int argc, char *argv[]);
+extern int utc_mqtt_main(int argc, char *argv[]);
 
 /* Not yet */
 extern int mpu_tc_main(int argc, char *argv[]);
@@ -147,6 +151,13 @@ int tc_main(int argc, char *argv[])
 	pid = task_create("sysioitc", SCHED_PRIORITY_DEFAULT, TC_SYSTEMIO_STACK, itc_sysio_main, argv);
 	if (pid < 0) {
 		printf("System IO itc is not started, err = %d\n", pid);
+	}
+#endif
+
+#ifdef CONFIG_EXAMPLES_TESTCASE_MQTT_UTC
+	pid = task_create("mqttutc", SCHED_PRIORITY_DEFAULT, TC_MQTT_STACK, utc_mqtt_main, argv);
+	if (pid < 0) {
+		printf("MQTT utc is not started, err = %d\n", pid);
 	}
 #endif
 	do {
