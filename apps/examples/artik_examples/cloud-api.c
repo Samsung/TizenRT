f9dcdec505f5a141864a1372638d818facd5ba7f
Gregory Lemercier <g.lemercier@samsung.com>
2017-09-18 10:46:46 +0200

    examples/artik_examples: configure SSL verification for cloud commands
    Change-Id: I3eb435496a8f5635015f0fe8f64ae38e3e83b936
    Signed-off-by: Gregory Lemercier <g.lemercier@samsung.com>

--- apps/examples/artik_examples/cloud-api.c
+++ apps/examples/artik_examples/cloud-api.c
@@ -59,6 +59,36 @@ struct ota_info {
 	int fd;
 };
 
+static const char akc_root_ca[] =
+	"-----BEGIN CERTIFICATE-----\r\n"
+	"MIIE0zCCA7ugAwIBAgIQGNrRniZ96LtKIVjNzGs7SjANBgkqhkiG9w0BAQUFADCB\r\n"
+	"yjELMAkGA1UEBhMCVVMxFzAVBgNVBAoTDlZlcmlTaWduLCBJbmMuMR8wHQYDVQQL\r\n"
+	"ExZWZXJpU2lnbiBUcnVzdCBOZXR3b3JrMTowOAYDVQQLEzEoYykgMjAwNiBWZXJp\r\n"
+	"U2lnbiwgSW5jLiAtIEZvciBhdXRob3JpemVkIHVzZSBvbmx5MUUwQwYDVQQDEzxW\r\n"
+	"ZXJpU2lnbiBDbGFzcyAzIFB1YmxpYyBQcmltYXJ5IENlcnRpZmljYXRpb24gQXV0\r\n"
+	"aG9yaXR5IC0gRzUwHhcNMDYxMTA4MDAwMDAwWhcNMzYwNzE2MjM1OTU5WjCByjEL\r\n"
+	"MAkGA1UEBhMCVVMxFzAVBgNVBAoTDlZlcmlTaWduLCBJbmMuMR8wHQYDVQQLExZW\r\n"
+	"ZXJpU2lnbiBUcnVzdCBOZXR3b3JrMTowOAYDVQQLEzEoYykgMjAwNiBWZXJpU2ln\r\n"
+	"biwgSW5jLiAtIEZvciBhdXRob3JpemVkIHVzZSBvbmx5MUUwQwYDVQQDEzxWZXJp\r\n"
+	"U2lnbiBDbGFzcyAzIFB1YmxpYyBQcmltYXJ5IENlcnRpZmljYXRpb24gQXV0aG9y\r\n"
+	"aXR5IC0gRzUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCvJAgIKXo1\r\n"
+	"nmAMqudLO07cfLw8RRy7K+D+KQL5VwijZIUVJ/XxrcgxiV0i6CqqpkKzj/i5Vbex\r\n"
+	"t0uz/o9+B1fs70PbZmIVYc9gDaTY3vjgw2IIPVQT60nKWVSFJuUrjxuf6/WhkcIz\r\n"
+	"SdhDY2pSS9KP6HBRTdGJaXvHcPaz3BJ023tdS1bTlr8Vd6Gw9KIl8q8ckmcY5fQG\r\n"
+	"BO+QueQA5N06tRn/Arr0PO7gi+s3i+z016zy9vA9r911kTMZHRxAy3QkGSGT2RT+\r\n"
+	"rCpSx4/VBEnkjWNHiDxpg8v+R70rfk/Fla4OndTRQ8Bnc+MUCH7lP59zuDMKz10/\r\n"
+	"NIeWiu5T6CUVAgMBAAGjgbIwga8wDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8E\r\n"
+	"BAMCAQYwbQYIKwYBBQUHAQwEYTBfoV2gWzBZMFcwVRYJaW1hZ2UvZ2lmMCEwHzAH\r\n"
+	"BgUrDgMCGgQUj+XTGoasjY5rw8+AatRIGCx7GS4wJRYjaHR0cDovL2xvZ28udmVy\r\n"
+	"aXNpZ24uY29tL3ZzbG9nby5naWYwHQYDVR0OBBYEFH/TZafC3ey78DAJ80M5+gKv\r\n"
+	"MzEzMA0GCSqGSIb3DQEBBQUAA4IBAQCTJEowX2LP2BqYLz3q3JktvXf2pXkiOOzE\r\n"
+	"p6B4Eq1iDkVwZMXnl2YtmAl+X6/WzChl8gGqCBpH3vn5fJJaCGkgDdk+bW48DW7Y\r\n"
+	"5gaRQBi5+MHt39tBquCWIMnNZBU4gcmU7qKEKQsTb47bDN0lAtukixlE0kF6BWlK\r\n"
+	"WE9gyn6CagsCqiUXObXbf+eEZSqVir2G3l6BFoMtEMze/aiCKm0oHw0LxOXnGiYZ\r\n"
+	"4fQRbxC1lfznQgUy286dUV4otp6F01vvpX1FQHKOtw5rDgb7MzVIcbidJ4vEZV8N\r\n"
+	"hnacRHr2lVz2XTIIM6RUthg/aFzyQkqFOFSDX9HoLPKsEdao7WNq\r\n"
+	"-----END CERTIFICATE-----\r\n";
+
 static int device_command(int argc, char *argv[]);
 static int devices_command(int argc, char *argv[]);
 static int message_command(int argc, char *argv[]);
@@ -116,6 +146,7 @@ static int device_command(int argc, char *argv[])
 	char *response = NULL;
 	artik_error err = S_OK;
 	bool properties = false;
+	artik_ssl_config ssl;
 
 	if (!cloud) {
 		fprintf(stderr, "Failed to request cloud module\n");
@@ -133,7 +164,11 @@ static int device_command(int argc, char *argv[])
 		properties = (atoi(argv[5]) > 0);
 	}
 
-	err = cloud->get_device(argv[3], argv[4], properties, &response, NULL);
+	memset(&ssl, 0, sizeof(ssl));
+	ssl.ca_cert.data = (char *)akc_root_ca;
+	ssl.ca_cert.len = sizeof(akc_root_ca);
+
+	err = cloud->get_device(argv[3], argv[4], properties, &response, &ssl);
 	if (err != S_OK) {
 		FAIL_AND_EXIT("Failed to get user devices\n");
 		goto exit;
@@ -160,6 +195,7 @@ static int devices_command(int argc, char *argv[])
 	int count = 10;
 	bool properties = false;
 	int offset = 0;
+	artik_ssl_config ssl;
 
 	if (!cloud) {
 		fprintf(stderr, "Failed to request cloud module\n");
@@ -182,7 +218,11 @@ static int devices_command(int argc, char *argv[])
 		}
 	}
 
-	err = cloud->get_user_devices(argv[3], count, properties, 0, argv[4], &response, NULL);
+	memset(&ssl, 0, sizeof(ssl));
+	ssl.ca_cert.data = (char *)akc_root_ca;
+	ssl.ca_cert.len = sizeof(akc_root_ca);
+
+	err = cloud->get_user_devices(argv[3], count, properties, 0, argv[4], &response, &ssl);
 	if (err != S_OK) {
 		FAIL_AND_EXIT("Failed to get user devices\n");
 		goto exit;
@@ -206,6 +246,7 @@ static int message_command(int argc, char *argv[])
 	int ret = 0;
 	char *response = NULL;
 	artik_error err = S_OK;
+	artik_ssl_config ssl;
 
 	if (!cloud) {
 		fprintf(stderr, "Failed to request cloud module\n");
@@ -218,7 +259,11 @@ static int message_command(int argc, char *argv[])
 		goto exit;
 	}
 
-	err = cloud->send_message(argv[3], argv[4], argv[5], &response, NULL);
+	memset(&ssl, 0, sizeof(ssl));
+	ssl.ca_cert.data = (char *)akc_root_ca;
+	ssl.ca_cert.len = sizeof(akc_root_ca);
+
+	err = cloud->send_message(argv[3], argv[4], argv[5], &response, &ssl);
 	if (err != S_OK) {
 		FAIL_AND_EXIT("Failed to send message\n");
 		goto exit;
