d63346a869ba609a1f56fce0114767bee9aafff5
Junhwan Park <junhwan.park@samsung.com>
2017-10-12 14:34:52 +0900

    Update s5j_clock.c

--- os/arch/arm/src/s5j/s5j_clock.c
+++ os/arch/arm/src/s5j/s5j_clock.c
@@ -226,7 +226,7 @@ void s5j_clk_set_rate(unsigned int id, unsigned long rate)
 	case CLK_SPL_SPI1:
 	case CLK_SPL_SPI2:
 	case CLK_SPL_SPI3:
-		/* Exynos T200 provides an SPI with various kinds of clock sources.
+		/* S5J provides an SPI with various kinds of clock sources.
 		 * SPI uses the SCLK_SPI clock from an external Clock Controller Module (CMU).
 		 * You can also select an SCLK_SPI from various clock sources.
 		 * An SPI contains an internal 1/2 clock divider.
