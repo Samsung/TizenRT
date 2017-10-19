c8dee724098f3f94a6e91158fec5def76f5de05e
Lokesh B V <lokeshbv@users.noreply.github.com>
2017-10-19 20:48:49 +0530

    tools/csvparser.c: Fix build error.
    Fix build error caused by  wrong typo of variable "nparms" in commit https://github.com/Samsung/TizenRT/commit/819fa0bdfb525199d7f587fade7f5af1311d2827

--- os/tools/csvparser.c
+++ os/tools/csvparser.c
@@ -209,7 +209,7 @@ int parse_csvline(char *ptr)
 	 */
 
 	do {
-		if (nparams >= MAX_FIELDS) {
+		if (nparms >= MAX_FIELDS) {
 			fprintf(stderr, "%d: too many Parameters: \"%s\"\n", g_lineno, g_line);
 			exit(8);
 		}
