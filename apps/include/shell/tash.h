269daf983cca8ebb72e059be21629513b3659635
sunghan <sh924.chang@samsung.com>
2017-11-02 17:03:43 +0900

    TASH: clean up on tash.h
    1. Split type definition and strucutre definition
    2. Modify doxygen comments for group definition and descriptions for each APIs

--- apps/include/shell/tash.h
+++ apps/include/shell/tash.h
@@ -15,107 +15,116 @@
  * language governing permissions and limitations under the License.
  *
  ****************************************************************************/
+/**
+ * @defgroup TASH SHELL
+ * @brief Provides APIs for shell (TASH - TinyAra SHell)
+ */
+
+/// @file  tash.h
+/// @brief Shell APIs to register commands and to get command informations
+
 #ifndef __APPS_INCLUDE_SHELL_TASH_H
 #define __APPS_INCLUDE_SHELL_TASH_H
 
 #include <tinyara/config.h>
 
+/**
+ * @brief   Definitions of command execution types
+ * @details @b #include <apps/shell/tash.h> \n
+ *          The type, SYNC (0) is running command in TASH task.
+ *                   ASYNC (1) is running command in seperated thread.
+ */
 #define TASH_EXECMD_SYNC  (0)
 #define TASH_EXECMD_ASYNC (1)
 
-/* Typedef for tash command's callback
- * @retval 0 success
- **/
+/**
+ * @brief tash command's callback function prototype
+ * @details @b #include <apps/shell/tash.h>
+ */
 typedef int (*TASH_CMD_CALLBACK)(int argc, char **args);
 
-typedef struct {
+/**
+ * @brief   Structure of TASH command list
+ * @details @b #include <apps/shell/tash.h> \n
+ *          To register TASH command, these elements are needed.
+ */
+struct tash_cmdlist_s {
 	const char *str;
 	const TASH_CMD_CALLBACK cb;
 	const int thread_exec;
-} tash_cmdlist_t;
+};
+typedef struct tash_cmdlist_s tash_cmdlist_t;
 
+/**
+ * @brief   Structure of task information for TASH command
+ * @details @b #include <apps/shell/tash.h>
+ */
 struct tash_taskinfo_s {
 	const char *str;
 	const int task_prio;
 	const int task_stacksize;
 };
-
 typedef struct tash_taskinfo_s tash_taskinfo_t;
 
-/** @name tash_cmd_install
- * @brief API to install TASH shell commands
- * @ingroup tash
- * @param[in] str - command's first string(Example: ps, ls, ifconfig etc)
- * @param[in] cb - callback function for the command
- * @param[in] thread_exec: TASH_EXECMD_ASYNC-execute callback as a thread
+/**
+ * @brief   API to install a TASH command
+ * @details @b #include <apps/shell/tash.h>
+ * @param[in] str          Command string(Example: ps, ls, ifconfig etc)
+ * @param[in] cb           Callback function for the command
+ * @param[in] thread_exec  TASH_EXECMD_ASYNC-execute callback as a thread
 						   TASH_EXECMD_SYNC -invoked callback directly
  * @retval 0 Success
  * @retval -ve Failure
- *
- * Example Usage:
- * @code
- *
- * @endcode
- **/
+ */
 int tash_cmd_install(const char *str, TASH_CMD_CALLBACK cb, int thread_exec);
 
-/** @name tash_cmdlist_install
- * @brief API to register TASH shell command list
- * @ingroup tash
- * @param[in] list[] - tash command list structure which has command name, callback fnc, and attribute
- *
- * Example Usage:
- * @code
- *
- * @endcode
- **/
+/**
+ * @brief   API to register multiple TASH commands at once
+ * @details @b #include <apps/shell/tash.h> \n
+ *          The difference with tash_cmd_install is supporting command list.
+ *          When you want to register several commands at once, you can use this.
+ * @param[in] list[] tash command list structure which has command name, callback fnc, and attribute
+ */
 void tash_cmdlist_install(const tash_cmdlist_t list[]);
 
-/** @name tash_start
- * @brief API to start TASH
- * @ingroup tash
- * @retval 0 Success
- * @retval -ve Failure
- *
- * Example Usage:
- * @code
- *
- * @endcode
- **/
+/**
+ * @cond
+ * @internal
+ */
+/**
+ * @brief   API to start TASH
+ * @details @b #include <apps/shell/tash.h>
+ * @return  On success, 0 is returned. On failure, negative value is returned.
+ */
 int tash_start(void);
+/**
+ * @endcond
+ */
 
 #if defined(CONFIG_TASH_COMMAND_INTERFACE)
-/** @name tash_get_cmdscount
- * @brief API to get the number of registered tash commands.
- * In protected build, it returns the count of user context commands only.
- * @ingroup tash
- * @ret +ve - Tash commands count
- *     -ve - Error
- *
- * Example Usage:
- * @code
- *
- * @endcode
- **/
+/**
+ * @brief   API to get the number of registered tash commands.
+ * @details @b #include <apps/shell/tash.h> \n
+ *          In protected build, it returns the count of user context commands only.
+ * @return  On success, positive value is returned. On failure, negative value is returned.
+ */
 int tash_get_cmdscount(void);
 
-/** @name tash_get_cmdpair
- * @brief API to get registered tash commands one at a time
- * @ingroup tash
- * @param[in] str-pointer to get the command string with space for 32 bytes
- * @param[in] cb- Callback function pointer
- * @param[in] index - Tash command index
-
- * @ret 0 - Successful
- *     -1 - Error
+/**
+ * @brief   API to get registered tash commands one at a time
+ * @details @b #include <apps/shell/tash.h>
+ * @param[in] str   Pointer to get the command string with space for 32 bytes
+ * @param[in] cb    Callback function pointer
+ * @param[in] index Tash command index
+ * @return  On success, 0 is returned. On failure, -1 is returned.
  *
  * Example Usage:
  * @code
-	count = tash_get_cmdscount();
-	for (i=0; i<count; i++)
-		tash_get_cmdpair(str, &cb, i );
+ *	count = tash_get_cmdscount();
+ *	for (i=0; i<count; i++)
+ *		tash_get_cmdpair(str, &cb, i );
  * @endcode
- **/
+ */
 int tash_get_cmdpair(char *str, TASH_CMD_CALLBACK *cb, int index);
 #endif							/*CONFIG_TASH_COMMAND_INTERFACE */
 
