a0be2eca0b97a14edec7a3dab0d7ded355c6e6a3
Camille Bégué <c.begue@samsung.com>
2017-11-28 18:03:27 +0100

    artik_examples/http: add asynchronous examples
    This patch adds new examples for asynchronous API of HTTP module.
    
    Change-Id: Ibba25cac96e855bd90b9dd5c088625b19db86c3c
    Signed-off-by: Camille Bégué <c.begue@samsung.com>

--- apps/examples/artik_examples/http-api.c
+++ apps/examples/artik_examples/http-api.c
@@ -39,12 +39,20 @@ static int http_get(int argc, char **argv);
 static int http_post(int argc, char **argv);
 static int http_put(int argc, char **argv);
 static int http_delete(int argc, char **argv);
+static int http_get_async(int argc, char **argv);
+static int http_post_async(int argc, char **argv);
+static int http_put_async(int argc, char **argv);
+static int http_delete_async(int argc, char **argv);
 
 const struct command http_commands[] = {
-	{ "get", "get <url>", http_get},
-	{ "post", "post <url> <body>", http_post},
-	{ "put", "put <url> <body>", http_put},
+	{ "get", "get <url>", http_get },
+	{ "post", "post <url> <body>", http_post },
+	{ "put", "put <url> <body>", http_put },
 	{ "delete", "delete <url>", http_delete },
+	{ "get-async", "get-async <url>", http_get_async },
+	{ "post-async", "post-async <url> <body>", http_post_async },
+	{ "put-async", "put-async <url> <body>", http_put_async },
+	{ "delete-async", "delete-async <url>", http_delete_async },
 	{ "", "", NULL }
 };
 
@@ -191,6 +199,147 @@ static int http_delete(int argc, char **argv)
 		fprintf(stderr, "HTTP %d - %s\n", status, response);
 		free(response);
 	}
+
+exit:
+	artik_release_api_module(http);
+	return (ret == S_OK);
+}
+
+static void http_response_callback(artik_error result, int status,
+								char *response, void *user_data)
+{
+	if (result != S_OK)
+		fprintf(stderr, "error = %s\n", error_msg(result));
+	else
+		fprintf(stdout, "response = %s\nstatus = %d\n", response,
+								status);
+}
+
+static int http_get_async(int argc, char **argv)
+{
+	artik_http_module *http = (artik_http_module *)artik_request_api_module("http");
+	artik_error ret = S_OK;
+	artik_http_headers headers;
+	artik_http_header_field fields[] = {
+		{"Connect", "close"},
+		{"User-Agent", "Artik browser"},
+		{"Accept-Language", "en-US,en;q=0.8"},
+	};
+
+	if (argc < 4) {
+		fprintf(stderr, "Wrong number of arguments");
+		goto exit;
+	}
+
+	headers.fields = fields;
+	headers.num_fields = ARRAY_SIZE(fields);
+
+	printf("uri = %s\n", argv[3]);
+	ret = http->get_async(argv[3], &headers, http_response_callback, NULL, NULL);
+	if (ret != S_OK) {
+		fprintf(stderr, "Failed to get %s (err:%s)\n", argv[3], error_msg(ret));
+		goto exit;
+	}
+
+	fprintf(stdout, "ASYNC GET %s", argv[3]);
+
+exit:
+	artik_release_api_module(http);
+	return (ret == S_OK);
+}
+
+static int http_post_async(int argc, char **argv)
+{
+	artik_http_module *http = (artik_http_module *)artik_request_api_module("http");
+	artik_error ret = S_OK;
+	artik_http_headers headers;
+	artik_http_header_field fields[] = {
+		{"Connect", "close"},
+		{"User-Agent", "Artik browser"},
+		{"Accept-Language", "en-US,en;q=0.8"},
+	};
+
+	if (argc < 5) {
+		fprintf(stderr, "Wrong number of arguments");
+		goto exit;
+	}
+
+	headers.fields = fields;
+	headers.num_fields = ARRAY_SIZE(fields);
+
+	ret = http->post_async(argv[3], &headers, argv[4],
+			http_response_callback, NULL, NULL);
+	if (ret != S_OK) {
+		fprintf(stderr, "Failed to get %s (err:%s)\n", argv[3], error_msg(ret));
+		goto exit;
+	}
+
+	fprintf(stdout, "ASYNC POST %s", argv[3]);
+
+exit:
+	artik_release_api_module(http);
+	return (ret == S_OK);
+}
+
+static int http_put_async(int argc, char **argv)
+{
+	artik_http_module *http = (artik_http_module *)artik_request_api_module("http");
+	artik_error ret = S_OK;
+	artik_http_headers headers;
+	artik_http_header_field fields[] = {
+		{"Connect", "close"},
+		{"User-Agent", "Artik browser"},
+		{"Accept-Language", "en-US,en;q=0.8"},
+	};
+
+	if (argc < 4) {
+		fprintf(stderr, "Wrong number of arguments");
+		goto exit;
+	}
+
+	headers.fields = fields;
+	headers.num_fields = ARRAY_SIZE(fields);
+
+	ret = http->put_async(argv[3], &headers, argv[4],
+			http_response_callback, NULL, NULL);
+	if (ret != S_OK) {
+		fprintf(stderr, "Failed to get %s (err:%s)\n", argv[3], error_msg(ret));
+		goto exit;
+	}
+
+	fprintf(stdout, "ASYNC PUT %s", argv[3]);
+exit:
+	artik_release_api_module(http);
+	return (ret == S_OK);
+}
+
+static int http_delete_async(int argc, char **argv)
+{
+	artik_http_module *http = (artik_http_module *)artik_request_api_module("http");
+	artik_error ret = S_OK;
+	artik_http_headers headers;
+	artik_http_header_field fields[] = {
+		{"Connect", "close"},
+		{"User-Agent", "Artik browser"},
+		{"Accept-Language", "en-US,en;q=0.8"},
+	};
+
+	if (argc < 3) {
+		fprintf(stderr, "Wrong number of arguments");
+		goto exit;
+	}
+
+	headers.fields = fields;
+	headers.num_fields = ARRAY_SIZE(fields);
+
+	ret = http->del_async(argv[3], &headers,
+			http_response_callback, NULL, NULL);
+	if (ret != S_OK) {
+		fprintf(stderr, "Failed to get %s (err:%s)\n", argv[3], error_msg(ret));
+		goto exit;
+	}
+
+	fprintf(stdout, "ASYNC DELETE %s", argv[3]);
 
 exit:
 	artik_release_api_module(http);
