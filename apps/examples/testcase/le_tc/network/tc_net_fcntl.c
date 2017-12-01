aab916e430b9421b73876368638086675ee1da23
Arvin Mittal <arvin.mittal@samsung.com>
2017-12-01 19:22:17 +0530

    testcase/le_tc/network : add scenario for net_fcntl file path : apps/examples/testcase/le_tc/network file name : tc_net_fcntl.c
    add scenario :
    tc_net_fcntl_dupfd_p
    tc_net_fcntl_dupfd_nonblock_p
    
    Signed-off-by: Arvin Mittal <arvin.mittal@samsung.com>

--- apps/examples/testcase/le_tc/network/tc_net_fcntl.c
+++ apps/examples/testcase/le_tc/network/tc_net_fcntl.c
@@ -16,104 +16,150 @@
  *
  ****************************************************************************/
 
-/// @file tc_net_fcntl.c
-/// @brief Test Case Example for fcntl() API
-#include <tinyara/config.h>
-#include <stdio.h>
-#include <errno.h>
-
-#include <sys/stat.h>
-#include <net/if.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-//#include <arch/board/board.h>
-#include <netutils/netlib.h>
+///@file tc_net_fcntl.c
+///@brief Test Case Example for fcntl() API
 
 #include <sys/socket.h>
-
 #include "tc_internal.h"
 
 /**
-   * @testcase		   :tc_net_fcntl_nonblock_p
-   * @brief		   :
-   * @scenario		   :
-   * @apicovered	   :fcntl()
-   * @precondition	   :
-   * @postcondition	   :
-   */
+* @testcase          :tc_net_fcntl_nonblock_p
+* @brief             :manipulate file descriptor
+* @scenario          :open socket and call fcntl with flag F_SETFL and O_NONBLOCK
+* @apicovered        :fcntl()
+* @precondition      :socket open
+* @postcondition     :socket close
+*/
 static void tc_net_fcntl_nonblock_p(void)
 {
-
 	int fd = -1;
 	fd = socket(AF_INET, SOCK_STREAM, 0);
+	TC_ASSERT_GEQ("socket", fd, 0);
+
 	int ret = fcntl(fd, F_SETFL, O_NONBLOCK);
-	close(fd);
+	TC_ASSERT_NEQ_CLEANUP("fcntl", ret, -1, close(fd));
 
-	TC_ASSERT_NEQ("fcntl", ret, -1);
+	ret = close(fd);
+	TC_ASSERT_EQ("close", ret, 0);
+
 	TC_SUCCESS_RESULT();
-
 }
 
 /**
-   * @testcase		   :tc_net_fcntl_p
-   * @brief		   :
-   * @scenario		   :
-   * @apicovered	   :fcntl()
-   * @precondition	   :
-   * @postcondition	   :
-   */
+* @testcase          :tc_net_fcntl_p
+* @brief             :manipulate file descriptor
+* @scenario          :open socket and call fcntl with flag F_SETFL
+* @apicovered        :fcntl()
+* @precondition      :open socket
+* @postcondition     :close socket
+*/
 static void tc_net_fcntl_p(void)
 {
-
 	int fd = -1;
 	fd = socket(AF_INET, SOCK_STREAM, 0);
+	TC_ASSERT_GEQ("socket", fd, 0);
+
 	int ret = fcntl(fd, F_SETFL, 0);
-	close(fd);
+	TC_ASSERT_NEQ_CLEANUP("fcntl", ret, -1, close(fd));
 
-	TC_ASSERT_NEQ("fcntl", ret, -1);
+	ret = close(fd);
+	TC_ASSERT_EQ("close", ret, 0);
+
 	TC_SUCCESS_RESULT();
-
 }
 
+/**
+* @testcase          :tc_net_fcntl_dupfd_p
+* @brief             :manipulate file descriptor
+* @scenario          :open socket and call fcntl with flag F_DUPFD which duplicates the file descriptor
+* @apicovered        :fcntl()
+* @precondition      :open socket
+* @postcondition     :close socket
+*/
+static void tc_net_fcntl_dupfd_p(void)
+{
+	int ret;
+	int fd = -1;
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	TC_ASSERT_GEQ("socket", fd, 0);
+
+	int nfd = fcntl(fd, F_DUPFD, 0);
+	TC_ASSERT_GEQ_CLEANUP("fcntl", nfd, fd, close(fd));
+
+	ret = close(fd);
+	TC_ASSERT_EQ_CLEANUP("close", ret, 0, close(nfd));
+
+	ret = close(nfd);
+	TC_ASSERT_EQ("close", ret, 0);
+
+	TC_SUCCESS_RESULT();
+}
 
 /**
-   * @testcase		   :tc_net_fcntl_n
-   * @brief		   :
-   * @scenario		   :
-   * @apicovered	   :fcntl()
-   * @precondition	   :
-   * @postcondition	   :
-   */
+* @testcase          :tc_net_fcntl_dupfd_nonblock_p
+* @brief             :manipulate file descriptor
+* @scenario          :open socket and call fcntl with flag O_NONBLOCK and F_DUPFD which duplicates the file descriptor
+* @apicovered        :fcntl()
+* @precondition      :socket open
+* @postcondition     :socket close
+*/
+static void tc_net_fcntl_dupfd_nonblock_p(void)
+{
+	int ret;
+	int fd = -1;
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	TC_ASSERT_GEQ("socket", fd, 0);
+
+	int nfd = fcntl(fd, F_DUPFD, O_NONBLOCK);
+	TC_ASSERT_GEQ_CLEANUP("fcntl", nfd, fd, close(fd));
+
+	ret = close(fd);
+	TC_ASSERT_EQ_CLEANUP("close", ret, 0, close(nfd));
+
+	ret = close(nfd);
+	TC_ASSERT_EQ("close", ret, 0);
+
+	TC_SUCCESS_RESULT();
+}
+
+/**
+* @testcase          :tc_net_fcntl_n
+* @brief             :manipulate file descriptor
+* @scenario          :call fcntl with invalid descriptor
+* @apicovered        :fcntl()
+* @precondition      :none
+* @postcondition     :none
+*/
 static void tc_net_fcntl_n(void)
 {
-
 	int fd = -1;
 	int ret = fcntl(fd, F_SETFL, O_NONBLOCK);
-
 	TC_ASSERT_NEQ("fcntl", ret, 0);
-	TC_SUCCESS_RESULT();
 
+	TC_SUCCESS_RESULT();
 }
 
 /**
-   * @testcase		   :tc_net_fcntl_ndelay_p
-   * @brief		   :
-   * @scenario		   :
-   * @apicovered	   :fcntl()
-   * @precondition	   :
-   * @postcondition	   :
-   */
+* @testcase          :tc_net_fcntl_ndelay_p
+* @brief             :manipulate file descriptor
+* @scenario          :open socket and call fcntl with flag F_SETFL and O_NDELAY
+* @apicovered        :fcntl()
+* @precondition      :open socket
+* @postcondition     :close socket descriptor
+*/
 static void tc_net_fcntl_ndelay_p(void)
 {
-
 	int fd = -1;
 	fd = socket(AF_INET, SOCK_STREAM, 0);
+	TC_ASSERT_GEQ("socket", fd, 0);
+
 	int ret = fcntl(fd, F_SETFL, O_NDELAY);
-	close(fd);
+	TC_ASSERT_NEQ_CLEANUP("fcntl", ret, -1, close(fd));
 
-	TC_ASSERT_NEQ("fcntl", ret, -1);
+	ret = close(fd);
+	TC_ASSERT_EQ("close", ret, 0);
+
 	TC_SUCCESS_RESULT();
-
 }
 
 /****************************************************************************
@@ -122,10 +168,12 @@ static void tc_net_fcntl_ndelay_p(void)
 
 int net_fcntl_main(void)
 {
-
 	tc_net_fcntl_nonblock_p();
 	tc_net_fcntl_p();
+	tc_net_fcntl_dupfd_p();
+	tc_net_fcntl_dupfd_nonblock_p();
 	tc_net_fcntl_n();
 	tc_net_fcntl_ndelay_p();
+
 	return 0;
 }
