aa7f514939720307820328ca884a98595af1fd91
Kavya <kavya.nunna@partner.samsung.com>
2017-12-26 19:45:47 +0530

    testcase/le_tc/kernel : Add Testcases for workqueue
    Covers apis :
    work_cancel()
    work_queue()
    
    Signed-off-by: Kavya <kavya.nunna@partner.samsung.com>

--- apps/examples/testcase/le_tc/kernel/tc_internal.h
+++ apps/examples/testcase/le_tc/kernel/tc_internal.h
@@ -90,6 +90,7 @@ int timer_main(void);
 int umm_heap_main(void);
 int tash_heapinfo_main(void);
 int tash_stackmonitor_main(void);
+int wqueue_main(void);
 int itc_environ_main(void);
 int itc_libc_pthread_main(void);
 int itc_libc_semaphore_main(void);
