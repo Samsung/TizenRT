d6c8883905787bba88c8b16030c30e64416706f9
junmin kim <junmindd.kim@samsung.com>
2018-02-21 23:26:09 -0800

    Add datapath value for constructor
    sync with RecorderDataSource

--- apps/examples/cxxtest/PlayerDataSource.cxx
+++ apps/examples/cxxtest/PlayerDataSource.cxx
@@ -6,6 +6,11 @@ namespace Media
 	{
 	}
 
+	PlayerDataSource::PlayerDataSource(int _channels, int _sampleRate, int _pcmFormat, std::string &_dataPath)
+		: DataSource(_channels, _sampleRate, _pcmFormat, _dataPath)
+	{
+	}
+
 	PlayerDataSource::~PlayerDataSource()
 	{
 	}
