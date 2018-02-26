4e45d7759c95ec86776e85b50f35971706ac7dc7
sunghan <sh924.chang@samsung.com>
2018-02-26 21:37:58 +0900

    apps/shell: make a new config, TASH_SCRIPT
    Script functionality on TASH is rarely used so that supporting disable script
    functionality is better on footprint.

--- apps/shell/tash_command.c
+++ apps/shell/tash_command.c
@@ -97,7 +97,7 @@ static struct tash_cmd_info_s tash_cmds_info = {PTHREAD_MUTEX_INITIALIZER};
 const static tash_cmdlist_t tash_basic_cmds[] = {
 	{"exit",  tash_exit,   TASH_EXECMD_SYNC},
 	{"help",  tash_help,   TASH_EXECMD_SYNC},
-#ifndef CONFIG_DISABLE_ENVIRON
+#ifdef CONFIG_TASH_SCRIPT
 	{"sh",    tash_script, TASH_EXECMD_SYNC},
 #endif
 #ifndef CONFIG_DISABLE_SIGNALS
