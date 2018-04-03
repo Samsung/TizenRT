714172203ad4493f84fb5af84570d45c6b80612a
jc_.kim <jc_.kim@samsung.com>
2018-04-03 11:00:26 +0900

    tc/heapinfo: Implement User defined group Heapinfo TC
    Three groups are pre-defined for testing "heapinfo -g".

--- apps/examples/testcase/le_tc/kernel/tc_tash_heapinfo.c
+++ apps/examples/testcase/le_tc/kernel/tc_tash_heapinfo.c
@@ -39,6 +39,20 @@
 
 static char *str = "tc-heapinfo";
 static int malloc_num;
+#ifdef CONFIG_HEAPINFO_GROUP
+#define HEAPINFO_ALLOC 0
+#define HEAPINFO_FREE 1
+#define HEAPINFO_STACK_ALLOC 2
+#define HEAPINFO_STACK_FREE 3
+/* group_x_size[0] means current heap alloc size of group_x
+ * group_x_size[1] means peak heap size of group_x
+ * group_x_size[2] means total current alloc size of grou_x
+ * group_x_size[3] means total current alloc size of grou_x
+ */
+int group_a_info[4];
+int group_b_info[4];
+int group_c_info[4];
+#endif
 
 static void *heapinfo_thread(void *arg)
 {
@@ -58,12 +72,120 @@ static void *heapinfo_thread(void *arg)
 
 	return NULL;
 }
+#ifdef CONFIG_HEAPINFO_GROUP
+static void calc_group_size(int *mem, int info[4], int type)
+{
+	struct mm_allocnode_s *node;
+	node = (struct mm_allocnode_s *)((char *)mem - SIZEOF_MM_ALLOCNODE);
+	if (type == HEAPINFO_ALLOC) {
+		info[0] += node->size;
+		if (info[0] > info[1]) {
+			info[1] = info[0];
+		}
+		info[2] += node->size;
+		if (info[2] > info[3]) {
+			info[3] = info[2];
+		}
+	} else if (type == HEAPINFO_FREE) {
+		info[0] -= node->size;
+		info[2] -= node->size;
+	} else if (type == HEAPINFO_STACK_ALLOC) {
+		info[2] += node->size;
+		if (info[2] > info[3]) {
+			info[3] = info[2];
+		}
+	} else {
+		info[2] -= node->size;
+	}
+}
+static void *heapinfo_malloc(size_t size, int info[4])
+{
+	int *mem;
+	mem = (int *)malloc(size);
+	calc_group_size(mem, info, HEAPINFO_ALLOC);
+	return mem;
+}
+static void *heapinfo_free(void *mem, int info[4])
+{
+	calc_group_size(mem, info, HEAPINFO_FREE);
+	free(mem);
+	return NULL;
+}
+
+static void heapinfo_calc_stack(pid_t pid, int info[4], int type)
+{
+	struct tcb_s *tcb;
+	tcb = sched_gettcb(pid);
+	if (type == HEAPINFO_STACK_ALLOC) {
+		calc_group_size(tcb->stack_alloc_ptr, info, HEAPINFO_STACK_ALLOC);
+	} else {
+		calc_group_size(tcb->stack_alloc_ptr, info, HEAPINFO_STACK_FREE);
+	}
+}
+static int heapinfo_task(int argc, char *argv[])
+{
+	int *mem_ptr;
+	heapinfo_calc_stack(getpid(), group_a_info, HEAPINFO_STACK_ALLOC);
+	
+	mem_ptr = (int *)heapinfo_malloc(1000, group_a_info);
+	heapinfo_free(mem_ptr, group_a_info);
+
+	mem_ptr = (int *)heapinfo_malloc(500, group_a_info);
+	heapinfo_free(mem_ptr, group_a_info);
+
+	heapinfo_calc_stack(getpid(), group_a_info, HEAPINFO_STACK_FREE);
+	return OK;
+}
+
+static int heapinfo_task2(int argc, char *argv[])
+{
+	int *mem_ptr;
+	heapinfo_calc_stack(getpid(), group_a_info, HEAPINFO_STACK_ALLOC);
+
+	mem_ptr = (int *)heapinfo_malloc(500, group_a_info);
+	heapinfo_free(mem_ptr, group_a_info);
+	mem_ptr = (int *)heapinfo_malloc(2000, group_a_info);
+	heapinfo_free(mem_ptr, group_a_info);
+
+	heapinfo_calc_stack(getpid(), group_a_info, HEAPINFO_STACK_FREE);
+	return OK;
+}
+
+static void *heapinfo_pthread(void *param)
+{
+	int *mem_ptr;
+	heapinfo_calc_stack(getpid(), group_c_info, HEAPINFO_STACK_ALLOC);
+
+	mem_ptr = (int *)heapinfo_malloc(300, group_c_info);
+	heapinfo_free(mem_ptr, group_c_info);
+	mem_ptr = (int *)heapinfo_malloc(500, group_c_info);
+	heapinfo_free(mem_ptr, group_c_info);
+
+	heapinfo_calc_stack(getpid(), group_c_info, HEAPINFO_STACK_FREE);
+	return OK;
+}
+
+static int heapinfo_task3(int argc, char *argv[])
+{
+	int *mem_ptr;
+	heapinfo_calc_stack(getpid(), group_b_info, HEAPINFO_STACK_ALLOC);
+
+	mem_ptr = (int *)heapinfo_malloc(1000, group_b_info);
+	heapinfo_free(mem_ptr, group_b_info);
+	mem_ptr = (int *)heapinfo_malloc(300, group_b_info);
+	heapinfo_free(mem_ptr, group_b_info);
+
+	heapinfo_calc_stack(getpid(), group_b_info, HEAPINFO_STACK_FREE);
+	return OK;
+}
+#endif
 
 static int tc_tash_heapinfo(int argc, char **args)
 {
 	int ret = ERROR;
 	pthread_t tc_thread;
 	pthread_attr_t tc_thread_attr;
+	pthread_t pid;
 
 	pthread_attr_init(&tc_thread_attr);
 
@@ -71,7 +193,33 @@ static int tc_tash_heapinfo(int argc, char **args)
 		printf("No param - Test with default value(stacksize:1024, malloc-num:5)\n");
 		tc_thread_attr.stacksize = DEFAULT_STKSIZE;
 		malloc_num = DEFAULT_MALLOC_NUM;
-	} else {
+	}
+#ifdef CONFIG_HEAPINFO_GROUP
+	else if (argc == 2 && strcmp(args[1], "-g") == 0) {
+		/* Test heapinfo group functionality,
+		 * Group A : heapinfo_task, heapinfo_task2
+		 * Group B : heapinfo_task3
+		 * Group C : heapinfo_pthread */
+		task_create("heapinfo_task", SCHED_PRIORITY_MAX, 1024, heapinfo_task, (char * const *)NULL);
+		task_create("heapinfo_task2", SCHED_PRIORITY_MAX, 1024, heapinfo_task2, (char * const *)NULL);
+		task_create("heapinfo_task3", SCHED_PRIORITY_MAX, 1024, heapinfo_task3, (char * const *)NULL);
+		pthread_create(&pid, NULL, heapinfo_pthread, (void *)NULL);
+		pthread_setname_np(pid, "heapinfo_pthread");
+
+		sleep(5);
+		printf("EXPECTED PEAK ALLOC SIZE AND STACK SIZE PER GROUP\n");
+		printf("-------------------------------------------------\n");
+		printf("PEAK | HEAP_ON_PEAK | STACK_ON_PEAK\n");
+		printf("------------------------------------\n");
+		printf("%3d | %12d | %13d\n", group_a_info[3], group_a_info[1], group_a_info[3] - group_a_info[1]);
+		printf("%3d | %12d | %13d\n", group_b_info[3], group_b_info[1], group_b_info[3] - group_b_info[1]);
+		printf("%3d | %12d | %13d\n", group_c_info[3], group_c_info[1], group_c_info[3] - group_c_info[1]);
+		TC_SUCCESS_RESULT();
+		return OK;
+	}
+#endif
+	else {
+		/* Test heapinfo normal functionality */
 		tc_thread_attr.stacksize = atoi(args[1]);
 		malloc_num = atoi(args[2]);
 		printf("Test with stacksize: %d, malloc-num: %d\n", tc_thread_attr.stacksize, malloc_num);
