9249aa57c1a4bd90407ecef810a6fcf6a71610a9
sunghan-chang <sh924.chang@samsung.com>
2018-05-02 05:23:44 -0700

    refactoring of table: list by Arphabet order
    To search quickly.

--- apps/examples/testcase/tc_main.c
+++ apps/examples/testcase/tc_main.c
@@ -33,20 +33,32 @@
 #if defined(CONFIG_EXAMPLES_TESTCASE_ARASTORAGE_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_ARASTORAGE_ITC)
 #define TC_ARASTORAGE_STACK       4096
 #endif
+#if defined(CONFIG_EXAMPLES_TESTCASE_AUDIO_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_AUDIO_ITC)
+#define TC_AUDIO_STACK  2048
+#endif
+#if defined(CONFIG_EXAMPLES_TESTCASE_DM_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_DM_ITC)
+#define TC_DM_STACK  2048
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_DRIVERS
+#define TC_DRIVERS_STACK   2048
+#endif
 #ifdef CONFIG_EXAMPLES_TESTCASE_FILESYSTEM
 #define TC_FS_STACK       4096
 #endif
 #ifdef CONFIG_EXAMPLES_TESTCASE_KERNEL
 #define TC_KERNEL_STACK   2048
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_DRIVERS
-#define TC_DRIVERS_STACK   2048
-#endif
 #ifdef CONFIG_EXAMPLES_TESTCASE_MPU
 #define TC_MPU_STACK   2048
 #endif
+#if defined(CONFIG_EXAMPLES_TESTCASE_MQTT_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_MQTT_ITC)
+#define TC_MQTT_STACK 4096
+#endif
 #ifdef CONFIG_EXAMPLES_TESTCASE_NETWORK
 #define TC_NETWORK_STACK  2048
+#endif
+#if defined(CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_ITC)
+#define TC_SYSTEMIO_STACK 2048
 #endif
 #if defined(CONFIG_EXAMPLES_TESTCASE_TASK_MANAGER_UTC)
 #define TC_TASK_MANAGER_STACK  2048
@@ -54,23 +66,8 @@
 #ifdef CONFIG_EXAMPLES_TESTCASE_TTRACE
 #define TC_TTRACE_STACK  2048
 #endif
-#if defined(CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_ITC)
-#define TC_SYSTEMIO_STACK 2048
-#endif
-#if defined(CONFIG_EXAMPLES_TESTCASE_DM_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_DM_ITC)
-#define TC_DM_STACK  2048
-#endif
 #if defined(CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_ITC)
 #define TC_WIFI_MANAGER_STACK  4096
-#endif
-#if defined(CONFIG_EXAMPLES_TESTCASE_MQTT_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_MQTT_ITC)
-#define TC_MQTT_STACK 4096
-#endif
-#if defined(CONFIG_EXAMPLES_TESTCASE_AUDIO_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_AUDIO_ITC)
-#define TC_AUDIO_STACK  2048
-#endif
-#if defined(CONFIG_EXAMPLES_TESTCASE_MEDIA_UTC) || defined(CONFIG_EXAMPLES_TESTCASE_MEDIA_ITC)
-#define TC_MEDIA_STACK  8192
 #endif
 #endif
 
@@ -80,27 +77,26 @@ int total_pass;
 int total_fail;
 
 /* Library&Environment Test Case as le_tc*/
+extern int tc_drivers_main(int argc, char *argv[]);
 extern int tc_filesystem_main(int argc, char *argv[]);
 extern int tc_kernel_main(int argc, char *argv[]);
 extern int tc_network_main(int argc, char *argv[]);
 extern int tc_ttrace_main(int argc, char *argv[]);
-extern int tc_drivers_main(int argc, char *argv[]);
 
 /* TinyAra Public API Test Case as ta_tc */
 extern int utc_arastorage_main(int argc, char *argv[]);
 extern int itc_arastorage_main(int argc, char *argv[]);
-extern int utc_sysio_main(int argc, char *argv[]);
-extern int itc_sysio_main(int argc, char *argv[]);
+extern int utc_audio_main(int argc, char *argv[]);
+extern int itc_audio_main(int argc, char *argv[]);
 extern int utc_dm_main(int argc, char *argv[]);
 extern int itc_dm_main(int argc, char *argv[]);
+extern int utc_mqtt_main(int argc, char *argv[]);
+extern int itc_mqtt_main(int argc, char *argv[]);
+extern int utc_sysio_main(int argc, char *argv[]);
+extern int itc_sysio_main(int argc, char *argv[]);
 extern int utc_task_manager_main(int argc, char *argv[]);
 extern int utc_wifi_manager_main(int argc, char *argv[]);
 extern int itc_wifi_manager_main(int argc, char *argv[]);
-extern int utc_mqtt_main(int argc, char *argv[]);
-extern int itc_mqtt_main(int argc, char *argv[]);
-extern int utc_audio_main(int argc, char *argv[]);
-extern int itc_audio_main(int argc, char *argv[]);
-extern int utc_media_main(int argc, char *argv[]);
 
 /* Not yet */
 extern int tc_mpu_main(int argc, char *argv[]);
@@ -113,26 +109,11 @@ static const tash_cmdlist_t tc_cmds[] = {
 #ifdef CONFIG_EXAMPLES_TESTCASE_ARASTORAGE_ITC
 	{"arastorage_itc", itc_arastorage_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_FILESYSTEM
-	{"filesystem_tc", tc_filesystem_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_AUDIO_UTC
+	{"audio_utc", utc_audio_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_KERNEL
-	{"kernel_tc", tc_kernel_main, TASH_EXECMD_ASYNC},
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_DRIVERS
-	{"drivers_tc", tc_drivers_main, TASH_EXECMD_ASYNC},
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_MPU
-	{"mpu_tc", tc_mpu_main, TASH_EXECMD_ASYNC},
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_NETWORK
-	{"network_tc", tc_network_main, TASH_EXECMD_ASYNC},
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_TASK_MANAGER_UTC
-	{"taskmgr_utc", utc_task_manager_main, TASH_EXECMD_ASYNC},
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_TTRACE
-	{"ttrace_tc", tc_ttrace_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_AUDIO_ITC
+	{"audio_itc", itc_audio_main, TASH_EXECMD_ASYNC},
 #endif
 #ifdef CONFIG_EXAMPLES_TESTCASE_DM_UTC
 	{"dm_utc", utc_dm_main, TASH_EXECMD_ASYNC},
@@ -140,17 +121,17 @@ static const tash_cmdlist_t tc_cmds[] = {
 #ifdef CONFIG_EXAMPLES_TESTCASE_DM_ITC
 	{"dm_itc", itc_dm_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_UTC
-	{"sysio_utc", utc_sysio_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_DRIVERS
+	{"drivers_tc", tc_drivers_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_ITC
-	{"sysio_itc", itc_sysio_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_FILESYSTEM
+	{"filesystem_tc", tc_filesystem_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_UTC
-	{"wifi_manager_utc", utc_wifi_manager_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_KERNEL
+	{"kernel_tc", tc_kernel_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_ITC
-	{"wifi_manager_itc", itc_wifi_manager_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_MPU
+	{"mpu_tc", tc_mpu_main, TASH_EXECMD_ASYNC},
 #endif
 #ifdef CONFIG_EXAMPLES_TESTCASE_MQTT_UTC
 	{"mqtt_utc", utc_mqtt_main, TASH_EXECMD_ASYNC},
@@ -158,22 +139,31 @@ static const tash_cmdlist_t tc_cmds[] = {
 #ifdef CONFIG_EXAMPLES_TESTCASE_MQTT_ITC
 	{"mqtt_itc", itc_mqtt_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_AUDIO_UTC
-	{"audio_utc", utc_audio_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_NETWORK
+	{"network_tc", tc_network_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_AUDIO_ITC
-	{"audio_itc", itc_audio_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_UTC
+	{"sysio_utc", utc_sysio_main, TASH_EXECMD_ASYNC},
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_MEDIA_UTC
-	{"media_utc", utc_media_main, TASH_EXECMD_ASYNC},
+#ifdef CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_ITC
+	{"sysio_itc", itc_sysio_main, TASH_EXECMD_ASYNC},
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_TASK_MANAGER_UTC
+	{"taskmgr_utc", utc_task_manager_main, TASH_EXECMD_ASYNC},
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_TTRACE
+	{"ttrace_tc", tc_ttrace_main, TASH_EXECMD_ASYNC},
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_UTC
+	{"wifi_manager_utc", utc_wifi_manager_main, TASH_EXECMD_ASYNC},
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_ITC
+	{"wifi_manager_itc", itc_wifi_manager_main, TASH_EXECMD_ASYNC},
 #endif
 	{NULL, NULL, 0}
 };
 #endif
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 int tc_handler(tc_op_type_t type, const char *tc_name)
 {
 	switch (type) {
@@ -231,101 +221,10 @@ int tc_main(int argc, char *argv[])
 		printf("Arastorage utc is not started, err = %d\n", pid);
 	}
 #endif
-
 #ifdef CONFIG_EXAMPLES_TESTCASE_ARASTORAGE_ITC
 	pid = task_create("arastorageitc", SCHED_PRIORITY_DEFAULT, TC_ARASTORAGE_STACK, itc_arastorage_main, argv);
 	if (pid < 0) {
 		printf("Arastorage itc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_FILESYSTEM
-	pid = task_create("fstc", SCHED_PRIORITY_DEFAULT, TC_FS_STACK, tc_filesystem_main, argv);
-	if (pid < 0) {
-		printf("FS tc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_KERNEL
-	pid = task_create("kerneltc", SCHED_PRIORITY_DEFAULT, TC_KERNEL_STACK, tc_kernel_main, argv);
-	if (pid < 0) {
-		printf("Kernel tc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_DRIVERS
-	pid = task_create("driverstc", SCHED_PRIORITY_DEFAULT, TC_DRIVERS_STACK, tc_drivers_main, argv);
-	if (pid < 0) {
-		printf("Drivers tc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_MPU
-	pid = task_create("mputc", SCHED_PRIORITY_DEFAULT, TC_MPU_STACK, tc_mpu_main, argv);
-	if (pid < 0) {
-		printf("MPU tc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_NETWORK
-	pid = task_create("nettc", SCHED_PRIORITY_DEFAULT, TC_NETWORK_STACK, tc_network_main, argv);
-	if (pid < 0) {
-		printf("Network tc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_TASK_MANAGER_UTC
-	pid = task_create("taskmgrutc", SCHED_PRIORITY_DEFAULT, TC_TASK_MANAGER_STACK, utc_task_manager_main, argv);
-	if (pid < 0) {
-		printf("Task Manager utc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_TTRACE
-	pid = task_create("ttracetc", SCHED_PRIORITY_DEFAULT, TC_TTRACE_STACK, tc_ttrace_main, argv);
-	if (pid < 0) {
-		printf("T-trace tc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_DM_UTC
-	pid = task_create("dmapi", SCHED_PRIORITY_DEFAULT, TC_DM_STACK, utc_dm_main, argv);
-	if (pid < 0) {
-		printf("DM utc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_DM_ITC
-	pid = task_create("dmapi", SCHED_PRIORITY_DEFAULT, TC_DM_STACK, itc_dm_main, argv);
-	if (pid < 0) {
-		printf("DM itc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_UTC
-	pid = task_create("sysioutc", SCHED_PRIORITY_DEFAULT, TC_SYSTEMIO_STACK, utc_sysio_main, argv);
-	if (pid < 0) {
-		printf("System IO utc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_ITC
-	pid = task_create("sysioitc", SCHED_PRIORITY_DEFAULT, TC_SYSTEMIO_STACK, itc_sysio_main, argv);
-	if (pid < 0) {
-		printf("System IO itc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_UTC
-	pid = task_create("wifimgrutc", SCHED_PRIORITY_DEFAULT, TC_WIFI_MANAGER_STACK, utc_wifi_manager_main, argv);
-	if (pid < 0) {
-		printf("Wi-Fi Manager utc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_ITC
-	pid = task_create("wifimgritc", SCHED_PRIORITY_DEFAULT, TC_WIFI_MANAGER_STACK, itc_wifi_manager_main, argv);
-	if (pid < 0) {
-		printf("Wi-Fi Manager itc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_MQTT_UTC
-	pid = task_create("mqttutc", SCHED_PRIORITY_DEFAULT, TC_MQTT_STACK, utc_mqtt_main, argv);
-	if (pid < 0) {
-		printf("MQTT utc is not started, err = %d\n", pid);
-	}
-#endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_MQTT_ITC
-	pid = task_create("mqttitc", SCHED_PRIORITY_DEFAULT, TC_MQTT_STACK, itc_mqtt_main, argv);
-	if (pid < 0) {
-		printf("MQTT itc is not started, err = %d\n", pid);
 	}
 #endif
 #ifdef CONFIG_EXAMPLES_TESTCASE_AUDIO_UTC
@@ -340,10 +239,94 @@ int tc_main(int argc, char *argv[])
 		printf("Audio itc is not started, err = %d\n", pid);
 	}
 #endif
-#ifdef CONFIG_EXAMPLES_TESTCASE_MEDIA_UTC
-	pid = task_create("mediautc", SCHED_PRIORITY_DEFAULT, TC_MEDIA_STACK, utc_media_main, argv);
+#ifdef CONFIG_EXAMPLES_TESTCASE_DM_UTC
+	pid = task_create("dmapi", SCHED_PRIORITY_DEFAULT, TC_DM_STACK, utc_dm_main, argv);
 	if (pid < 0) {
-		printf("Media utc is not started, err = %d\n", pid);
+		printf("DM utc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_DM_ITC
+	pid = task_create("dmapi", SCHED_PRIORITY_DEFAULT, TC_DM_STACK, itc_dm_main, argv);
+	if (pid < 0) {
+		printf("DM itc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_DRIVERS
+	pid = task_create("driverstc", SCHED_PRIORITY_DEFAULT, TC_DRIVERS_STACK, tc_drivers_main, argv);
+	if (pid < 0) {
+		printf("Drivers tc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_FILESYSTEM
+	pid = task_create("fstc", SCHED_PRIORITY_DEFAULT, TC_FS_STACK, tc_filesystem_main, argv);
+	if (pid < 0) {
+		printf("FS tc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_KERNEL
+	pid = task_create("kerneltc", SCHED_PRIORITY_DEFAULT, TC_KERNEL_STACK, tc_kernel_main, argv);
+	if (pid < 0) {
+		printf("Kernel tc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_MPU
+	pid = task_create("mputc", SCHED_PRIORITY_DEFAULT, TC_MPU_STACK, tc_mpu_main, argv);
+	if (pid < 0) {
+		printf("MPU tc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_MQTT_UTC
+	pid = task_create("mqttutc", SCHED_PRIORITY_DEFAULT, TC_MQTT_STACK, utc_mqtt_main, argv);
+	if (pid < 0) {
+		printf("MQTT utc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_MQTT_ITC
+	pid = task_create("mqttitc", SCHED_PRIORITY_DEFAULT, TC_MQTT_STACK, itc_mqtt_main, argv);
+	if (pid < 0) {
+		printf("MQTT itc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_NETWORK
+	pid = task_create("nettc", SCHED_PRIORITY_DEFAULT, TC_NETWORK_STACK, tc_network_main, argv);
+	if (pid < 0) {
+		printf("Network tc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_UTC
+	pid = task_create("sysioutc", SCHED_PRIORITY_DEFAULT, TC_SYSTEMIO_STACK, utc_sysio_main, argv);
+	if (pid < 0) {
+		printf("System IO utc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_SYSTEMIO_ITC
+	pid = task_create("sysioitc", SCHED_PRIORITY_DEFAULT, TC_SYSTEMIO_STACK, itc_sysio_main, argv);
+	if (pid < 0) {
+		printf("System IO itc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_TASK_MANAGER_UTC
+	pid = task_create("taskmgrutc", SCHED_PRIORITY_DEFAULT, TC_TASK_MANAGER_STACK, utc_task_manager_main, argv);
+	if (pid < 0) {
+		printf("Task Manager utc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_TTRACE
+	pid = task_create("ttracetc", SCHED_PRIORITY_DEFAULT, TC_TTRACE_STACK, tc_ttrace_main, argv);
+	if (pid < 0) {
+		printf("T-trace tc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_UTC
+	pid = task_create("wifimgrutc", SCHED_PRIORITY_DEFAULT, TC_WIFI_MANAGER_STACK, utc_wifi_manager_main, argv);
+	if (pid < 0) {
+		printf("Wi-Fi Manager utc is not started, err = %d\n", pid);
+	}
+#endif
+#ifdef CONFIG_EXAMPLES_TESTCASE_WIFI_MANAGER_ITC
+	pid = task_create("wifimgritc", SCHED_PRIORITY_DEFAULT, TC_WIFI_MANAGER_STACK, itc_wifi_manager_main, argv);
+	if (pid < 0) {
+		printf("Wi-Fi Manager itc is not started, err = %d\n", pid);
 	}
 #endif
 
@@ -352,7 +335,3 @@ int tc_main(int argc, char *argv[])
 
 	return 0;
 }
-
-#ifdef __cplusplus
-}
-#endif
