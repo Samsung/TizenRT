49e33c068fa271cd71c95a4af689e9709a5dc83d
Ubuntu 16.04 <ty1115.ha@samsung.com>
2018-05-28 17:46:03 -0700

    framework/task_manager: Add broadcast functionality Add broadcast functionality to task manager The broadcast function can be used for broadcasting the specific pre-defined messages by using the task_manager_broadcast(). Pre-defined messages for broadcast are defined at the framework/src/task_manager/task_manager.h In order to receive a broadcast message, user must register a handler to handle the broadcast message using task_manager_set_broadcast_handler() in advance. When you set a handler for receiving broadcast messages, you can set a msg_mask to select message to receive (e.g., msg_mask = (WIFI_ON | WIFI_OFF)).

--- framework/src/task_manager/task_manager_internal.h
+++ framework/src/task_manager/task_manager_internal.h
@@ -20,20 +20,21 @@
 #define __TASK_MANAGER_INTERNAL_H__
 
 /* Command Types */
-#define TASKMGT_REGISTER_TASK        0
-#define TASKMGT_REGISTER_THREAD      1
-#define TASKMGT_START                2
-#define TASKMGT_TERMINATE            3
-#define TASKMGT_RESTART              4
-#define TASKMGT_PAUSE                5
-#define TASKMGT_RESUME               6
-#define TASKMGT_SCAN                 7
-#define TASKMGT_UNICAST              8
-#define TASKMGT_BROADCAST            9
-#define TASKMGT_SCAN_NAME            10
-#define TASKMGT_SCAN_HANDLE          11
-#define TASKMGT_SCAN_GROUP           12
-#define TASKMGT_UNREGISTER_TASK      13
+#define TASKMGT_REGISTER_TASK            0
+#define TASKMGT_REGISTER_THREAD          1
+#define TASKMGT_START                    2
+#define TASKMGT_TERMINATE                3
+#define TASKMGT_RESTART                  4
+#define TASKMGT_PAUSE                    5
+#define TASKMGT_RESUME                   6
+#define TASKMGT_SCAN                     7
+#define TASKMGT_UNICAST                  8
+#define TASKMGT_BROADCAST                9
+#define TASKMGT_SCAN_NAME                10
+#define TASKMGT_SCAN_HANDLE              11
+#define TASKMGT_SCAN_GROUP               12
+#define TASKMGT_UNREGISTER_TASK          13
+#define TASKMGT_SET_BROADCAST_HANDLER    14
 
 /* Message Queue Values */
 #define TM_MQ_PRIO   50
