9b36e2e34ac42867009726d257e1185b1d016061
sunghan-chang <sh924.chang@samsung.com>
2018-09-07 05:27:24 -0700

    build: add board specifc operation to make a board-specific binary on build time
    The board-specific binary was made at download step.
    But it is not a download step and some board uses toolchain to do it
    like strip command in Cypress.
    It is not working in Docker environment.
    Let's call board-specific operation after making TizenRT binary
    and define it at each board config file name Make.defs.
    
    Signed-off-by: sunghan-chang <sh924.chang@samsung.com>

--- build/configs/artik05x/artik05x_make_bin.sh
+++ build/configs/artik05x/artik05x_make_bin.sh
@@ -17,8 +17,8 @@
 #
 ###########################################################################
 #
-# File          : artik05x_cmn.sh
-# Description   : Common file for other *sh in directory
+# File          : artik05x_make_bin.sh
+# Description   : make board-specific binary for ARTIK boards like signing, adding checksum
 
 set -e
 
@@ -28,22 +28,42 @@ BUILD_DIR_PATH=${OS_DIR_PATH}/../build
 CONFIGS_DIR_PATH=${BUILD_DIR_PATH}/configs
 OUTPUT_BINARY_PATH=${BUILD_DIR_PATH}/output/bin
 ARTIK05X_DIR_PATH=${CONFIGS_DIR_PATH}/artik05x
-TIZENRT_BIN=$OUTPUT_BINARY_PATH/tinyara_head.bin
 CODESIGNER_TOOL=artik05x_AppCodesigner
-CODESIGNER_PATH=${ARTIK05X_DIR_PATH}/tools/codesigner
 CODESIGNER_DIR_PATH=${ARTIK05X_DIR_PATH}/tools/codesigner
-OPENOCD_DIR_PATH=${BUILD_DIR_PATH}/tools/openocd
+
+while test $# -gt 0; do
+	case $1 in
+		--bin)
+			shift
+			BIN_NAME=$1
+			;;
+		--ext)
+			shift
+			EXT_NAME=$1
+			;;
+		--secure)
+			SECURE_BOARD=y
+			;;
+	esac
+	shift
+done
+
+
+TIZENRT_BIN=${BIN_NAME}.${EXT_NAME}
+SAMSUNG_NS2_BIN=${BIN_NAME}_head.${EXT_NAME}
+
+if [ "${TIZENRT_BIN}" == "." ]; then
+	echo "Error!! Fail to make a binary for ARTIK. Please check --bin and --ext options"
+	exit 1
+fi
 
 if [[ $OSTYPE == "darwin"* ]]; then
-	OPENOCD_BIN_PATH=${OPENOCD_DIR_PATH}/macos
 	CODESIGNER_PATH=${CODESIGNER_DIR_PATH}/macos
 elif [[ $OSTYPE == "linux"* ]]; then
 	SYSTEM_TYPE=`getconf LONG_BIT`
 	if [ "$SYSTEM_TYPE" = "64" ]; then
-		OPENOCD_BIN_PATH=${OPENOCD_DIR_PATH}/linux64
 		CODESIGNER_PATH=${CODESIGNER_DIR_PATH}/linux64
 	else
-		OPENOCD_BIN_PATH=${OPENOCD_DIR_PATH}/linux32
 		CODESIGNER_PATH=${CODESIGNER_DIR_PATH}/linux32
 	fi
 else
@@ -51,7 +71,10 @@ else
     exit 1
 fi
 
-OPENOCD=${OPENOCD_BIN_PATH}/openocd
+add_checksum() {
+	echo "ATTACHNS2: ${TIZENRT_BIN} --> ${SAMSUNG_NS2_BIN}"
+	${OS_DIR_PATH}/tools/s5jchksum.py ${OUTPUT_BINARY_PATH}/${TIZENRT_BIN} ${OUTPUT_BINARY_PATH}/${SAMSUNG_NS2_BIN}
+}
 
 signing() {
 	if [ ! -f ${CODESIGNER_PATH}/${CODESIGNER_TOOL} ]; then
@@ -59,8 +82,10 @@ signing() {
 		exit 1
 	fi
 
-	${CODESIGNER_PATH}/${CODESIGNER_TOOL} ${CODESIGNER_DIR_PATH}/rsa_private.key $TIZENRT_BIN
-	TIZENRT_BIN=${TIZENRT_BIN}-signed
+	${CODESIGNER_PATH}/${CODESIGNER_TOOL} ${CODESIGNER_DIR_PATH}/rsa_private.key ${OUTPUT_BINARY_PATH}/${SAMSUNG_NS2_BIN}
 }
 
-
+add_checksum
+if [ "${SECURE_BOARD}" == "y" ]; then
+	signing
+fi
