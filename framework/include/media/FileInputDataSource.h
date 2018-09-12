0b42c9b235ade5ae5def9671075e84346765f7a0
junmin-kim <junmindd.kim@samsung.com>
2018-09-12 15:54:11 +0900

    media: Add doxygen comment of read API
    Add comment of FileInputDataSource::read

--- framework/include/media/FileInputDataSource.h
+++ framework/include/media/FileInputDataSource.h
@@ -95,7 +95,14 @@ public:
 	 * @since TizenRT v2.0 PRE
 	 */
 	bool close() override;
-
+	/**
+	 * @brief Read the file
+	 * @details @b #include <media/FileInputDataSource.h>
+	 * param[in] buf  pointer to a buffer
+	 * param[in] size size to be read
+	 * @return The number of elements read
+	 * @since TizenRT v2.0 PRE
+	 */
 	ssize_t read(unsigned char *buf, size_t size) override;
 
 private:
