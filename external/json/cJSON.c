9204f9b964116ceaa8468fd52158d7685a29e147
kmjhej <43839938+kmjhej@users.noreply.github.com>
2018-10-04 08:57:43 +0000

    Update cJSON.c

--- external/json/cJSON.c
+++ external/json/cJSON.c
@@ -89,20 +89,17 @@ CJSON_PUBLIC(const char*) cJSON_Version(void)
 /* Case insensitive string comparison, doesn't consider two NULL pointers equal though */
 static int case_insensitive_strcmp(const unsigned char *string1, const unsigned char *string2)
 {
-    if ((string1 == NULL) || (string2 == NULL))
-    {
+    if ((string1 == NULL) || (string2 == NULL)) {
         return 1;
     }
 
-    if (string1 == string2)
-    {
+    if (string1 == string2) {
         return 0;
     }
 
     for(; tolower(*string1) == tolower(*string2); (void)string1++, string2++)
     {
-        if (*string1 == '\0')
-        {
+        if (*string1 == '\0') {
             return 0;
         }
     }
