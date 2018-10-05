503e58964fdc459f1e6ada41b60c97886c41fb41
Youngdae Oh <yd.oh@samsung.com>
2018-10-05 16:03:57 +0900

    securityapp: reduce security_sdk_set_compute_dhm_params stack size
    When security_sdk_set_compute_dhm_params is running, stack memory is
    damaged, because it uses lots of stack.
    
    Change-Id: I76ea1275648a7557d8b251d4e58d343794f37bcd
    Signed-off-by: Youngdae Oh <yd.oh@samsung.com>

--- apps/examples/security_sdk/src/security_sdk_aes.c
+++ apps/examples/security_sdk/src/security_sdk_aes.c
@@ -69,7 +69,7 @@ int security_sdk_aes_encrypt_decrypt(void)
 									 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
 									 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
 								   };
-	unsigned char iv[8] = { 0 };
+	unsigned char iv[16] = { 0 };
 	char key_name[16] = { 0 };
 	unsigned char *out = NULL;
 	unsigned int outlen = 0;
