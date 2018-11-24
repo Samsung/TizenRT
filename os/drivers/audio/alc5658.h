46c7dca7e173fc9438b8b471299298fb227c6366
Woo, Dong Keun <35188271+wodonggun@users.noreply.github.com>
2018-11-24 13:27:22 +0900

    os/drivers/audio/alc5658.h : Modify a typo
    Line 104 IN -> In
    Line 119 IN -> In

--- os/drivers/audio/alc5658.h
+++ os/drivers/audio/alc5658.h
@@ -99,7 +99,7 @@ struct alc5658_dev_s {
 	uint16_t balance;			/* Current balance level (b16) */
 #endif							/* CONFIG_AUDIO_EXCLUDE_BALANCE */
 	uint16_t volume;			/* Current volume level {0..31} */
-	uint16_t gain;				/* IN port gain */
+	uint16_t gain;				/* In port gain */
 	uint8_t nchannels;			/* Number of channels (1 or 2) */
 	uint8_t bpsamp;				/* Bits per sample (8 or 16) */
 	volatile uint8_t inflight;	/* Number of audio buffers in-flight */
@@ -114,7 +114,7 @@ struct alc5658_dev_s {
 #endif
 	bool reserved;				/* True: Device is reserved */
 	volatile int result;		/* The result of the last transfer */
-	bool inout;					/* True: IN device */
+	bool inout;					/* True: In device */
 };
 
 /****************************************************************************
