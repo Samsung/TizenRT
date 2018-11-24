24ecdb09214cd6f58a1e7f5ad2b815a599174974
Woo, Dong Keun <35188271+wodonggun@users.noreply.github.com>
2018-11-24 13:34:50 +0900

    os/drivers/audio/alc5658.h : Line Alignment
    Line 84 ~ 116 space -> Tab

--- os/drivers/audio/alc5658.h
+++ os/drivers/audio/alc5658.h
@@ -81,14 +81,14 @@ struct alc5658_dev_s {
 	 * "half" that is referred to as "lower".
 	 */
 
-	struct audio_lowerhalf_s dev;	/* ALC5658 audio lower half (this device) */
+	struct audio_lowerhalf_s dev;		/* ALC5658 audio lower half (this device) */
 
 	/* Our specific driver data goes here */
 
 	FAR struct alc5658_lower_s *lower;	/* Pointer to the board lower functions */
-	FAR struct i2c_dev_s *i2c;	/* I2C driver to use */
-	FAR struct i2s_dev_s *i2s;	/* I2S driver to use */
-	struct sq_queue_s pendq;	/* Queue of pending buffers to be sent */
+	FAR struct i2c_dev_s *i2c;		/* I2C driver to use */
+	FAR struct i2s_dev_s *i2s;		/* I2S driver to use */
+	struct sq_queue_s pendq;		/* Queue of pending buffers to be sent */
 	sem_t devsem;				/* Protection for both pendq & dev */
 
 #ifdef ALC5658_USE_FFLOCK_INT
@@ -97,24 +97,24 @@ struct alc5658_dev_s {
 	uint16_t samprate;			/* Configured samprate (samples/sec) */
 #ifndef CONFIG_AUDIO_EXCLUDE_BALANCE
 	uint16_t balance;			/* Current balance level (b16) */
-#endif							/* CONFIG_AUDIO_EXCLUDE_BALANCE */
+#endif						/* CONFIG_AUDIO_EXCLUDE_BALANCE */
 	uint16_t volume;			/* Current volume level {0..31} */
 	uint16_t gain;				/* In port gain */
-	uint8_t nchannels;			/* Number of channels (1 or 2) */
+	uint8_t nchannels;			/* Numbe	r of channels (1 or 2) */
 	uint8_t bpsamp;				/* Bits per sample (8 or 16) */
-	volatile uint8_t inflight;	/* Number of audio buffers in-flight */
+	volatile uint8_t inflight;		/* Number of audio buffers in-flight */
 #ifdef ALC5658_USE_FFLOCK_INT
-	volatile bool locked;		/* FLL is locked */
+	volatile bool locked;			/* FLL is locked */
 #endif
 	bool running;				/* True: Worker thread is running */
 	bool paused;				/* True: Playing is paused */
-	bool mute;					/* True: Output is muted */
+	bool mute;				/* True: Output is muted */
 #ifndef CONFIG_AUDIO_EXCLUDE_STOP
 	bool terminating;			/* True: Stop requested */
 #endif
 	bool reserved;				/* True: Device is reserved */
-	volatile int result;		/* The result of the last transfer */
-	bool inout;					/* True: In device */
+	volatile int result;			/* The result of the last transfer */
+	bool inout;				/* True: In device */
 };
 
 /****************************************************************************
