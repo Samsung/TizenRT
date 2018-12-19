3a70f0bf829993494c72475a56b3fb8d70fac17f
shbyeon <sh.byeon@samsung.com>
2018-12-19 13:30:14 +0900

    external/mbedtls: Refactor SEE API definitions
    - Refactor SEE API definitions

--- framework/src/st_things/things_stack/framework/things_sss_manager.c
+++ framework/src/st_things/things_stack/framework/things_sss_manager.c
@@ -169,7 +169,7 @@ static int things_set_cert_chains(void)
 {
 	int ret = -1;
 	uint8_t *buf = NULL;
-	uint32_t buflen = SEE_BUF_MAX_SIZE;
+	uint32_t buflen = SEE_MAX_BUF_SIZE;
 
 	buf = (uint8_t *)things_malloc(buflen);
 	if (buf == NULL) {
