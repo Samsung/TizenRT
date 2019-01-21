5e63315a3a7364f3d45a709e43bb8b4b89791a10
kang <pilseob.kang@samsung.com>
2019-01-21 15:46:21 +0900

    external/iotivity: change build procedure
    TizenRT uses scons build procedure in Iotivity before.
    Because following their build procedure is better to apply Iotivity
    patches as soon as possible.
    However there are few changes in Iotivity now while new chipsets ported
    to TizenRT are requested frequently.
    There should be some modifications to build Iotivity in new chipset
    environment.
    This patch is that Iotivity build procedure uses TizenRT build.
    So changes in Iotivity build script are not required even though new
    chipsets are ported.

---
+++ external/iotivity/iotivity_1.2-rel/resource/c_common/iotivity_config.h
@@ -0,0 +1,55 @@
+
+/* ****************************************************************************
+ * iotivity_config.h - IoTivity platform-specific configuration header.
+ *
+ * Auto-generated code for the tizenrt armv7-r platform.
+ *
+ * Generated at 2019-01-18 04:41:16.895168
+ *
+ *************************************************************************** */
+
+#ifndef IOTIVITY_CONFIG_H__
+#define IOTIVITY_CONFIG_H__
+
+#include <tinyara/config.h>
+
+#define HAVE_ARPA_INET_H 1
+
+#define HAVE_FCNTL_H 1
+
+#define HAVE_MEMORY_H 1
+
+#define HAVE_NETDB_H 1
+
+#define HAVE_NETINET_IN_H 1
+
+#define HAVE_PTHREAD_H 1
+
+#define HAVE_STDLIB_H 1
+
+#define HAVE_STRING_H 1
+
+//#define HAVE_STRINGS_H 1
+
+#define HAVE_SYS_SOCKET_H 1
+
+#define HAVE_SYS_STAT_H 1
+
+#define HAVE_SYS_TIME_H 1
+
+#define HAVE_SYS_TYPES_H 1
+
+#define HAVE_SYSLOG_H 1
+
+#define HAVE_TIME_H 1
+
+#define HAVE_UNISTD_H 1
+
+#define HAVE_UUID_UUID_H 1
+
+
+
+#include "platform_features.h"
+
+#endif // IOTIVITY_CONFIG_H__
+
