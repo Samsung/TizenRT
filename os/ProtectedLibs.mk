df77490e5d48dc1dca2cf093670c4e0d81098e6f
kang <pilseob.kang@samsung.com>
2019-04-09 13:55:06 +0900

    Merge branch 'master' of https://github.com/pillip8282/TizenRT into HEAD

--- os/ProtectedLibs.mk
+++ os/ProtectedLibs.mk
@@ -70,6 +70,7 @@ TINYARALIBS += $(LIBRARIES_DIR)$(DELIM)libstubs$(LIBEXT) $(LIBRARIES_DIR)$(DELIM
 TINYARALIBS += $(LIBRARIES_DIR)$(DELIM)libkmm$(LIBEXT) $(LIBRARIES_DIR)$(DELIM)libkarch$(LIBEXT)
 USERLIBS  += $(LIBRARIES_DIR)$(DELIM)libproxies$(LIBEXT) $(LIBRARIES_DIR)$(DELIM)libuc$(LIBEXT)
 USERLIBS  += $(LIBRARIES_DIR)$(DELIM)libumm$(LIBEXT) $(LIBRARIES_DIR)$(DELIM)libuarch$(LIBEXT)
+USERLIBS += $(LIBRARIES_DIR)$(DELIM)libc$(LIBEXT)
 
 # Add libraries for C++ support.  CXX, CXXFLAGS, and COMPILEXX must
 # be defined in Make.defs for this to work!
@@ -133,6 +134,9 @@ endif
 ifneq ($(CONFIG_BINFMT_DISABLE),y)
 TINYARALIBS += $(LIBRARIES_DIR)$(DELIM)libbinfmt$(LIBEXT)
 endif
+
+# Add library for Framework
+TINYARALIBS += $(LIBRARIES_DIR)$(DELIM)libframework$(LIBEXT)
 
 # Add libraries for iotjs support
 
