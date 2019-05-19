4dbbbe5ae500d106cd4e8b6992b709f85e319a93
Amandeep Chauhan <a.chauhan@samsung.com>
2019-05-20 00:25:58 +0530

    apps/exmaples/elf: Add support for creating compressed binary
    - modifies Makefiles for micomapp and wifiapp to add support for
      compressing created elf binaries
    - modifies mkbinheader.py file to handle creation for both
      compressed/uncompressed binaries
    
    Signed-off-by: Amandeep Chauhan <a.chauhan@samsung.com>

--- os/tools/mkbinheader.py
+++ os/tools/mkbinheader.py
@@ -23,19 +23,19 @@ import struct
 #
 # header information :
 #
-# total header size is 60bytes.
-# +-------------------------------------------------------------------------
-# | Header size | Binary type | Binary priority | Binary size | Binary name
-# |   (2bytes)  |   (1byte)   |     (1byte)     |  (4bytes)   |  (16bytes)
-# +-------------------------------------------------------------------------
-# --------------------------------------------------------------------------
-# | Binary version | Binary ram size | Binary stack size |  Kernel version |
-# |   (16bytes)    |    (4bytes)     |     (4bytes)      |     (8bytes)    |
-# --------------------------------------------------------------------------
-# --------------+
-#  Jump address |
-#    (4bytes)   |
-# --------------+
+# total header size is 61bytes.
+# +--------------------------------------------------------------------------
+# | Header size | Binary type | Compression  | Binary priority | Binary size|
+# |   (2bytes)  |   (1byte)   |   (1byte)    |     (1byte)     |  (4bytes)  |
+# +--------------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# | Binary name | Binary version | Binary ram size | Binary stack size |
+# |  (16bytes)  |   (16bytes)    |    (4bytes)     |     (4bytes)      |
+# ----------------------------------------------------------------------
+# ------------------------------ +
+# | Kernel Version |Jump address |
+# |  (8bytes)      |  (4bytes)   |
+# -------------------------------+
 #
 # parameter information :
 #
@@ -47,6 +47,8 @@ import struct
 # argv[6] is a dynamic ram size required to run this binary.
 # argv[7] is binary stack size.
 # argv[8] is binary priority.
+# argv[9] is compression type
+# argv[10] is block size for compression
 #
 ############################################################################
 
@@ -58,12 +60,18 @@ binary_ver = sys.argv[5]
 dynamic_ram_size = sys.argv[6]
 binary_stack_size = sys.argv[7]
 binary_priority = sys.argv[8]
+comp_enabled = sys.argv[9]
+comp_blk_size = sys.argv[10]
 
 # This path is only for dbuild.
 elf_path_for_bin_type = 'root/tizenrt/build/output/bin/tinyara'
 
+# Path to directory of this file
+mkbinheader_path = os.path.dirname(__file__)
+
 SIZE_OF_HEADERSIZE = 2
 SIZE_OF_BINTYPE = 1
+SIZE_OF_COMFLAG = 1
 SIZE_OF_BINPRIORITY = 1
 SIZE_OF_BINSIZE = 4
 SIZE_OF_BINNAME = 16
@@ -73,10 +81,14 @@ SIZE_OF_BINSTACKSIZE = 4
 SIZE_OF_KERNELVER = 8
 SIZE_OF_JUMPADDR = 4
 
-header_size = SIZE_OF_HEADERSIZE + SIZE_OF_BINTYPE + SIZE_OF_BINPRIORITY + SIZE_OF_BINSIZE + SIZE_OF_BINNAME + SIZE_OF_BINVER + SIZE_OF_BINRAMSIZE + SIZE_OF_BINSTACKSIZE + SIZE_OF_KERNELVER + SIZE_OF_JUMPADDR
+header_size = SIZE_OF_HEADERSIZE + SIZE_OF_BINTYPE + SIZE_OF_COMFLAG + SIZE_OF_BINPRIORITY + SIZE_OF_BINSIZE + SIZE_OF_BINNAME + SIZE_OF_BINVER + SIZE_OF_BINRAMSIZE + SIZE_OF_BINSTACKSIZE + SIZE_OF_KERNELVER + SIZE_OF_JUMPADDR
 
 ELF = 1
 BIN = 2
+
+COMP_NONE = 0
+COMP_LZMA = 1
+COMP_MAX = COMP_LZMA
 
 # In size command on linux, 4th value is the summation of text, data and bss.
 # We will use this value for elf.
@@ -128,10 +140,32 @@ with open(file_path, 'rb') as fp:
 
     binary_ram_size = int(static_ram_size) + int(dynamic_ram_size)
 
+    # based on comp_enabled, check if we need to compress binary.
+    # If yes, assign to bin_comp value for compression algorithm to use.
+    # Else, assign 0 to bin_comp to represent no compression
+    if 0 < int(comp_enabled) <= COMP_MAX :
+        bin_comp = int(comp_enabled)
+    else :
+        bin_comp = 0
+
+    # Compress data according to Compression Algorithm represented by bin_comp
+    # Run mkcompressimg tool with provided options. Read output compressed file into data.
+    if bin_comp > COMP_NONE :
+        fp_tmp = open("tmp", 'wb+')
+        fp_tmp.write(data)
+        fp_tmp.close()
+        os.system(mkbinheader_path + '/compression/mkcompressimg ' + comp_blk_size + ' ' + comp_enabled + ' tmp' + ' tmp_comp')
+        fp_tmp = open("tmp_comp", 'rb')
+        data = fp_tmp.read()
+        file_size = fp_tmp.tell()
+        fp_tmp.close()
+        os.system('rm tmp tmp_comp')
+
     fp = open(file_path, 'wb')
 
     fp.write(struct.pack('H', header_size))
     fp.write(struct.pack('B', bin_type))
+    fp.write(struct.pack('B', bin_comp))
     fp.write(struct.pack('B', binary_priority))
     fp.write(struct.pack('I', file_size))
     fp.write('{:{}{}.{}}'.format(binary_name, '<', SIZE_OF_BINNAME, SIZE_OF_BINNAME - 1).replace(' ','\0'))
