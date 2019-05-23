apache/nuttx

Fix an error when a task with open message queue descriptors is killed via task_delete().  Noted by Anton Gropyanov.

fs/mqueue/mq_close.c
@@ -44,34 +44,70 @@
 #include <assert.h>
 
 #include <nuttx/kmalloc.h>
+#include <nuttx/sched.h>
 #include <nuttx/mqueue.h>
 
 #include "inode/inode.h"
 #include "mqueue/mqueue.h"
 
 /****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
-
-/****************************************************************************
- * Private Type Declarations
- ****************************************************************************/
-
-/****************************************************************************
- * Public Data
- ****************************************************************************/
-
-/****************************************************************************
- * Private Data
- ****************************************************************************/
-
-/****************************************************************************
- * Private Functions
- ****************************************************************************/
-
-/****************************************************************************
  * Public Functions
  ****************************************************************************/
+
+/****************************************************************************
+ * Name: mq_close_group
+ *
+ * Description:
+ *   This function is used to indicate that all threads in the group are
+ *   finished with the specified message queue mqdes.  The mq_close_group()
+ *   deallocates any system resources allocated by the system for use by
+ *   this task for its message queue.
+ *
+ * Parameters:
+ *   mqdes - Message queue descriptor.
+ *   group - Group that has the open descriptor.
+ *
+ * Return Value:
+ *   0 (OK) if the message queue is closed successfully,
+ *   otherwise, -1 (ERROR).
+ *
+ ****************************************************************************/
+
+int mq_close_group(mqd_t mqdes, FAR struct task_group_s *group)
+{
+  FAR struct mqueue_inode_s *msgq;
+  FAR struct inode *inode;
+
+  DEBUGASSERT(mqdes != NULL && group != NULL);
+
+  /* Verify the inputs */
+
+   if (mqdes)
+     {
+       sched_lock();
+
+       /* Find the message queue associated with the message descriptor */
+
+       msgq = mqdes->msgq;
+       DEBUGASSERT(msgq && msgq->inode);
+
+       /* Close/free the message descriptor */
+
+       mq_desclose_group(mqdes, group);
+
+       /* Get the inode from the message queue structure */
+
+       inode = msgq->inode;
+       DEBUGASSERT(inode->u.i_mqueue == msgq);
+
+       /* Decrement the reference count on the inode, possibly freeing it */
+
+       mq_inode_release(inode);
+       sched_unlock();
+     }
+
+  return OK;
+}
 
 /****************************************************************************
  * Name: mq_close
@@ -103,37 +139,23 @@
 
 int mq_close(mqd_t mqdes)
 {
-  FAR struct mqueue_inode_s *msgq;
-  FAR struct inode *inode;
+  FAR struct tcb_s *rtcb = (FAR struct tcb_s *)sched_self();
+  int ret;
 
-  /* Verify the inputs */
+  /* Lock the scheduler to prevent any asynchrounous task delete operation
+   * (unlikely).
+   */
 
-   if (mqdes)
-     {
-       sched_lock();
+  sched_lock();
 
-       /* Find the message queue associated with the message descriptor */
+  rtcb = (FAR struct tcb_s *)sched_self();
+  DEBUGASSERT(mqdes != NULL && rtcb != NULL && rtcb->group != NULL);
 
-       msgq = mqdes->msgq;
-       DEBUGASSERT(msgq && msgq->inode);
-
-       /* Close/free the message descriptor */
-
-       mq_desclose(mqdes);
-
-       /* Get the inode from the message queue structure */
-
-       inode = msgq->inode;
-       DEBUGASSERT(inode->u.i_mqueue == msgq);
-
-       /* Decrement the reference count on the inode, possibly freeing it */
-
-       mq_inode_release(inode);
-       sched_unlock();
-     }
-
-  return OK;
+  ret = mq_close_group(mqdes, rtcb->group);
+  sched_unlock();
+  return ret;
 }
+
 
 /****************************************************************************
  * Name: mq_inode_release

sched/mqueue/mq_desclose.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  *  sched/mqueue/mq_desclose.c
  *
- *   Copyright (C) 2007, 2009, 2013-2015 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007, 2009, 2013-2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -45,6 +45,7 @@
 #include <assert.h>
 #include <queue.h>
 
+#include <nuttx/sched.h>
 #include <nuttx/mqueue.h>
 
 #include "mqueue/mqueue.h"
@@ -71,7 +72,7 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: mq_close
+ * Name: mq_desclose_group
  *
  * Description:
  *   This function performs the portion of the mq_close operation related
@@ -79,6 +80,7 @@
  *
  * Parameters:
  *   mqdes - Message queue descriptor.
+ *   group - Group that has the open descriptor.
  *
  * Return Value:
  *   None.
@@ -88,13 +90,11 @@
  *
  ****************************************************************************/
 
-void mq_desclose(mqd_t mqdes)
+void mq_desclose_group(mqd_t mqdes, FAR struct task_group_s *group)
 {
-  FAR struct tcb_s *rtcb = (FAR struct tcb_s *)sched_self();
-  FAR struct task_group_s *group = rtcb->group;
   FAR struct mqueue_inode_s *msgq;
 
-  DEBUGASSERT(mqdes && group);
+  DEBUGASSERT(mqdes != NULL && group != NULL);
 
   /* Remove the message descriptor from the current task's list of message
    * descriptors.
@@ -123,4 +123,3 @@ void mq_desclose(mqd_t mqdes)
 
   mq_desfree(mqdes);
 }
-

sched/mqueue/mq_descreate.c
@@ -1,7 +1,7 @@
 /****************************************************************************
  *  sched/mqueue/mq_descreate.c
  *
- *   Copyright (C) 2007-2009, 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007-2009, 2013, 2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -50,6 +50,7 @@
 
 #include <nuttx/arch.h>
 #include <nuttx/kmalloc.h>
+#include <nuttx/sched.h>
 #include <nuttx/mqueue.h>
 
 #include "signal/signal.h"

Samsung/TizenRT

Conflicts:
	fs/mqueue/mq_close.c
	fs/mqueue/mq_open.c
	fs/mqueue/mq_unlink.c
	include/nuttx/mqueue.h
	sched/mqueue/mq_desclose.c
	sched/mqueue/mq_descreate.c
	sched/mqueue/mq_release.c
	sched/mqueue/mqueue.h
	sched/task/task_exithook.c

os/fs/mqueue/mq_close.c
@@ -61,34 +61,69 @@
 #include <assert.h>
 
 #include <tinyara/kmalloc.h>
+#include <tinyara/sched.h>
 #include <tinyara/mqueue.h>
 
 #include "inode/inode.h"
 #include "mqueue/mqueue.h"
 
 /****************************************************************************
- * Pre-processor Definitions
- ****************************************************************************/
-
-/****************************************************************************
- * Private Type Declarations
- ****************************************************************************/
-
-/****************************************************************************
- * Public Variables
- ****************************************************************************/
-
-/****************************************************************************
- * Private Variables
- ****************************************************************************/
-
-/****************************************************************************
- * Private Functions
- ****************************************************************************/
-
-/****************************************************************************
  * Public Functions
  ****************************************************************************/
+
+/****************************************************************************
+ * Name: mq_close_group
+ *
+ * Description:
+ *   This function is used to indicate that all threads in the group are
+ *   finished with the specified message queue mqdes.  The mq_close_group()
+ *   deallocates any system resources allocated by the system for use by
+ *   this task for its message queue.
+ *
+ * Parameters:
+ *   mqdes - Message queue descriptor.
+ *   group - Group that has the open descriptor.
+ *
+ * Return Value:
+ *   0 (OK) if the message queue is closed successfully,
+ *   otherwise, -1 (ERROR).
+ *
+ ****************************************************************************/
+
+int mq_close_group(mqd_t mqdes, FAR struct task_group_s *group)
+{
+	FAR struct mqueue_inode_s *msgq;
+	FAR struct inode *inode;
+
+	DEBUGASSERT(mqdes != NULL && group != NULL);
+
+	/* Verify the inputs */
+
+	if (mqdes) {
+		sched_lock();
+
+		/* Find the message queue associated with the message descriptor */
+
+		msgq = mqdes->msgq;
+		DEBUGASSERT(msgq && msgq->inode);
+
+		/* Close/free the message descriptor */
+
+		mq_desclose_group(mqdes, group);
+
+		/* Get the inode from the message queue structure */
+
+		inode = msgq->inode;
+		DEBUGASSERT(inode->u.i_mqueue == msgq);
+
+		/* Decrement the reference count on the inode, possibly freeing it */
+
+		mq_inode_release(inode);
+		sched_unlock();
+	}
+
+	return OK;
+}
 
 /****************************************************************************
  * Name: mq_close
@@ -120,39 +155,26 @@
 
 int mq_close(mqd_t mqdes)
 {
-	FAR struct mqueue_inode_s *msgq;
-	FAR struct inode *inode;
+	FAR struct tcb_s *rtcb = (FAR struct tcb_s *)sched_self();
+	int ret;
 
-	/* Verify the inputs */
+	/* Lock the scheduler to prevent any asynchrounous task delete operation
+	* (unlikely).
+	*/
 
-	if (mqdes) {
-		sched_lock();
+	sched_lock();
 
-		/* Find the message queue associated with the message descriptor */
+	rtcb = (FAR struct tcb_s *)sched_self();
+	DEBUGASSERT(mqdes != NULL && rtcb != NULL && rtcb->group != NULL);
 
-		msgq = mqdes->msgq;
-		DEBUGASSERT(msgq && msgq->inode);
+	ret = mq_close_group(mqdes, rtcb->group);
+	sched_unlock();
 
-		/* Close/free the message descriptor */
-
-		mq_desclose(mqdes);
-
-		/* Get the inode from the message queue structure */
-
-		inode = msgq->inode;
-		DEBUGASSERT(inode->u.i_mqueue == msgq);
-
-		/* Decrement the reference count on the inode, possibly freeing it */
-
-		mq_inode_release(inode);
-		sched_unlock();
-	}
-
-	return OK;
+	return ret;
 }
 
 /****************************************************************************
- * Name: mq_close
+ * Name: mq_inode_release
  *
  * Description:
  *   Release a reference count on a message queue inode.

os/kernel/mqueue/mq_desclose.c
@@ -18,7 +18,7 @@
 /****************************************************************************
  *  kernel/mqueue/mq_desclose.c
  *
- *   Copyright (C) 2007, 2009, 2013-2014 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007, 2009, 2013-2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -61,6 +61,7 @@
 #include <assert.h>
 #include <queue.h>
 
+#include <tinyara/sched.h>
 #include <tinyara/mqueue.h>
 
 #include "mqueue/mqueue.h"
@@ -103,7 +104,7 @@
  ****************************************************************************/
 
 /****************************************************************************
- * Name: mq_close
+ * Name: mq_desclose_group
  *
  * Description:
  *   This function performs the portion of the mq_close operation related
@@ -111,6 +112,7 @@
  *
  * Parameters:
  *   mqdes - Message queue descriptor.
+ *   group - Group that has the open descriptor.
  *
  * Return Value:
  *   None.
@@ -120,13 +122,11 @@
  *
  ****************************************************************************/
 
-void mq_desclose(mqd_t mqdes)
+void mq_desclose_group(mqd_t mqdes, FAR struct task_group_s *group)
 {
-	FAR struct tcb_s *rtcb = (FAR struct tcb_s *)sched_self();
-	FAR struct task_group_s *group = rtcb->group;
 	FAR struct mqueue_inode_s *msgq;
 
-	DEBUGASSERT(mqdes && group);
+	DEBUGASSERT(mqdes != NULL && group != NULL);
 
 	/* Remove the message descriptor from the current task's list of message
 	 * descriptors.

os/kernel/mqueue/mq_descreate.c
@@ -18,7 +18,7 @@
 /****************************************************************************
  *  kernel/mqueue/mq_descreate.c
  *
- *   Copyright (C) 2007-2009, 2013 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2007-2009, 2013, 2016 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -67,6 +67,7 @@
 
 #include <tinyara/arch.h>
 #include <tinyara/kmalloc.h>
+#include <tinyara/sched.h>
 #include <tinyara/mqueue.h>
 
 #include "signal/signal.h"