8171d4a41a3648c2ff8ad06f3ece8b921ff90599
kang <pilseob.kang@samsung.com>
2019-06-24 16:39:55 +0900

    network/realtek: fix build path
    including (TOPDIR)/include causes unexpected errors which is hard to
    find. therefore remove the script line and fix the referenced path.

--- os/drivers/wireless/realtek/platform/ethernetif_tizenrt.h
+++ os/drivers/wireless/realtek/platform/ethernetif_tizenrt.h
@@ -1,8 +1,8 @@
 #ifndef __ETHERNETIF_H__
 #define __ETHERNETIF_H__
 
-#include "lwip/err.h"
-#include "lwip/netif.h"
+#include "net/lwip/err.h"
+#include "net/lwip/netif.h"
 
 //----- ------------------------------------------------------------------
 // Ethernet Buffer
