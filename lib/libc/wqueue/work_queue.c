417a5aca315b53d86e3d0810637e5f4b110d5aed
YiHyunjin <hj0412.yi@samsung.com>
2019-07-17 19:42:59 -0700

    libc/wqueue : Block the same work comes in workqueue
    If same work comes in workqueue, it will not work. So the same work block.

--- lib/libc/wqueue/work_queue.c
+++ lib/libc/wqueue/work_queue.c
@@ -122,15 +122,20 @@ static int work_qqueue(FAR struct usr_wqueue_s *wqueue, FAR struct work_s *work,
 {
 	DEBUGASSERT(work != NULL);
 
+	/* Get exclusive access to the work queue */
+
+	while (work_lock() < 0);
+
+	if (work->worker != NULL) {
+		work_unlock();
+		return -EALREADY;
+	}
+
 	/* First, initialize the work structure */
 
 	work->worker = worker;		/* Work callback */
 	work->arg = arg;			/* Callback argument */
 	work->delay = delay;		/* Delay until work performed */
-
-	/* Get exclusive access to the work queue */
-
-	while (work_lock() < 0);
 
 	/* Now, time-tag that entry and put it in the work queue. */
 
