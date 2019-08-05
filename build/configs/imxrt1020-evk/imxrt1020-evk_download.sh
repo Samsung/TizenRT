c1c9480ca90932b5244c061d94f9833082f361e9
Yashwanth <v.yashwanth@samsung.com>
2019-08-05 11:54:07 +0530

    imxrt1020-evk: Add download support for smartfs image
    This patch adds support for downloading the smartfs image
    onto the board.
    
    Signed-off-by: Yashwanth <v.yashwanth@samsung.com>

--- build/configs/imxrt1020-evk/imxrt1020-evk_download.sh
+++ build/configs/imxrt1020-evk/imxrt1020-evk_download.sh
@@ -98,6 +98,7 @@ function get_executable_name()
 		app) echo "tinyara_user.bin";;
 		micom) echo "micom";;
 		wifi) echo "wifi";;
+		userfs) echo "imxrt1020-evk_smartfs.bin";;
 		*) echo "No Binary Match"
 		exit 1
 	esac
@@ -111,6 +112,14 @@ function get_partition_index()
 		app | App | APP) echo "1";;
 		micom | Micom | MICOM) echo "2";;
 		wifi | Wifi | WIFI) echo "4";;
+		userfs | Userfs | USERFS)
+		for i in "${!parts[@]}"
+		do
+		   if [[ "${parts[$i]}" = "userfs" ]]; then
+			echo $i
+		   fi
+		done
+		;;
 		*) echo "No Matching Partition"
 		exit 1
 	esac
