e1689d915b5733ce3b11ca15c67a2872676d4406
Chanhee <ch2102.lee@samsung.com>
2019-08-27 20:53:21 +0900

    tools/fs: Add sector filters
    - Checkboxes are added to filter sectors by status.
    - There are three status in physical sectors. (ACTIVE, DIRTY, and CLEAN)

--- tools/fs/FS_Dump_Parser/src/smartfs_dump_parser/handlers/QuitHandler.java
+++ tools/fs/FS_Dump_Parser/src/smartfs_dump_parser/handlers/QuitHandler.java
@@ -5,7 +5,6 @@ import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
-
 public class QuitHandler {
 	@Execute
 	public void execute(IWorkbench workbench, Shell shell){
