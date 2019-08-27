8fd5cc39a833a884c4469f92673127250bd0c696
Chanhee <ch2102.lee@samsung.com>
2019-08-27 20:53:21 +0900

    tools/fs: Add sector filters
    - Checkboxes are added to filter sectors by status.
    - There are three status in physical sectors. (ACTIVE, DIRTY, and CLEAN)

--- tools/fs/FS_Dump_Parser/src/smartfs_dump_parser/data_model/ModelProvider.java
+++ tools/fs/FS_Dump_Parser/src/smartfs_dump_parser/data_model/ModelProvider.java
@@ -5,18 +5,46 @@ import java.util.List;
 
 public enum ModelProvider {
 	INSTANCE;
-	
+
 	private List<Header> headerList;
-	
+	private List<Header> activeSectors;
+	private List<Header> dirtySectors;
+	private List<Header> cleanSectors;
+
 	private ModelProvider() {
-		headerList = new ArrayList<Header>();		
+		headerList = new ArrayList<Header>();
 	}
-	
+
 	public List<Header> getHeaders() {
 		return headerList;
 	}
-	
-	public void addSectorHeader(Header header) {		
+
+	public void addSectorHeader(Header header) {
 		headerList.add(header);
 	}
+
+	public List<Header> getActiveSectors() {
+		return activeSectors;
+	}
+
+	public void setActiveSectors(List<Header> activeSectors) {
+		this.activeSectors = activeSectors;
+	}
+
+	public List<Header> getDirtySectors() {
+		return dirtySectors;
+	}
+
+	public void setDirtySectors(List<Header> dirtySectors) {
+		this.dirtySectors = dirtySectors;
+	}
+
+	public List<Header> getCleanSectors() {
+		return cleanSectors;
+	}
+
+	public void setCleanSectors(List<Header> cleanSectors) {
+		this.cleanSectors = cleanSectors;
+	}
+
 }
--- tools/fs/FS_Dump_Parser/src/smartfs_dump_parser/parts/DumpViewer.java
+++ tools/fs/FS_Dump_Parser/src/smartfs_dump_parser/parts/DumpViewer.java
@@ -1,5 +1,8 @@
 package smartfs_dump_parser.parts;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -11,8 +14,11 @@ import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
@@ -20,16 +26,25 @@ import org.eclipse.swt.widgets.TableColumn;
 import smartfs_dump_parser.data_model.Header;
 import smartfs_dump_parser.data_model.ModelProvider;
 
+enum SectorStatus {
+	ACTIVE, DIRTY, CLEAN
+}
+
 public class DumpViewer {
 
 	private TableViewer tableViewer;
+	private Button activeButton;
+	private Button dirtyButton;
+	private Button cleanButton;
 
 	@Inject
 	private MPart part;
 
 	@PostConstruct
 	public void createComposite(Composite parent) {
-		parent.setLayout(new GridLayout(1, false));
+		parent.setLayout(new GridLayout(3, false));
+
+		createCheckbox(parent);
 
 		tableViewer = new TableViewer(parent,
 				SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION | SWT.BORDER);
@@ -45,7 +60,7 @@ public class DumpViewer {
 
 		GridData gridData = new GridData();
 		gridData.verticalAlignment = GridData.FILL;
-		gridData.horizontalSpan = 2;
+		gridData.horizontalSpan = 3;
 		gridData.grabExcessHorizontalSpace = true;
 		gridData.grabExcessVerticalSpace = true;
 		gridData.horizontalAlignment = GridData.FILL;
@@ -60,6 +75,107 @@ public class DumpViewer {
 	@Persist
 	public void save() {
 		part.setDirty(false);
+	}
+
+	private void createCheckbox(final Composite parent) {
+		activeButton = new Button(parent, SWT.CHECK);
+		activeButton.setText("Active Sectors");
+		activeButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (activeButton.getSelection()) {
+					if (ModelProvider.INSTANCE.getHeaders().size() == 0) {
+						activeButton.setSelection(false);
+						return;
+					}
+					List<Header> activeSectors = ModelProvider.INSTANCE.getActiveSectors();
+					if (activeSectors == null) {
+						activeSectors = filterSectors(SectorStatus.ACTIVE, ModelProvider.INSTANCE.getHeaders());
+						ModelProvider.INSTANCE.setActiveSectors(activeSectors);
+					}
+					tableViewer.setInput(activeSectors);
+					tableViewer.refresh();
+
+					dirtyButton.setSelection(false);
+					cleanButton.setSelection(false);
+				}
+			}
+		});
+
+		dirtyButton = new Button(parent, SWT.CHECK);
+		dirtyButton.setText("Dirty Sectors");
+		dirtyButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (ModelProvider.INSTANCE.getHeaders().size() == 0) {
+					dirtyButton.setSelection(false);
+					return;
+				}
+				List<Header> dirtySectors = ModelProvider.INSTANCE.getDirtySectors();
+				if (dirtySectors == null) {
+					dirtySectors = filterSectors(SectorStatus.DIRTY, ModelProvider.INSTANCE.getHeaders());
+					ModelProvider.INSTANCE.setDirtySectors(dirtySectors);
+				}
+				tableViewer.setInput(dirtySectors);
+				tableViewer.refresh();
+
+				activeButton.setSelection(false);
+				cleanButton.setSelection(false);
+			}
+		});
+
+		cleanButton = new Button(parent, SWT.CHECK);
+		cleanButton.setText("Clean Sectors");
+		cleanButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (ModelProvider.INSTANCE.getHeaders().size() == 0) {
+					cleanButton.setSelection(false);
+					return;
+				}
+				List<Header> cleanSectors = ModelProvider.INSTANCE.getCleanSectors();
+				if (cleanSectors == null) {
+					cleanSectors = filterSectors(SectorStatus.CLEAN, ModelProvider.INSTANCE.getHeaders());
+					ModelProvider.INSTANCE.setCleanSectors(cleanSectors);
+				}
+				tableViewer.setInput(cleanSectors);
+				tableViewer.refresh();
+
+				activeButton.setSelection(false);
+				dirtyButton.setSelection(false);
+			}
+		});
+	}
+
+	private List<Header> filterSectors(SectorStatus targetStatus, List<Header> headerList) {
+		ArrayList<Header> resultList = new ArrayList<Header>();
+		switch (targetStatus) {
+		case ACTIVE:
+			for (Header h : headerList) {
+				int status = h.getStatus();
+				if (status < 128 && status >= 64) {
+					resultList.add(h);
+				}
+			}
+			break;
+		case DIRTY:
+			for (Header h : headerList) {
+				if (h.getStatus() < 64) {
+					resultList.add(h);
+				}
+			}
+			break;
+		case CLEAN:
+			for (Header h : headerList) {
+				if (h.getStatus() == 255) {
+					resultList.add(h);
+				}
+			}
+			break;
+		default:
+			break;
+		}
+		return resultList;
 	}
 
 	private void createColumns(final Composite parent, final TableViewer viewer) {
