88028630ee11592fda1f80d402cbdc14ad855e83
shbyeon <sh.byeon@samsung.com>
2019-08-28 13:24:43 +0900

    os/net/dhcp: Modify dhcp internal fuunctions
    - Remove the unused and unnecessary internal function of dhcpc
    - Add a logic to modify the priority of dhcp daemon

--- external/dhcpd/dhcpd_lwip.c
+++ external/dhcpd/dhcpd_lwip.c
@@ -214,6 +214,9 @@ int dhcp_server_status(char *intf)
 
 int dhcp_server_start(char *intf, dhcp_sta_joined dhcp_join_cb)
 {
+	pthread_attr_t attr;
+	struct sched_param sparam;
+	int ret = ERROR;
 	dhcp_join_data_s *data = (dhcp_join_data_s *)malloc(sizeof(dhcp_join_data_s));
 	if (!data) {
 		ndbg("Failed to alloc mem for data\n");
@@ -228,7 +231,21 @@ int dhcp_server_start(char *intf, dhcp_sta_joined dhcp_join_cb)
 	memcpy(data->intf, intf, strlen(intf) + 1);
 	data->fn = dhcp_join_cb;
 
-	int ret = pthread_create(&g_dhcpd_tid, NULL, _dhcpd_join_handler, (void *)data);
+	g_dhcpd_term = 0;
+	if ((ret = pthread_attr_init(&attr)) != 0) {
+		ndbg("Failed to init attr\n");
+		free(data->intf);
+		free(data);
+		return ERROR;
+	}
+	sparam.sched_priority = 110;
+	if ((ret = pthread_attr_setschedparam(&attr, &sparam)) != 0) {
+		ndbg("Failed to set attr\n");
+		free(data->intf);
+		free(data);
+		return ERROR;
+	}
+	ret = pthread_create(&g_dhcpd_tid, &attr, _dhcpd_join_handler, (void *)data);
 	if (ret < 0) {
 		free(data->intf);
 		free(data);
