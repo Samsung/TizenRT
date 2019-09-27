2b7c427cc42bc63113caf3d8e25b0dfc4d73fce5
Nishtha97 <n.maloo@samsung.com>
2019-09-27 16:22:15 +0530

    os/binfmt/libelf: Set cut-off ratio for no. of blocks for Caching
    - No. of blocks for caching should be equal to CUTOFF_RATIO_CACHE_BLOCKS
      of total number of blocks in file.
    - If this value exceeds CONFIG_ELF_CACHE_BLOCKS_COUNT, then set it equal
      to CONFIG_ELF_CACHE_BLOCKS_COUNT.
    
    Signed-off-by: Nishtha97 <n.maloo@samsung.com>

--- os/binfmt/libelf/libelf_cache.c
+++ os/binfmt/libelf/libelf_cache.c
@@ -453,6 +453,13 @@ int elf_cache_init(int filfd, uint16_t offset, off_t filelen, uint8_t compressio
 	number_of_blocks = file_len / cache_blocks_size;
 	elf_compress_type = compression_type;
 
+	/* Set number of blocks to use for caching */
+	if (CONFIG_ELF_CACHE_BLOCKS_COUNT < (CUTOFF_RATIO_CACHE_BLOCKS) * (number_of_blocks)) {
+		number_blocks_caching = CONFIG_ELF_CACHE_BLOCKS_COUNT;
+	} else {
+		number_blocks_caching = ((CUTOFF_RATIO_CACHE_BLOCKS) * (number_of_blocks));
+	}
+
 	/* Extra unaligned data apart from blocksize */
 	if (file_len % cache_blocks_size)
 		number_of_blocks++;
