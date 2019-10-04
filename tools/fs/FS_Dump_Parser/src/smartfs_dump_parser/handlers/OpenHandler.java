12f7b1fdb21c19cff21a4b00d3909f9a17cfbe86
Chanhee <ch2102.lee@samsung.com>
2019-10-04 19:09:23 +0900

    tool/fs: Fix a file open function to load contents correctly
    - A bug that the previous directory hierarchy and sectors are still
      remained when an another dump file is opened is fixed.

--- tools/fs/FS_Dump_Parser/src/smartfs_dump_parser/handlers/OpenHandler.java
+++ tools/fs/FS_Dump_Parser/src/smartfs_dump_parser/handlers/OpenHandler.java
@@ -5,11 +5,9 @@ import javax.inject.Inject;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
-import smartfs_dump_parser.data_model.SmartFileSystem;
 import smartfs_dump_parser.parts.DumpViewer;
 import smartfs_dump_visualizer.controllers.SmartFSOrganizer;
 
@@ -22,11 +20,6 @@ public class OpenHandler {
 	public void execute(Shell shell) {
 		FileDialog dialog = new FileDialog(shell);
 		if (dialog.open() != null) {
-			if (SmartFileSystem.isValidSmartFS()) {
-				MessageDialog.openInformation(shell, "Information",
-						"A dump file " + " is already opened.");
-				return;
-			}
 			String filePath = dialog.getFilterPath();
 			String fileName = dialog.getFileName();
 			System.out.println("filename = " + fileName);
