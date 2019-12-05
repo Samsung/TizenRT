c7119469d4904f5862b2ef5a027c4db9372711fc
Amandeep Chauhan <a.chauhan@samsung.com>
2019-12-05 17:26:52 +0530

    os/tools/compression: Add support for compressing using Miniz library
    Signed-off-by: Amandeep Chauhan <a.chauhan@samsung.com>

--- os/tools/compression/mkcompresstool.sh
+++ os/tools/compression/mkcompresstool.sh
@@ -35,6 +35,11 @@ then
 	mkdir -p $SRCDIR/lzma
 	TMPDIR=$SRCDIR/lzma
 	SOURCEDIR=$OS_PATH/../external/lzma
+elif [ $CONFIG_COMPRESSION_TYPE == 2 ]
+then
+	mkdir -p $SRCDIR/miniz
+	TMPDIR=$SRCDIR/miniz
+	SOURCEDIR=$OS_PATH/../external/miniz
 fi
 
 APPNAME=mkcompressimg
