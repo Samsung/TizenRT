2333255a52921cc8d76d4025e64b6a6da9500a6e
Sangamanatha <sangam.swami@samsung.com>
2020-02-13 11:40:31 +0530

    build/configs/imxrt1050-evk, imxrt1020-evk: Create new patitions. Need new patitions to store coredump and ramdump content.
    Signed-off-by: Sangamanatha <sangam.swami@samsung.com>

--- build/configs/imxrt1020-evk/imxrt1020-evk_download.sh
+++ build/configs/imxrt1020-evk/imxrt1020-evk_download.sh
@@ -247,6 +247,12 @@ all)
 		if [[ "$part" == "config" ]];then
 			continue
 		fi
+		if [[ "$part" == "coredump" ]];then
+			continue
+		fi
+		if [[ "$part" == "ramdump" ]];then
+			continue
+		fi
 		gidx=$(get_partition_index $part)
 		flash_erase ${offsets[$gidx]} ${sizes[$gidx]}
 		exe_name=$(get_executable_name ${parts[$gidx]})
