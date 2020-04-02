e7b9ff3632b565d0de1411d358d309faec2b21d8
ziyik <ziyik.cheah@gmail.com>
2020-04-02 15:41:17 +0800

    Add fail case handler, Correct comment type, Remove white space

--- os/se/ameba/rtl_security_api_wrapper.c
+++ os/se/ameba/rtl_security_api_wrapper.c
@@ -56,29 +56,31 @@
 #include "flash_api.h"
 #include "device_lock.h"
 
-//To fix duplicate hal enum problem
-//Enum on Security_hal.h
-//	HAL_SUCCESS = 0
-//	HAL_NOT_SUPPORTED = 15
-//	HAL_FAIL = 18
-#define RTL_HAL_SUCCESS				0
-#define RTL_HAL_NOT_SUPPORTED		15
-#define RTL_HAL_FAIL				18
+/*
+ * To fix duplicate hal enum problem
+ * Enum on Security_hal.h
+ * HAL_SUCCESS = 0
+ * HAL_NOT_SUPPORTED = 15
+ * HAL_FAIL = 18
+ */
+#define RTL_HAL_SUCCESS 0
+#define RTL_HAL_NOT_SUPPORTED 15
+#define RTL_HAL_FAIL 18
 
-//AES
+/* AES */
 static const unsigned char aes_iv[16] = {
 	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
 	0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
 };
 
 
-//Secure efuse key location
-#define SAMSUNG_KEY_ADDR	0x150
-#define SAMSUNG_KEY_SIZE	32
+/* Secure efuse key location */
+#define SAMSUNG_KEY_ADDR 0x150
+#define SAMSUNG_KEY_SIZE 32
 
-//Secure Storage Variable
-#define SEC_STORE_OFFSET	0x1C0000
-#define TAG_LENGTH			16
+/* Secure Storage Variable */
+#define SEC_STORE_OFFSET 0x1C0000
+#define TAG_LENGTH 16
 
 static const unsigned char aes_test_iv_gcm[12] = {
 	0xca, 0xfe, 0xba, 0xbe, 0xfa, 0xce, 0xdb, 0xad,
@@ -91,7 +93,7 @@ static const unsigned char aes_test_aad_gcm[20] = {
 	0xab, 0xad, 0xda, 0xd2
 };
 
-//Hal Data struct define
+/* Hal Data struct define */
 typedef struct _hal_data {
 	void *data;
 	uint32_t data_len;
@@ -124,15 +126,17 @@ uint32_t rtl_cryptoEngine_init_wrapper(void)
 
 	if (!crypto_inited) {
 		device_mutex_lock(RT_DEV_LOCK_CRYPTO);
-		ret = rtl_cryptoEngine_init();					// init crypto engine
+		ret = rtl_cryptoEngine_init();	/* init crypto engine */
 		device_mutex_unlock(RT_DEV_LOCK_CRYPTO);
 
 		if (ret == RTL_HAL_SUCCESS) {
 			crypto_inited = 1;
+		} else {
+			return RTL_HAL_FAIL;
 		}
 	}
 
-	//Read Samsung Key for Decyption
+	/* Read Samsung Key for Decyption */
 	device_mutex_lock(RT_DEV_LOCK_EFUSE);
 	rtl_ReadEfuse(SAMSUNG_KEY_ADDR, SAMSUNG_KEY_SIZE, samsung_key);
 	device_mutex_unlock(RT_DEV_LOCK_EFUSE);
@@ -149,17 +153,19 @@ uint32_t rtl_cryptoAES_ecb_wrapper(uint8_t* key, uint32_t keylen, unsigned char*
 	device_mutex_lock(RT_DEV_LOCK_CRYPTO);
 	ret = rtl_crypto_aes_ecb_init(key, keylen);
 
-	//Init Success
+	/* Init Success */
 	if (ret == RTL_HAL_SUCCESS) {
 		if (mode == MBEDTLS_ENCRYPT) {
-			//Encryption operation
+			/* Encryption operation */
 			ret = rtl_crypto_aes_ecb_encrypt(message, msglen, NULL, 0, pResult);
 		} else if (mode == MBEDTLS_DECRYPT) {
-			//Decryption operation
+			/* Decryption operation */
 			ret = rtl_crypto_aes_ecb_decrypt(message, msglen, NULL, 0, pResult);
 		} else {
 			ret = RTL_HAL_NOT_SUPPORTED;
 		}
+	} else {
+		ret = RTL_HAL_FAIL;
 	}
 	device_mutex_unlock(RT_DEV_LOCK_CRYPTO);
 
@@ -177,14 +183,16 @@ uint32_t rtl_cryptoAES_cbc_wrapper(uint8_t* key, uint32_t keylen, unsigned char*
 
 	if (ret == RTL_HAL_SUCCESS) {
 		if (mode == MBEDTLS_ENCRYPT) {
-			//Encryption operation
+			/* Encryption operation */
 			ret = rtl_crypto_aes_cbc_encrypt(message, msglen, iv, sizeof(iv), pResult);
 		} else if (mode == MBEDTLS_DECRYPT) {
-			//Decryption operation
+			/* Decryption operation */
 			ret = rtl_crypto_aes_ecb_decrypt(message, msglen, iv, sizeof(iv), pResult);
 		} else {
 			ret = RTL_HAL_NOT_SUPPORTED;
 		}
+	} else {
+		ret = RTL_HAL_FAIL;
 	}
 	device_mutex_unlock(RT_DEV_LOCK_CRYPTO);
 
@@ -202,14 +210,16 @@ uint32_t rtl_cryptoAES_ctr_wrapper(uint8_t* key, uint32_t keylen, unsigned char*
 
 	if (ret == RTL_HAL_SUCCESS) {
 		if (mode == MBEDTLS_ENCRYPT) {
-			//Encryption operation
+			/* Encryption operation */
 			ret = rtl_crypto_aes_ctr_encrypt(message, msglen, iv, sizeof(iv), pResult);
 		} else if (mode == MBEDTLS_DECRYPT) {
-			//Decryption operation
+			/* Decryption operation */
 			ret = rtl_crypto_aes_ctr_decrypt(message, msglen, iv, sizeof(iv), pResult);
 		} else {
 			ret = RTL_HAL_NOT_SUPPORTED;
 		}
+	} else {
+		ret = RTL_HAL_FAIL;
 	}
 	device_mutex_unlock(RT_DEV_LOCK_CRYPTO);
 
@@ -221,12 +231,12 @@ uint32_t rtl_cryptoAES_ctr_wrapper(uint8_t* key, uint32_t keylen, unsigned char*
  */
 uint32_t rtl_write_storage_wrapper(uint32_t ss_idx, hal_data *data)
 {
-	uint32_t	ret;
-	uint8_t		tmpbuf[64];
-	uint8_t		buf_length[4];
-	uint8_t		*key = (uint8_t*)(((uint32_t) tmpbuf + 31) & (~31));	// align to 32byte
-	uint32_t	data_len = (data->data_len + 15) / 16 * 16;				// AES blocks
-	flash_t		flash;
+	uint32_t ret;
+	uint8_t tmpbuf[64];
+	uint8_t buf_length[4];
+	uint8_t *key = (uint8_t*)(((uint32_t) tmpbuf + 31) & (~31));	/* align to 32byte */
+	flash_t flash;
+	uint32_t data_len = (data->data_len + 15) / 16 * 16;	/* AES blocks */
 	const uint8_t aes_gcm_iv[12];
 	const uint8_t aes_gcm_aad[20];
 
@@ -252,6 +262,7 @@ uint32_t rtl_write_storage_wrapper(uint32_t ss_idx, hal_data *data)
 				crypto_inited = 1;
 			} else {
 				sedbg("\n\r ERROR: rtl_cryptoEngine_init \n\r");
+				ret = RTL_HAL_FAIL;
 				goto exit;
 			}
 		}
@@ -297,15 +308,15 @@ exit:
 
 uint32_t rtl_read_storage_wrapper(uint32_t ss_idx, hal_data *data)
 {
-	uint32_t	ret;
-	uint32_t	data_len;
-	uint8_t		dec_tag[16];
-	uint8_t		tmpbuf[64];
-	uint8_t		buf_length[4];
-	uint8_t		*key = (uint8_t*)(((uint32_t) tmpbuf + 31) & (~31));	// align to 32byte
-	flash_t		flash;
-	const uint8_t	aes_gcm_iv[12];
-	const uint8_t	aes_gcm_aad[20];
+	uint32_t ret;
+	uint32_t data_len;
+	uint8_t dec_tag[16];
+	uint8_t tmpbuf[64];
+	uint8_t buf_length[4];
+	uint8_t *key = (uint8_t*)(((uint32_t) tmpbuf + 31) & (~31));	/* align to 32byte */
+	flash_t flash;
+	const uint8_t aes_gcm_iv[12];
+	const uint8_t aes_gcm_aad[20];
 
 	device_mutex_lock(RT_DEV_LOCK_FLASH);
 	flash_stream_read(&flash, SEC_STORE_OFFSET + ss_idx * 4096, 4, buf_length);
