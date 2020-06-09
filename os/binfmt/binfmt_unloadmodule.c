1d312322207175f8f2cf1f329d58262e181dcae3
Kishore S N <kishore.sn@samsung.com>
2020-06-09 10:55:42 +0530

    os/binfmt : Modfiy method of allocation ELF sections
    If we allocate each elf section separately using memalign, then there
    is high overhead due to the requirement of ALLOCNODE at the starting of
    each allocation. To overcome this, we combine all the sections of the elf
    and allocate a single chunk of memory. The sections are then arranged at
    their corresponding power of 2 address within the chunk. In order to
    ensure proper alignment boundaries, the sections need to be arranged in
    descending order of the alignsize.
    
    Signed-off-by: Kishore S N <kishore.sn@samsung.com>

--- os/binfmt/binfmt_unloadmodule.c
+++ os/binfmt/binfmt_unloadmodule.c
@@ -153,7 +153,6 @@ static inline int exec_dtors(FAR struct binary_s *binp)
 int unload_module(FAR struct binary_s *binp)
 {
 	int ret;
-	int i;
 
 	if (binp) {
 		/* Perform any format-specific unload operations */
@@ -189,19 +188,11 @@ int unload_module(FAR struct binary_s *binp)
 
 		/* Free allocated address spaces */
 
-#if defined(CONFIG_OPTIMIZE_APP_RELOAD_TIME) || !defined(CONFIG_APP_BINARY_SEPARATION)
-#ifdef CONFIG_OPTIMIZE_APP_RELOAD_TIME
-		if (!binp->reload) {
-#endif
-			for (i = 0; i < ALLOC_MAX; i++) {
-				if (binp->alloc[i]) {
-					binfo("Freeing alloc[%d]: %p\n", i, binp->alloc[i]);
-					kumm_free((FAR void *)binp->alloc[i]);
-				}
-			}
-#ifdef CONFIG_OPTIMIZE_APP_RELOAD_TIME
+#ifndef CONFIG_APP_BINARY_SEPARATION
+		if (binp->alloc[i]) {
+			binfo("Freeing alloc[%d]: %p\n", i, binp->alloc[i]);
+			kumm_free((FAR void *)binp->alloc[i]);
 		}
-#endif
 #endif
 
 		/* Notice that the address environment is not destroyed.  This should
