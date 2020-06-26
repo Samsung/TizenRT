970872e824a68e44c9f889a1412528a1c7e8fa41
sunghan-chang <sh924.chang@samsung.com>
2020-06-26 15:45:29 +0900

    fix typo
    diffrent -> different
    accomodate -> accommodate
    
    Signed-off-by: sunghan-chang <sh924.chang@samsung.com>

--- os/net/lwip/src/core/init.c
+++ os/net/lwip/src/core/init.c
@@ -145,7 +145,7 @@ PACK_STRUCT_END
 #endif
 /* There must be sufficient timeouts, taking into account requirements of the subsystems. */
 #if LWIP_TIMERS && (MEMP_NUM_SYS_TIMEOUT < (LWIP_TCP + IP_REASSEMBLY + LWIP_ARP + (2*LWIP_DHCP) + LWIP_AUTOIP + LWIP_IGMP + LWIP_DNS + PPP_SUPPORT + (LWIP_IPV6 ? (1 + LWIP_IPV6_REASS + LWIP_IPV6_MLD) : 0)))
-#error "MEMP_NUM_SYS_TIMEOUT is too low to accomodate all required timeouts"
+#error "MEMP_NUM_SYS_TIMEOUT is too low to accommodate all required timeouts"
 #endif
 #if (IP_REASSEMBLY && (MEMP_NUM_REASSDATA > IP_REASS_MAX_PBUFS))
 #error "MEMP_NUM_REASSDATA > IP_REASS_MAX_PBUFS doesn't make sense since each struct ip_reassdata must hold 2 pbufs at least!"
