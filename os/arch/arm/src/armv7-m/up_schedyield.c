57d6b9fb348eb90c21290e1af50a14c44b3de8cd
sunghan-chang <sh924.chang@samsung.com>
2020-07-08 15:52:50 +0900

    arch/arm: add missing codes in schedyield.c
    When compares to up(arm)_unblock_task, up(arm)_schedyield
    has many missing functions and headers. This commit adds
    them.
    
    Signed-off-by: sunghan-chang <sh924.chang@samsung.com>

--- os/arch/arm/src/armv7-m/up_schedyield.c
+++ os/arch/arm/src/armv7-m/up_schedyield.c
@@ -20,6 +20,22 @@
  * Included Files
  ****************************************************************************/
 
+#include <tinyara/config.h>
+
+#ifdef CONFIG_TASK_MONITOR
+#include <sys/types.h>
+#endif
+#ifdef CONFIG_SUPPORT_COMMON_BINARY
+#include <stdint.h>
+#endif
+#include <tinyara/sched.h>
+#ifdef CONFIG_ARMV7M_MPU
+#include <tinyara/mpu.h>
+#endif
+#ifdef CONFIG_TASK_SCHED_HISTORY
+#include <tinyara/debug/sysdbg.h>
+#endif
+
 #include "sched/sched.h"
 #include "up_internal.h"
 
@@ -30,6 +46,10 @@
 /****************************************************************************
  * Private Data
  ****************************************************************************/
+
+#ifdef CONFIG_SUPPORT_COMMON_BINARY
+extern uint32_t g_umm_app_id;
+#endif
 
 /****************************************************************************
  * Private Functions
@@ -68,6 +88,38 @@ void up_schedyield(struct tcb_s *rtcb)
 		 */
 
 		rtcb = this_task();
+#ifdef CONFIG_TASK_SCHED_HISTORY
+		/* Save the task name which will be scheduled */
+
+		save_task_scheduling_status(rtcb);
+#endif
+
+		/* Restore the MPU registers in case we are switching to an application task */
+#ifdef CONFIG_ARMV7M_MPU
+		/* Condition check : Update MPU registers only if this is not a kernel thread. */
+
+		if ((rtcb->flags & TCB_FLAG_TTYPE_MASK) != TCB_FLAG_TTYPE_KERNEL) {
+#if defined(CONFIG_APP_BINARY_SEPARATION)
+			for (int i = 0; i < 3 * MPU_NUM_REGIONS; i += 3) {
+				up_mpu_set_register(&rtcb->mpu_regs[i]);
+			}
+#endif
+#ifdef CONFIG_MPU_STACK_OVERFLOW_PROTECTION
+			up_mpu_set_register(rtcb->stack_mpu_regs);
+#endif
+		}
+#endif
+
+#ifdef CONFIG_SUPPORT_COMMON_BINARY
+		if (g_umm_app_id) {
+			*g_umm_app_id = rtcb->app_id;
+		}
+#endif
+#ifdef CONFIG_TASK_MONITOR
+		/* Update rtcb active flag for monitoring. */
+
+		rtcb->is_active = true;
+#endif
 		/* Then switch contexts */
 
 		up_restorestate(rtcb->xcp.regs);
@@ -80,6 +132,11 @@ void up_schedyield(struct tcb_s *rtcb)
 		 */
 
 		struct tcb_s *nexttcb = this_task();
+#ifdef CONFIG_TASK_SCHED_HISTORY
+		/* Save the task name which will be scheduled */
+
+		save_task_scheduling_status(nexttcb);
+#endif
 		up_switchcontext(rtcb->xcp.regs, nexttcb->xcp.regs);
 
 		/* up_switchcontext forces a context switch to the task at the
