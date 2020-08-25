bae69f470ee92f1b9ff250eabb7a7d3928ce4834
Vidisha <thapa.v@samsung.com>
2020-08-26 00:06:43 +0530

    build/configs/rtl8721csm: Update download script for loadable_apps config
    This patch updates the download script for loadable apps configuration.
    
    Signed-off-by: Vidisha <thapa.v@samsung.com>

--- build/configs/rtl8721csm/rtl8721csm_download.sh
+++ build/configs/rtl8721csm/rtl8721csm_download.sh
@@ -18,23 +18,18 @@
 ###########################################################################
 # rtl8721csm_download.sh
 
-if [ "$1" != "ALL" ]; then
-	echo -e "\nWarning: Unsupported download option \""$1"\" is detected !!!\n\nSuggested option: \"ALL\"\n";
-	exit 0
-fi
-
 if [ -n "$2" ]; then
-	PORT="$2"
+	TTYDEV="$2"
 else
-	PORT="ttyUSB1"
+	TTYDEV="ttyUSB1"
 fi
 
-WARNING="\n Port $PORT is selected\n\n
+WARNING="\n Port $TTYDEV is selected\n\n
 	############################################\n
 	WARNINGS:\n
 	1. Make sure the board is in DOWNLOAD MODE.\n
 	2. Make sure NO other application like putty,\n
-	is occupying $PORT.\n
+	is occupying $TTYDEV.\n
 	############################################\n"
 
 echo -e $WARNING
@@ -44,17 +39,325 @@ CUR_PATH=$(dirname "$CURDIR")
 TOOL_PATH=${CUR_PATH}/../../tools/amebad
 IMG_TOOL_PATH=${TOOL_PATH}/image_tool
 BIN_PATH=${CUR_PATH}/../../output/bin
+TOP_PATH=${CUR_PATH}/../../..
+OS_PATH=${TOP_PATH}/os
 SUDO=sudo
+FLASH_START_ADDR=0x08000000
+
+CONFIG=${OS_PATH}/.config
+source ${CONFIG}
+
+##Utility function for sanity check##
+function rtl8721csm_sanity_check()
+{
+	if [ ! -f ${CONFIG} ];then
+		echo "No .config file"
+		exit 1
+	fi
+
+	if [[ "${CONFIG_ARCH_BOARD_RTL8721CSM}" != "y" ]];then
+		echo "Target is NOT RTL8721CSM"
+		exit 1
+	fi
+
+	if [ ! -f ${TINYARA_BIN} ]; then
+		echo "missing file ${TINYARA_BIN}"
+		exit 1
+	fi
+
+	if fuser -s ${TTYDEV};then
+		echo "${TTYDEV} is used by another process, can't proceed"
+		exit 1
+	fi
+}
+
+download_km0_bl()
+{
+	cd ${IMG_TOOL_PATH}
+	echo ""
+	echo "=========================="
+	echo "Downloading KM0_BL IMAGE"
+	echo "=========================="
+
+	for part in ${parts[@]}; do
+
+		if [[ "$part" == "km0_bl" ]];then
+			idx=$(get_partition_index $part)
+			exe_name=$(get_executable_name ${parts[$idx]})
+			break
+		fi
+	done
+
+	$SUDO ./amebad_image_tool /dev/$TTYDEV 1 ${offsets[$idx]} ${exe_name}
+
+	echo "KM0_BL Download DONE"
+}
+
+download_km4_bl()
+{
+	cd ${IMG_TOOL_PATH}
+	echo ""
+	echo "=========================="
+	echo "Downloading KM4_BL IMAGE"
+	echo "=========================="
+
+	for part in ${parts[@]}; do
+
+		if [[ "$part" == "km4_bl" ]];then
+			idx=$(get_partition_index $part)
+			exe_name=$(get_executable_name ${parts[$idx]})
+			break
+		fi
+	done
+
+	$SUDO ./amebad_image_tool /dev/$TTYDEV 1 ${offsets[$idx]} ${exe_name}
+
+	echo "KM4_BL Download DONE"
+}
+
+download_kernel()
+{
+	cd ${IMG_TOOL_PATH}
+	echo ""
+	echo "=========================="
+	echo "Downloading KERNEL IMAGE"
+	echo "=========================="
+
+	for part in ${parts[@]}; do
+
+		if [[ "$part" == "kernel" ]];then
+			idx=$(get_partition_index $part)
+			exe_name=$(get_executable_name ${parts[$idx]})
+			break
+		fi
+	done
+
+	$SUDO ./amebad_image_tool /dev/$TTYDEV 1 ${offsets[$idx]} ${exe_name}
+
+	echo "KERNEL Download DONE"
+}
+
+download_smartfs()
+{
+	cd ${IMG_TOOL_PATH}
+	echo ""
+	echo "=========================="
+	echo "Downloading SMARTFS IMAGE"
+	echo "=========================="
+
+	for part in ${parts[@]}; do
+
+		if [[ "$part" == "userfs" ]];then
+			idx=$(get_partition_index $part)
+			exe_name=$(get_executable_name ${parts[$idx]})
+			break
+		fi
+	done
+
+	$SUDO ./amebad_image_tool /dev/$TTYDEV 1 ${offsets[$idx]} ${exe_name}
+
+	echo "SMARTFS Download DONE"
+}
+
+##Utility function to match partition name to binary name##
+function get_executable_name()
+{
+	case $1 in
+		km0_bl) echo "km0_boot_all.bin";;
+		km4_bl) echo "km4_boot_all.bin";;
+		kernel) echo "km0_km4_image2.bin";;
+		userfs) echo "rtl8721csm_smartfs.bin";;
+		*) echo "No Binary Match"
+		exit 1
+	esac
+}
+
+##Utility function to get partition index ##
+function get_partition_index()
+{
+	case $1 in
+		km0_bl | Km0_bl | KM0_BL) echo "0";;
+		km4_bl | Km4_bl | KM4_BL) echo "1";;
+		kernel | Kernel | KERNEL) echo "2";;
+		userfs | Userfs | USERFS)
+		for i in "${!parts[@]}"
+		do
+		   if [[ "${parts[$i]}" = "userfs" ]]; then
+			echo $i
+		   fi
+		done
+		;;
+		*) echo "No Matching Partition"
+		exit 1
+	esac
+}
+
+##Help utility##
+function rtl8721csm_dwld_help()
+{
+        cat <<EOF
+	HELP:
+		make download ALL or [PARTITION(S)]
+	PARTITION(S):
+		 [${uniq_parts[@]}]  NOTE:case sensitive
+
+	For examples:
+		make download ALL
+	        make download km0_bl km4_bl
+	        make download kernel
+	        make download smartfs
+EOF
+}
+
+##Utility function to read partitions from .config or Kconfig##
+function get_configured_partitions()
+{
+	local configured_parts
+	# Read Partitions
+	if [[ -z ${CONFIG_FLASH_PART_NAME} ]];then
+
+		configured_parts=`grep -A 2 'config FLASH_PART_NAME' ${PARTITION_KCONFIG} | sed -n 's/\tdefault "\(.*\)".*/\1/p'`
+	else
+		configured_parts=${CONFIG_FLASH_PART_NAME}
+	fi
+
+	echo $configured_parts
+}
+
+##Utility function to get the partition sizes from .config or Kconfig
+function get_partition_sizes()
+{
+	local sizes_str
+	#Read Partition Sizes
+	if [[ -z ${CONFIG_FLASH_PART_SIZE} ]]
+	then
+		sizes_str=`grep -A 2 'config FLASH_PART_SIZE' ${PARTITION_KCONFIG} | sed -n 's/\tdefault "\(.*\)".*/\1/p'`
+	else
+		sizes_str=${CONFIG_FLASH_PART_SIZE}
+	fi
+
+	echo $sizes_str
+}
+
+# Start here
 
 cp -p ${BIN_PATH}/km0_boot_all.bin ${IMG_TOOL_PATH}/km0_boot_all.bin
 cp -p ${BIN_PATH}/km4_boot_all.bin ${IMG_TOOL_PATH}/km4_boot_all.bin
 cp -p ${BIN_PATH}/km0_km4_image2.bin ${IMG_TOOL_PATH}/km0_km4_image2.bin
+if [[ "${CONFIG_APP_BINARY_SEPARATION}" == "y" ]];then
+	cp -p ${BIN_PATH}/rtl8721csm_smartfs.bin ${IMG_TOOL_PATH}/rtl8721csm_smartfs.bin
+fi
 
-cd ${IMG_TOOL_PATH}
+rtl8721csm_sanity_check;
 
-echo "Start Download"
-$SUDO ./amebad_image_tool /dev/$PORT
+parts=$(get_configured_partitions)
+IFS=',' read -ra parts <<< "$parts"
 
-$SUDO [ -e km0_boot_all.bin ] && rm km0_boot_all.bin
-$SUDO [ -e km4_boot_all.bin ] && rm km4_boot_all.bin
-$SUDO [ -e km0_km4_image2.bin ] && rm km0_km4_image2.bin
+sizes=$(get_partition_sizes)
+IFS=',' read -ra sizes <<< "$sizes"
+
+#Calculate Flash Offset
+num=${#sizes[@]}
+offsets[0]=`printf "0x%X" ${FLASH_START_ADDR}`
+
+for (( i=1; i<=$num-1; i++ ))
+do
+	val=$((sizes[$i-1] * 1024))
+	offsets[$i]=`printf "0x%X" $((offsets[$i-1] + ${val}))`
+done
+
+#Dump Info
+echo ""
+echo "==================================================================================="
+printf '%b\t\t' "\rPARTITION NAMES: \t${parts[@]}\n"
+printf '%b\t\t' "\rPARTITION SIZES: \t${sizes[@]}\n"
+printf '%b\t' "\rPARTITION OFFSETS: \t${offsets[@]}\n"
+printf "\r"
+echo "==================================================================================="
+echo ""
+
+if test $# -eq 0; then
+	echo "FAIL!! NO Given partition. Refer \"PARTITION NAMES\" above."
+	rtl8721csm_dwld_help 1>&2
+	exit 1
+fi
+
+uniq_parts=($(printf "%s\n" "${parts[@]}" | sort -u));
+
+#Validate arguments
+for i in ${cmd_args[@]};do
+
+	if [[ "${i}" == "ALL" ]];then
+		continue;
+	fi
+
+	for j in ${uniq_parts[@]};do
+		if [[ "${i}" == "${j}" ]];then
+			result=yes
+		fi
+	done
+
+	if [[ "$result" != "yes" ]];then
+		echo "FAIL!! Given \"${i}\" partition is not available. Refer \"PARTITION NAMES\" above."
+		rtl8721csm_dwld_help
+		exit 1
+	fi
+	result=no
+done
+
+download_all()
+{
+	cd ${IMG_TOOL_PATH}
+	echo "Starting Download..."
+
+	for part in ${parts[@]}; do
+
+		if [[ "${CONFIG_APP_BINARY_SEPARATION}" != "y" ]];then
+			if [[ "$part" == "userfs" ]];then
+				continue
+			fi
+		fi
+
+		echo ""
+		echo "=========================="
+		echo "Downloading ${part} binary"
+		echo "=========================="
+
+		gidx=$(get_partition_index $part)
+		exe_name=$(get_executable_name ${parts[$gidx]})
+
+		$SUDO ./amebad_image_tool /dev/$TTYDEV 1 ${offsets[$gidx]} ${exe_name}
+
+	done
+	echo ""
+	echo "Download COMPLETE!"
+
+	$SUDO [ -e km0_boot_all.bin ] && rm km0_boot_all.bin
+	$SUDO [ -e km4_boot_all.bin ] && rm km4_boot_all.bin
+	$SUDO [ -e km0_km4_image2.bin ] && rm km0_km4_image2.bin
+	if [[ "${CONFIG_APP_BINARY_SEPARATION}" == "y" ]];then
+		$SUDO [ -e rtl8721csm_smartfs.bin ] && rm rtl8721csm_smartfs.bin
+	fi
+}
+
+case $1 in
+	km0_bl|KM0_BL)
+		download_km0_bl
+		;;
+	km4_bl|KM4_BL)
+		download_km4_bl
+		;;
+	kernel|KERNEL)
+		download_kernel
+		;;
+	smartfs|SMARTFS)
+		download_smartfs
+		;;
+	all|ALL)
+		download_all
+		;;
+	*)
+		echo "Not supported"
+		rtl8721csm_dwld_help
+		;;
+esac
