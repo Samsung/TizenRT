7f9f03ed7ae56c534419e7120af9441117a1e93d
Ahreum Jeong <ahreum.jeong@samsung.com>
2020-10-22 17:07:32 +0900

    kernel/group: Remove unused group_exitinfo function
    Remove unused group_exitinfo function.

--- os/include/tinyara/sched.h
+++ os/include/tinyara/sched.h
@@ -917,31 +917,6 @@ pid_t task_vforkstart(FAR struct task_tcb_s *child);
  */
 void task_vforkabort(FAR struct task_tcb_s *child, int errcode);
 
-#ifdef CONFIG_BINFMT_LOADABLE
-/****************************************************************************
- * Name: group_exitinfo
- *
- * Description:
- *   This function may be called to when a task is loaded into memory.  It
- *   will setup the to automatically unload the module when the task exits.
- *
- * Input Parameters:
- *   pid     - The task ID of the newly loaded task
- *   bininfo - This structure allocated with kmm_malloc().  This memory
- *             persists until the task exits and will be used unloads
- *             the module from memory.
- *
- * Returned Value:
- *   This is a NuttX internal function so it follows the convention that
- *   0 (OK) is returned on success and a negated errno is returned on
- *   failure.
- *
- ****************************************************************************/
-/**
- * @internal
- */
-int group_exitinfo(pid_t pid, FAR struct binary_s *bininfo);
-#endif
 /**
  * @endcond
  */
