dc86478f8cb3e7992695bb9ceaa70ae15dba1350
ziyik <ziyik.cheah@gmail.com>
2020-12-11 11:33:56 +0800

    os/board/rtl8721csm: Add MPU for Secure PSRAM

--- os/board/rtl8721csm/src/component/soc/realtek/amebad/fwlib/ram_hp/rtl8721dhp_app_start.c
+++ os/board/rtl8721csm/src/component/soc/realtek/amebad/fwlib/ram_hp/rtl8721dhp_app_start.c
@@ -1010,6 +1010,11 @@ u32 app_mpu_nocache_init(void)
 	return 0;
 }
 
+u32 app_mpu_s_nocache_init(void)
+{
+	mpu_s_no_cache_init();
+}
+
 VOID app_vdd1833_detect(VOID)
 {
 	u32 temp;
@@ -1338,6 +1343,7 @@ extern void __libc_init_array(void);
 #ifdef CONFIG_PLATFORM_TIZENRT_OS
 	mpu_init();
 	app_mpu_nocache_init();
+	app_mpu_s_nocache_init();
 #endif
 	app_vdd1833_detect();
 	memcpy_gdma_init();
