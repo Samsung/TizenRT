6566def2164c45541ad9c0af957951f8aa2993f2
pillip <pilseob.kang@samsung.com>
2021-01-21 15:31:44 +0900

    network/wifimanager: Add info type log in wi-fi manager
    Add info log to display wi-fi manager information.
    Error and debug are for handling exceptional cases.
    And verbose is used for both cases debug and getting info.
    Adding info separates the roles. verbose is used for debugging and
    info is used for getting info.

--- framework/src/wifi_manager/wifi_manager_lwnl_listener.c
+++ framework/src/wifi_manager/wifi_manager_lwnl_listener.c
@@ -190,6 +190,9 @@ static int _wifi_utils_callback_handler(int argc, char *argv[])
 		rfds = ofds;
 		res = select(maxfd, &rfds, NULL, NULL, NULL);
 		if (res < 0) {
+			if (errno == EINTR) {
+				continue;
+			}
 			WM_ERR;
 			break;
 		}
@@ -203,7 +206,7 @@ static int _wifi_utils_callback_handler(int argc, char *argv[])
 			// get events from netlink driver
 			res = _wifi_utils_fetch_event(nd);
 			if (res < 0) {
-				WM_LOG_ERROR("message currupted\n");
+				WM_ERR;
 				break;
 			}
 		} else {
@@ -222,7 +225,7 @@ static int _wifi_utils_callback_handler(int argc, char *argv[])
  */
 void lwnl_start_monitor(void)
 {
-	int tid = task_create("lwnl8021 cb handler", 110, 4096, (main_t)_wifi_utils_callback_handler, NULL);
+	int tid = task_create("lwnl cb handler", 110, 4096, (main_t)_wifi_utils_callback_handler, NULL);
 	if (tid < 0) {
 		WM_ERR;
 	}
