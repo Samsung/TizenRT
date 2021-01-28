e4711af45f95848f95f9e36f657975c22d560fa1
jeongchanKim <jc_.kim@samsung.com>
2021-01-28 19:10:38 +0900

    kernel : Support Reboot Reason Interfaces
    There are 3 types of reboot reason interfaces : read, write, clear
    They can be used through prctl() with PR_REBOOT_REASON_READ, PR_REBOOT_REASON_WRITE and PR_REBOOT_REASON_CLEAR.

---
+++ os/include/tinyara/reboot_reason.h
@@ -0,0 +1,36 @@
+/****************************************************************************
+ *
+ * Copyright 2021 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+#ifndef __INCLUDE_REBOOT_REASON_H
+#define __INCLUDE_REBOOT_REASON_H
+
+/********************************************************************************************
+ * Included Files
+ ********************************************************************************************/
+
+typedef enum {
+	REBOOT_SYSTEM_WATCHDOG		= 0,	/* watchdog timeout */        
+	REBOOT_SYSTEM_DATAABORT		= 1,	/* Data abort */
+	REBOOT_SYSTEM_PREFETCHABORT	= 2,	/* Prefetch abort */
+	REBOOT_SYSTEM_MEMORYALLOCFAIL	= 3,	/* Memory allocation failure */
+	REBOOT_SYSTEM_HW_RESET		= 4,	/* HW power reset */
+	REBOOT_SYSTEM_WIFICORE_WATCHDOG = 11,	/* Wi-Fi Core Watchdog Reset */
+	REBOOT_SYSTEM_WIFICORE_PANIC    = 12,	/* Wi-Fi Core Panic */
+	REBOOT_UNKNOWN 	 		= 32
+} reboot_reason_code_t;
+
+#endif					/* __INCLUDEREBOOT_REASON_H */
