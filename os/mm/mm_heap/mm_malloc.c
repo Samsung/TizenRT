b35f84b73c824eda3f64a71a5066dc120ae3c267
jeongchanKim <jc_.kim@samsung.com>
2021-02-04 12:41:46 +0900

    os/mm : Add reboot reason when allocation failure
    If MM_ASSERT_ON_FAIL is enabled, board will be rebooted when memory allocation is failed.
    In this case, write the reboot reason about REBOOT_SYSTEM_MEMORYALLOCFAIL.

--- os/mm/mm_heap/mm_malloc.c
+++ os/mm/mm_heap/mm_malloc.c
@@ -60,6 +60,10 @@
 
 #ifdef CONFIG_MM_ASSERT_ON_FAIL
 #include <assert.h>
+#ifdef CONFIG_SYSTEM_REBOOT_REASON
+#include <sys/prctl.h>
+#include <tinyara/reboot_reason.h>
+#endif
 #endif
 #include <tinyara/mm/mm.h>
 
@@ -233,6 +237,9 @@ FAR void *mm_malloc(FAR struct mm_heap_s *heap, size_t size)
 	 */
 
 	if (!ret) {
+#if defined(CONFIG_MM_ASSERT_ON_FAIL) && defined(CONFIG_SYSTEM_REBOOT_REASON)
+		prctl(PR_REBOOT_REASON_WRITE, REBOOT_SYSTEM_MEMORYALLOCFAIL);
+#endif
 		mdbg("Allocation failed, size %u\n", size);
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
 		heapinfo_parse_heap(heap, HEAPINFO_DETAIL_ALL, HEAPINFO_PID_ALL);
