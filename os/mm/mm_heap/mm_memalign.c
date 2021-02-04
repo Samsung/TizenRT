f8e9e5ae78d42a649cc701814794d09b6c200226
jeongchanKim <jc_.kim@samsung.com>
2021-02-04 10:23:47 +0900

    os/mm : Add option to make assertion when fail to allocate memory
    When memory allocation fails, make assertion for debugging.
    From the application binary with loadable build, it will be reloaded.

--- os/mm/mm_heap/mm_memalign.c
+++ os/mm/mm_heap/mm_memalign.c
@@ -57,7 +57,9 @@
 #include <tinyara/config.h>
 
 #include <debug.h>
-
+#ifdef CONFIG_MM_ASSERT_ON_FAIL
+#include <assert.h>
+#endif
 #include <tinyara/mm/mm.h>
 
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
@@ -271,16 +273,18 @@ FAR void *mm_memalign(FAR struct mm_heap_s *heap, size_t alignment, size_t size)
 	 * to the SYSLOG.
 	 */
 
-#ifdef CONFIG_DEBUG_MM
 	if (!ret) {
 		mdbg("Allocation failed, size %u\n", size);
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
 		heapinfo_parse_heap(heap, HEAPINFO_DETAIL_ALL, HEAPINFO_PID_ALL);
 #endif
+
+#ifdef CONFIG_MM_ASSERT_ON_FAIL
+		up_assert();
+#endif
 	} else {
 		mvdbg("Allocated %p, size %u\n", ret, size);
 	}
-#endif
 
 	return ret;
 }
