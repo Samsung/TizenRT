124285be163fee460b7fe7600089d9803d425425
pilseob.kang <pilseob.kang@samsung.com>
2021-02-15 14:46:58 +0900

    security: apply rom and ram key index in seclink driver
    seclink driver only supported 4 key slots before. This commit
    separates the key slot area into rom which is unmodified and RAM which
    can be modified while the board is on in a seclink driver.
    A seclink driver manages as following invalid requests
    1) invalid request (ex. set key to rom area)
    2) invalid slot range (key index is invalid)
    3) key slot status in RAM(set key to the slot in which a key is already
    allocated)
    
    In seclink driver, 0~63 key slots are valid. However key slot range
    which is valid in each SE is different. Therefore SE driver should
    check that the key is whether valid or not by itself.

--- os/drivers/seclink/seclink_drv_key.c
+++ os/drivers/seclink/seclink_drv_key.c
@@ -29,43 +29,126 @@
 #endif
 #define SLDRV_TAG "[SECLINK_DRV_KEY]"
 
-#define NKEY_SLOT 4
+#define NKEY_SLOT 64
+/*
+ * +-----------------+------------------+
+ * |    ROM (0~31)   | RAM(32~63)       |
+ * +-----------------+------------------+
+ */
+#define KEY_ROM_ENTRY 0
+#define KEY_RAM_ENTRY 32
 static uint8_t g_key_inuse[NKEY_SLOT] = {0,};
+#define KEYSLOT_USE 0
+#define KEYSLOT_NOTUSE 1
 
-static int _set_key_slot(uint8_t num)
+static inline int _is_rom_area(uint8_t num)
 {
-	if (num >= NKEY_SLOT) {
-		return -1;
+	if (num < KEY_RAM_ENTRY) {
+		return 1;
 	}
-	g_key_inuse[num] = 1;
 	return 0;
 }
 
 
-static int _unset_key_slot(uint8_t num)
+static inline int _set_key_slot(uint8_t num)
 {
 	if (num >= NKEY_SLOT) {
 		return -1;
 	}
-	g_key_inuse[num] = 0;
+	g_key_inuse[num] = KEYSLOT_USE;
 	return 0;
 }
 
 
-static int _is_empty_key_slot(uint8_t num)
+static inline int _unset_key_slot(uint8_t num)
+{
+	if (num >= NKEY_SLOT) {
+		return -1;
+	}
+	g_key_inuse[num] = KEYSLOT_NOTUSE;
+	return 0;
+}
+
+
+static inline int _is_empty_key_slot(uint8_t num)
 {
 	if (num >= NKEY_SLOT) {
 		return -1;
 	}
 
-	return g_key_inuse[num] == 0 ? 1 : 0;
+	return g_key_inuse[num];
+}
+
+
+static int hd_handle_rom_key(int cmd, struct seclink_req *req, struct seclink_key_info *info, struct sec_lowerhalf_s *se)
+{
+	if (cmd != SECLINKIOC_GETKEY) {
+		req->res = HAL_INVALID_REQUEST;
+		return 0;
+	}
+	req->res = se->ops->get_key(info->mode, info->key_idx, info->key);
+	return 0;
+}
+
+
+static int hd_handle_ram_key(int cmd, struct seclink_req *req, struct seclink_key_info *info, struct sec_lowerhalf_s *se)
+{
+	int res = _is_empty_key_slot(info->key_idx);
+	SLDRV_LOG("keymgr request cmd(%x) (%d) isempty(%d)\n", cmd, info->mode, res);
+	if (res == -1) {
+		req->res = HAL_INVALID_SLOT_RANGE;
+		return 0;
+	}
+
+	switch (cmd) {
+	case SECLINKIOC_SETKEY:
+		if (res == KEYSLOT_USE) {
+			req->res = HAL_KEY_IN_USE;
+		} else {
+			if (_set_key_slot(info->key_idx) == -1) {
+				req->res = HAL_INVALID_SLOT_RANGE;
+			} else {
+				req->res = se->ops->set_key(info->mode, info->key_idx, info->key, info->prikey);
+				if (req->res != HAL_SUCCESS) {
+					_unset_key_slot(info->key_idx);
+				}
+			}
+		}
+		break;
+	case SECLINKIOC_GETKEY:
+		if (res == KEYSLOT_USE) {
+			req->res = se->ops->get_key(info->mode, info->key_idx, info->key);
+		} else {
+			req->res = HAL_EMPTY_SLOT;
+		}
+		break;
+	case SECLINKIOC_REMOVEKEY:
+		if (res == KEYSLOT_USE) {
+			_unset_key_slot(info->key_idx);
+			req->res = se->ops->remove_key(info->mode, info->key_idx);
+		} else {
+			req->res = HAL_EMPTY_SLOT;
+		}
+		break;
+	case SECLINKIOC_GENERATEKEY:
+		if (res == KEYSLOT_USE) {
+			req->res = HAL_KEY_IN_USE;
+		} else {
+			req->res = se->ops->generate_key(info->mode, info->key_idx);
+			if (req->res == HAL_SUCCESS) {
+				_set_key_slot(info->key_idx);
+			}
+		}
+		break;
+	}
+	return 0;
 }
 
 
 void hd_initialize_key_slot(void)
 {
 	for (int i = 0; i < NKEY_SLOT; i++) {
-		g_key_inuse[i] = 0;
+		g_key_inuse[i] = KEYSLOT_NOTUSE;
 	}
 }
 
@@ -73,6 +156,7 @@ void hd_initialize_key_slot(void)
 int hd_handle_key_request(int cmd, unsigned long arg, void *lower)
 {
 	SLDRV_ENTER;
+	int res = 0;
 
 	struct seclink_req *req = (struct seclink_req *)arg;
 	if (!req) {
@@ -89,57 +173,16 @@ int hd_handle_key_request(int cmd, unsigned long arg, void *lower)
 		return -EINVAL;
 	}
 
-	int res = _is_empty_key_slot(info->key_idx);
-	SLDRV_LOG("keymgr request cmd(%x) (%d) isempty(%d)\n", cmd, info->mode, res);
-	switch (cmd) {
-	case SECLINKIOC_SETKEY:
-		if (!res) {
-			req->res = HAL_KEY_IN_USE;
-		} else {
-			if (_set_key_slot(info->key_idx) == -1) {
-				req->res = HAL_INVALID_SLOT_RANGE;
-			} else {
-				req->res = se->ops->set_key(info->mode, info->key_idx, info->key, info->prikey);
-				if (req->res != HAL_SUCCESS) {
-					_unset_key_slot(info->key_idx);
-				}
-			}
-		}
-		break;
-	case SECLINKIOC_GETKEY:
-		if (!res) {
-			req->res = se->ops->get_key(info->mode, info->key_idx, info->key);
-		} else {
-			if (res == -1) {
-				req->res = HAL_INVALID_SLOT_RANGE;
-			} else {
-				req->res = HAL_EMPTY_SLOT;
-			}
-		}
-		break;
-	case SECLINKIOC_REMOVEKEY:
-		if (!res) {
-			_unset_key_slot(info->key_idx);
-			req->res = se->ops->remove_key(info->mode, info->key_idx);
-		} else {
-			req->res = HAL_EMPTY_SLOT;
-		}
-		break;
-	case SECLINKIOC_GENERATEKEY:
-		if (!res) {
-			req->res = HAL_KEY_IN_USE;
-		} else {
-			if (res == -1) {
-				req->res = HAL_INVALID_SLOT_RANGE;
-			} else {
-				req->res = se->ops->generate_key(info->mode, info->key_idx);
-				if (req->res == HAL_SUCCESS) {
-					_set_key_slot(info->key_idx);
-				}
-			}
-		}
-		break;
+	if (info->key_idx < 0 || info->key_idx > NKEY_SLOT) {
+		req->res = HAL_INVALID_SLOT_RANGE;
+		return 0;
 	}
 
-	return 0;
+	if (_is_rom_area(info->key_idx)) {
+		res = hd_handle_rom_key(cmd, req, info, se);
+	} else {
+		res = hd_handle_ram_key(cmd, req, info, se);
+	}
+
+	return res;
 }
