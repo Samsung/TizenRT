ae4c71b571607c73c8ebff3339db2b5899b2adb5
lexi_zhao <lexi_zhao@realsil.com.cn>
2021-02-19 14:47:59 +0800

    Remove duplicated head files

--- os/board/rtl8721csm/src/component/common/bluetooth/realtek/sdk/board/amebad/src/rtl8721d_bt.h
+++
@@ -1,7 +0,0 @@
-#ifndef _RTL8721D_BT_H_
-#define _RTL8721D_BT_H_
-
-const unsigned char *bt_get_patch_code_8721d(_adapter *padapter);
-unsigned int bt_get_patch_code_len_8721d(_adapter *padapter);
-
-#endif /*  _RTL8721D_BT_H_ */
