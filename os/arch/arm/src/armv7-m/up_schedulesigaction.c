cd03d798a4226bd1e3e89ca3fc393c233217d9f2
Kishore S N <kishore.sn@samsung.com>
2021-04-07 10:04:55 +0530

    os/arch/arm: Fix INVPC UsageFault during Wifi Stress test on RTK
    While running wifi stress test on RTK board, following crash occurs:
    =====================================================================
    WIFI deinitialized[WM] T6 <-- _handle_request
    up_usagefault: PANIC!!! Usagefault occurred while ecuting instruction at address : 0x0e005b55
    up_usagefault: CFLTS: 0x00040000
    ...
    up_registerdump: R0: 163844 10063870 10063890 000000e0 0000002f 10028b14 0000002f 10061fff
    up_registerdump: R8: 00000001 000000010028b30 100639a0 00000000 10063870 ffffffb8 0e005b55
    up_regierdump: xPSR: 01000000 BASEPRI: 00000020 CONTROL: 00000000
    upegisterdump: EXC_RETURN: ffffffb0
    ======================================================================
    
    Analysis:
    - When child task is terminated, a signal is sent to parent task. The crash is happening
    at the time of sending this signal.
    - The Usage Fault INVPC has occurred becuase of improper xPSR value upon exception return.
    - In this case, we try to send signal to the wifi msg handler task, which is in blocked state.
    - So, at the time of sending signal, SVC exception is generated to perform the context switch.
    - While returning from the SVC exception, the HW performs an integrity check and finds that the
    IPSR value in xPSR register does not match the exception number.
    - This is becuase, we are overwriting the IPSR value in tcb->xcp[REG_XPSR] in up_schedulesigaction.
    - It is not necessary to set this bit in the xPSR because, it is set during task init and needs to
    be always set in case of cortex-m
    - Hence, we remove this setting from up_schedulesigaction.
    
    Signed-off-by: Kishore S N <kishore.sn@samsung.com>

--- os/arch/arm/src/armv7-m/up_schedulesigaction.c
+++ os/arch/arm/src/armv7-m/up_schedulesigaction.c
@@ -186,7 +186,6 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
 #else
 				current_regs[REG_PRIMASK] = 1;
 #endif
-				current_regs[REG_XPSR] = ARMV7M_XPSR_T;
 #ifdef CONFIG_BUILD_PROTECTED
 				current_regs[REG_LR] = EXC_RETURN_PRIVTHR;
 #endif
@@ -231,7 +230,6 @@ void up_schedule_sigaction(struct tcb_s *tcb, sig_deliver_t sigdeliver)
 #else
 			tcb->xcp.regs[REG_PRIMASK] = 1;
 #endif
-			tcb->xcp.regs[REG_XPSR] = ARMV7M_XPSR_T;
 #ifdef CONFIG_BUILD_PROTECTED
 			tcb->xcp.regs[REG_LR] = EXC_RETURN_PRIVTHR;
 #endif
