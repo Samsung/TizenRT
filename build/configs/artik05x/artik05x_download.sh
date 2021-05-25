89cd7b5619441df2c1c8f3664903d4f1f3c8eccf
Abhishek Akkabathula <a.akkabathul@samsung.com>
2021-05-25 13:16:40 +0530

    build/configs/artik05x: Add common download script support.
        - artik05x will use build/configs/common_download.sh for download.
        - artik05x_download.sh will contain board specific functions.
    
    Signed-off-by: Abhishek Akkabathula <a.akkabathul@samsung.com>

--- build/configs/artik05x/artik05x_download.sh
+++ build/configs/artik05x/artik05x_download.sh
@@ -20,29 +20,35 @@
 # File   : artik05x_download.sh
 # Description : Download script for ARTIK 05X
 
-THIS_PATH=`test -d ${0%/*} && cd ${0%/*}; pwd`
-
 # When location of this script is changed, only OS_DIR_PATH should be changed together!!!
-OS_DIR_PATH=${THIS_PATH}/../../../os
 
-source ${OS_DIR_PATH}/.config
-
-USBRULE_PATH=${THIS_PATH}/../usbrule.sh
-USBRULE_BOARD="artik"
-USBRULE_IDVENDOR="0403"
-USBRULE_IDPRODUCT="6010"
+USBRULE_PATH=${TOP_PATH}/build/configs/usbrule.sh
 
 PARTMAP_DIR_PATH=${THIS_PATH}
-PARTITION_KCONFIG=${OS_DIR_PATH}/board/common/Kconfig
-BUILD_DIR_PATH=${OS_DIR_PATH}/../build
-CONFIGS_DIR_PATH=${BUILD_DIR_PATH}/configs
-OUTPUT_BINARY_PATH=${BUILD_DIR_PATH}/output/bin
+PARTITION_KCONFIG=${OS_PATH}/board/common/Kconfig
+BUILD_DIR_PATH=${OS_PATH}/../build
+CONFIGS_DIR_PATH=${TOP_PATH}/build/configs
+OUTPUT_BINARY_PATH=${TOP_PATH}/build/output/bin
 ARTIK05X_DIR_PATH=${CONFIGS_DIR_PATH}/artik05x
 SCRIPTS_PATH=${ARTIK05X_DIR_PATH}/scripts
 
-TIZENRT_BIN=${OUTPUT_BINARY_PATH}/tinyara_head.bin
-TIZENRT_APPS_BIN=${OUTPUT_BINARY_PATH}/tinyara_user.bin
-SMARTFS_BIN=${OUTPUT_BINARY_PATH}/artik05x_smartfs.bin
+if [[ "${CONFIG_ARCH_BOARD_ARTIK053}" == "y" ]]; then
+	BOARD_NAME=artik053
+	SECURE=""
+fi
+
+if [[ "${CONFIG_ARCH_BOARD_ARTIK053S}" == "y" ]]; then
+	BOARD_NAME=artik053s
+	SECURE="-signed"
+fi
+
+if [[ "${CONFIG_ARCH_BOARD_ARTIK055S}" == "y" ]]; then
+	BOARD_NAME=artik055s
+	SECURE="-signed"
+fi
+
+BOARD_DIR_PATH=${BUILD_DIR_PATH}/configs/${BOARD_NAME}
+FW_DIR_PATH=${BOARD_DIR_PATH}/bin
 
 OPENOCD_DIR_PATH=${BUILD_DIR_PATH}/tools/openocd
 if [[ $OSTYPE == "darwin"* ]]; then
@@ -62,245 +68,60 @@ OPENOCD=${OPENOCD_BIN_PATH}/openocd
 
 CFG_FILE=artik05x.cfg
 
-usage() {
-	cat <<EOF
-USAGE: `basename $0` [OPTIONS]
-OPTIONS:
-	[--board[="<board-name>"]]
-	[--secure]
-	[ALL | ROMFS | BL1 | BL2 | SSSFW | WLANFW | USERFS]
-
-For examples:
-	`basename $0` --board=artik053 ALL
-	`basename $0` --board=artik053 OS ROMFS
-	`basename $0` --board=artik053s --verify
-	`basename $0` --board=artik055s --secure
-
-Options:
-	--board[="<board-name>"]      select target board-name
-	--secure                      choose secure mode
-	--verify                      verify downloaded image if you need
-	ALL                           write each firmware image into FLASH
-	ROMFS                         write ROM File System image into FLASH
-	BL1                           write Primary Bootloader image into FLASH
-	BL2                           write Secondary Bootloader image into FLASH
-	SSSFW                         write Secure Element Firmware image into FLASH
-	WLANFW                        write WiFi Firmware image into FLASH
-	USERFS                        write user FS to SMARTFS partition
-
-EOF
-}
-
-# Exit if the file is not there
-ensure_file()
-{
-	if [ ! -f "${1:?}" ]; then
-		echo "missing file $1"
-		exit 1
-	fi
-}
-
-# Check if the file is present
-is_file_present()
-{
-	if [ ! -f "${1:?}" ]; then
-	    return 0
-	fi
-	return 1
-}
-
-compute_ocd_commands_user()
-{
-	local commands=
-	ensure_file ${SMARTFS_BIN}
-	commands+="flash_write userfs ${SMARTFS_BIN} ${VERIFY}"
-	echo ${commands}
-}
-
-compute_ocd_commands()
-{
-	local commands=
-	for part in "$@"; do
-		case "${part}" in
-			bl1)
-				ensure_file ${FW_DIR_PATH}/${part}.bin
-				commands+="flash_protect off; flash_write bl1 ${FW_DIR_PATH}/bl1.bin ${VERIFY}; flash_protect on; "
-				;;
-			bl2|sssfw|wlanfw)
-				ensure_file ${FW_DIR_PATH}/${part}.bin
-				commands+="flash_write ${part} ${FW_DIR_PATH}/${part}.bin ${VERIFY}; "
-				;;
-			os)
-				ensure_file ${TIZENRT_BIN}
-				commands+="flash_write ${part} ${TIZENRT_BIN} ${VERIFY}; "
-				;;
-			apps)
-				ensure_file ${TIZENRT_APPS_BIN}
-				commands+="flash_write ${part} ${TIZENRT_APPS_BIN} ${VERIFY}; "
-				;;
-			ota)
-			        is_file_present ${OUTPUT_BINARY_PATH}/ota.bin
-			        if [[ $? -eq 1 ]]; then
-				    commands+="flash_write ${part} ${OUTPUT_BINARY_PATH}/ota.bin ${VERIFY}; "
-			         else
-				     echo "#NOTE: ${OUTPUT_BINARY_PATH}/ota.bin is NOT present";
-				fi
-				;;
-			rom)
-			         is_file_present ${OUTPUT_BINARY_PATH}/romfs.img
-				 if [[ $? -eq 1 ]]; then
-				     commands+="flash_write ${part} ${OUTPUT_BINARY_PATH}/romfs.img ${VERIFY}; "
-				 else
-				     echo "#NOTE: ${OUTPUT_BINARY_PATH}/romfs.img is NOT present";
-				 fi
-				;;
-			zoneinfo)
-			         is_file_present ${OUTPUT_BINARY_PATH}/zoneinfo.img
-				 if [[ $? -eq 1 ]]; then
-				     commands+="flash_write ${part} ${OUTPUT_BINARY_PATH}/zoneinfo.img ${VERIFY}; "
-				 else
-				     echo "#NOTE: ${OUTPUT_BINARY_PATH}/zoneinfo.img is NOT present";
-				 fi
-				;;
-			loadparam)
-			         is_file_present ${OUTPUT_BINARY_PATH}/loadparam
-				 if [[ $? -eq 1 ]]; then
-				     commands+="flash_write ${part} ${OUTPUT_BINARY_PATH}/loadparam ${VERIFY}; "
-				 else
-				     echo "#NOTE: ${OUTPUT_BINARY_PATH}/loadparam is NOT present";
-				 fi
-				;;
-			micom)
-			         is_file_present ${OUTPUT_BINARY_PATH}/micom
-				 if [[ $? -eq 1 ]]; then
-				     commands+="flash_write ${part} ${OUTPUT_BINARY_PATH}/micom ${VERIFY}; "
-				 else
-				     echo "#NOTE: ${OUTPUT_BINARY_PATH}/micom is NOT present";
-				 fi
-				;;
-			wifi)
-			         is_file_present ${OUTPUT_BINARY_PATH}/wifi
-				 if [[ $? -eq 1 ]]; then
-				     commands+="flash_write ${part} ${OUTPUT_BINARY_PATH}/wifi ${VERIFY}; "
-				 else
-				     echo "#NOTE: ${OUTPUT_BINARY_PATH}/wifi is NOT present";
-				 fi
-				;;
-			*)
-				echo "#NOTE#: No binary available for ${part}"
-
-				;;
-		esac
-	done
-	echo ${commands}
-}
-
-download()
-{
-	parts=$1;
-
-	if [[ -n $parts ]] && [[ "$parts" != "all" ]]
-	then
-		echo "##Download $parts"
-	else
-	        default_parts=`grep -A 2 'config FLASH_PART_NAME' ${PARTITION_KCONFIG} | sed -n 's/\tdefault "\(.*\)".*/\1/p'`
-		configured_parts=${CONFIG_FLASH_PART_NAME:=${default_parts}}
-		parts=`echo $configured_parts | sed "s/,/ /g"`
-	fi
-
-	# Make Openocd commands for parts
-	if [[ "$parts" == "userfs" ]]
-	then
-		commands=$(compute_ocd_commands_user)
-		echo "ocd command to run: ${commands}"
-	else
-	# Make Openocd commands for parts
-		commands=$(compute_ocd_commands ${parts})
-		echo "ocd command to run: ${commands}"
-	fi
-
-	# Generate Partition Map
+function pre_download(){
 	${SCRIPTS_PATH}/partition_gen.sh
+}
 
+function board_download()
+{
+	commands=""
+	case $5 in
+		bl1)
+			if [ ! -f ${FW_DIR_PATH}/$3 ];then
+				echo "#NOTE#: No binary available for $5"
+			else
+				commands+="flash_protect off; flash_write $5 ${FW_DIR_PATH}/$3; flash_protect on; "
+			fi
+			;;
+		bl2|sssfw|wlanfw)
+			if [ ! -f ${FW_DIR_PATH}/$3 ];then
+				echo "#NOTE#: No binary available for $5"
+			else
+				commands+="flash_write $5 ${FW_DIR_PATH}/$3 ${VERIFY}; "
+			fi
+			;;
+		os)
+			if [ ! -f ${BIN_PATH}/$3 ];then
+				echo "#NOTE#: No binary available for $5"
+			else
+				commands+="flash_write $5 ${BIN_PATH}/$3${SECURE} ${VERIFY}; "
+			fi
+			;;
+		ota|rom|zoneinfo|loadparam|micom|wifi|userfs)
+			if [ ! -f ${BIN_PATH}/$3 ];then
+				echo "#NOTE#: No binary available for $5"
+			else
+				commands+="flash_write $5 ${BIN_PATH}/$3 ${VERIFY}; "
+			fi
+			;;
+		*)
+			echo "#NOTE#: No binary available for $5"
+	esac
 	# Download all binaries using openocd script
+	echo "${commands}"
 	pushd ${OPENOCD_DIR_PATH} > /dev/null
 	${OPENOCD} -f ${CFG_FILE} -s ${SCRIPTS_PATH} -c "${commands}" -c "init; reset; exit" || exit 1
 	popd > /dev/null
-
-	echo "Flash DONE"
 }
 
-erase()
+function board_erase()
 {
-	case $1 in
-		ota)
-			PART_NAME=ota
-			;;
-		userfs)
-			PART_NAME=userfs
-			;;
-		all)
-			PART_NAME=all
-			;;
-		*)
-			echo "${$1} is not supported"
-			exit 1
-			;;
-	esac
 
-	# Generate Partition Map
-	${SCRIPTS_PATH}/partition_gen.sh
-
-	# Download all binaries using openocd script
 	pushd ${OPENOCD_DIR_PATH} > /dev/null
-	${OPENOCD} -f ${CFG_FILE} -s ${SCRIPTS_PATH} -c "flash_erase_part ${PART_NAME};	exit" || exit 1
+	${OPENOCD} -f ${CFG_FILE} -s ${SCRIPTS_PATH} -c "flash_erase_part $4;	exit" || exit 1
 	popd > /dev/null
 }
 
-if test $# -eq 0; then
-	usage 1>&2
-	exit 1
-fi
-
-while test $# -gt 0; do
-	case "$1" in
-		-*=*) optarg=`echo "${1,,}" | sed 's/[-_a-zA-Z0-9]*=//'` ;;
-		*) optarg= ;;
-	esac
-
-	case ${1,,} in
-		--board*)
-			BOARD_NAME=$optarg
-			BOARD_DIR_PATH=${BUILD_DIR_PATH}/configs/${BOARD_NAME}
-			FW_DIR_PATH=${BOARD_DIR_PATH}/bin
-			if [ ! -d ${BOARD_DIR_PATH} ]; then
-				usage 1>&2
-				exit 1
-			fi
-			;;
-		--secure)
-			TIZENRT_BIN=${TIZENRT_BIN}-signed
-			;;
-		--verify)
-			VERIFY=verify
-			;;
-		all|os|apps|rom|bl1|bl2|sssfw|wlanfw|ota|micom|wifi|userfs|zoneinfo)
-			download ${1,,}
-			;;
-		usbrule)
-			${USBRULE_PATH} ${USBRULE_BOARD} ${USBRULE_IDVENDOR} ${USBRULE_IDPRODUCT} || exit 1
-			;;
-		erase)
-			while test $# -gt 1
-			do
-				erase ${2,,}
-				shift
-			done
-			;;
-		*)
-			usage 1>&2
-			exit 1
-			;;
-	esac
-	shift
-done
+function post_download(){
+	:;
+}
