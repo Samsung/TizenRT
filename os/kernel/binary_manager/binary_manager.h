d5f8aff20666ce4754562b03eee75058e17e54cb
jeongchanKim <jc_.kim@samsung.com>
2021-11-10 16:17:13 +0900

    kernel/binary_manager.h : Add prototype of binary_manager_update_bootparam
    Signed-off-by: jeongchanKim <jc_.kim@samsung.com>

--- os/kernel/binary_manager/binary_manager.h
+++ os/kernel/binary_manager/binary_manager.h
@@ -279,6 +279,7 @@ int binary_manager_get_index_with_name(char *bin_name);
 int binary_manager_scan_bootparam(binmgr_bpinfo_t *bp_info);
 binmgr_bpdata_t *binary_manager_get_bpdata(void);
 int binary_manager_get_inactive_path(int requester_pid, char *bin_name);
+void binary_manager_update_bootparam(int requester_pid, uint8_t type);
 
 /****************************************************************************
  * Binary Manager Main Thread
