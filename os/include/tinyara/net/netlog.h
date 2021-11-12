e4fb673ba2ae0db4ed300ee4e613c234dc285f83
pillip <pilseob.kang@samsung.com>
2021-11-12 19:34:32 +0900

    net/netlog: add network log for user space
    add network log for user space

--- os/include/tinyara/net/netlog.h
+++ os/include/tinyara/net/netlog.h
@@ -21,26 +21,17 @@
 #include <debug.h>
 #endif
 
-/*  network log for kernel space */
-#define NET_LOGK(tag, fmt, args...) \
-	nwdbg(fmt, ##args)
-#define NET_LOGKE(tag, fmt, args...) \
-	ndbg("%d " tag "[ERR]\t" fmt, __LINE__, ##args)
-#define NET_LOGKI(tag, fmt, args...) \
-	nwdbg("%d " tag "[INFO]\t" fmt, __LINE__, ##args)
-#define NET_LOGKV(tag, fmt, args...) \
-	nvdbg("%d " tag "[VERB]\t" fmt, __LINE__, ##args)
+typedef enum {
+	NL_COLOR_DEFAULT,
+	NL_COLOR_RED,
+	NL_COLOR_GREEN,
+	NL_COLOR_YELLOW,
+} nl_color_e;
 
-/*  To-do: change dbg to user space API */
-/*  network log for user space */
-#define NET_LOG(tag, fmt, args...) \
-	nwdbg(fmt, ##args)
-#define NET_LOGE(tag, fmt, args...) \
-	ndbg("%d " tag "[ERR]\t" fmt, __LINE__, ##args)
-#define NET_LOGI(tag, fmt, args...) \
-	nwdbg("%d " tag "[INFO]\t" fmt, __LINE__, ##args)
-#define NET_LOGV(tag, fmt, args...) \
-	nvdbg("%d " tag "[VERB]\t" fmt, __LINE__, ##args)
+typedef enum {
+	NL_OPT_ENABLE,
+	NL_OPT_DISABLE,
+} nl_options;
 
 typedef struct {
 	int idx;
@@ -51,6 +42,7 @@ typedef struct {
 typedef netmgr_logger *netmgr_logger_p;
 
 typedef enum {
+	NL_MOD_UNKNOWN,
 	/*  user level module */
 	NL_MOD_WIFI_MANAGER,
 	NL_MOD_NETLIB,
@@ -63,7 +55,7 @@ typedef enum {
 	NL_LEVEL_ERROR,
 	NL_LEVEL_INFO,
 	NL_LEVEL_VERB,
-  NL_LEVEL_UNKNOWN,
+	NL_LEVEL_UNKNOWN,
 } netlog_log_level_e;
 
 /*
@@ -99,7 +91,12 @@ int netlogger_serialize(netmgr_logger_p log, char **buf);
  * DESCRIPTION: print log to stdout
  * RETURN: return log length
  */
-int netlogger_print(netlog_module_e mod, netlog_log_level_e level, const char *format, ...);
+int netlogger_print(netlog_module_e mod,
+					netlog_log_level_e level,
+					const char *func,
+					const char *file,
+					const int line,
+					const char *fmt, ...);
 
 /*
  * DESCRIPTION: set level
@@ -108,7 +105,65 @@ int netlogger_print(netlog_module_e mod, netlog_log_level_e level, const char *f
 int netlog_set_level(netlog_module_e module, netlog_log_level_e level);
 
 /*
+ * DESCRIPTION: set color
+ * RETURN: if succeeded return 0 else return -1
+ */
+int netlog_set_color(netlog_module_e module, nl_color_e color);
+
+/*
+ * DESCRIPTION: configure timer in log
+ * RETURN: if succeeded return 0 else return -1
+ */
+int netlog_set_timer(nl_options opt);
+
+/*
+ * DESCRIPTION: configure timer in log
+ * RETURN: if succeeded return 0 else return -1
+ */
+int netlog_set_function(nl_options opt);
+
+/*
+ * DESCRIPTION: configure timer in log
+ * RETURN: if succeeded return 0 else return -1
+ */
+int netlog_set_file(nl_options opt);
+
+/*
  * DESCRIPTION: reset all log configurations
  * RETURN: if succeeded return 0 else return -1
  */
 int netlog_reset(void);
+
+/**************************************************
+ * API
+ ***************************************************/
+/*  network log for kernel space */
+#define NET_LOGK(tag, fmt, args...) \
+	nwdbg(fmt, ##args)
+#define NET_LOGKE(tag, fmt, args...) \
+	ndbg("%d " tag "[ERR]\t" fmt, __LINE__, ##args)
+#define NET_LOGKI(tag, fmt, args...) \
+	nwdbg("%d " tag "[INFO]\t" fmt, __LINE__, ##args)
+#define NET_LOGKV(tag, fmt, args...) \
+	nvdbg("%d " tag "[VERB]\t" fmt, __LINE__, ##args)
+
+/*  To-do: change printf to netlogger_print */
+/*  network log for user space */
+#ifdef __LINUX__
+#define NET_LOG(mod, fmt, args...) \
+	netlogger_print(mod, NL_LEVEL_UNKNOWN, __FUNCTION__, __FILE__, __LINE__, fmt, ##args);
+#define NET_LOGE(mod, fmt, args...) \
+	netlogger_print(mod, NL_LEVEL_ERROR, __FUNCTION__, __FILE__, __LINE__, fmt, ##args);
+#define NET_LOGI(mod, fmt, args...) \
+	netlogger_print(mod, NL_LEVEL_INFO, __FUNCTION__, __FILE__, __LINE__, fmt, ##args);
+#define NET_LOGV(mod, fmt, args...) \
+	netlogger_print(mod, NL_LEVEL_VERB, __FUNCTION__, __FILE__, __LINE__, fmt, ##args);
+#else
+#define NET_LOG(mod, fmt, args...) \
+	printf(mod fmt, ##args);
+#define NET_LOGE(mod, fmt, args...) \
+	printf(mod"[ERR] " fmt, ##args);
+#define NET_LOGI(mod, fmt, args...) \
+	printf(mod"[INFO] " fmt, ##args);
+#define NET_LOGV(mod, fmt, args...)
+#endif
