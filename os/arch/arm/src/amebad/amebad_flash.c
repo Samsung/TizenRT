fcc86def193a6d3125191d4c52012f37daa56c97
jeongchanKim <jc_.kim@samsung.com>
2021-12-07 14:14:42 +0900

    os : Replace lldbg to printf
    lldbg writes the log to uart directly, not using uart buffer.
    In this case, log can be mixed.
    Those changed logs should be printed always, so print them through printf instead of lldbg.
    
    Signed-off-by: jeongchanKim <jc_.kim@samsung.com>

--- os/arch/arm/src/amebad/amebad_flash.c
+++ os/arch/arm/src/amebad/amebad_flash.c
@@ -352,7 +352,7 @@ FAR struct mtd_dev_s *up_flashinitialize(void)
 		priv->nsectors = AMEBAD_NSECTORS;
 		u8 chip_id[4];
 		flash_read_id(NULL, chip_id, 4);
-		lldbg("Manufacturer : %u memory type : %u capacity : %u\n", chip_id[0], chip_id[1], chip_id[2]);
+		printf("Manufacturer : %u memory type : %u capacity : %u\n", chip_id[0], chip_id[1], chip_id[2]);
 		return (FAR struct mtd_dev_s *)priv;
 	}
 	return NULL;
