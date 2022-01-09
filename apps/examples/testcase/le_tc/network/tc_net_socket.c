bf2d121d140ef92ce5c30224489d030b56147cd9
pilseob.kang <pilseob.kang@samsung.com>
2022-01-10 08:51:10 +0900

    testcase/network: remove AF_AX25
    AF_AX25 is not supported to TizenRT

--- apps/examples/testcase/le_tc/network/tc_net_socket.c
+++ apps/examples/testcase/le_tc/network/tc_net_socket.c
@@ -220,24 +220,6 @@ static void tc_net_socket_af_x25_sock_stream_n(void)
 }
 #endif
 
-#ifdef AF_AX25
-/**
- * @testcase		tc_net_socket_af_ax25_sock_stream_n
- * @brief
- * @scenario
- * @apicovered		socket()
- * @precondition
- * @postcondition
- */
-static void tc_net_socket_af_ax25_sock_stream_n(void)
-{
-	int fd = -1;
-	fd = socket(AF_AX25, SOCK_STREAM, 0);
-	TC_ASSERT_EQ_CLEANUP("socket", fd, -1, close(fd))
-	TC_SUCCESS_RESULT()
-}
-#endif
-
 #ifdef AF_PACKET
 /**
  * @testcase		tc_net_socket_af_packet_sock_stream_n
@@ -656,24 +638,6 @@ static void tc_net_socket_af_x25_sock_dgram_n(void)
 {
 	int fd = -1;
 	fd = socket(AF_X25, SOCK_DGRAM, 0);
-	TC_ASSERT_EQ_CLEANUP("socket", fd, -1, close(fd))
-	TC_SUCCESS_RESULT()
-}
-#endif
-
-#ifdef AF_AX25
-/**
- * @testcase		tc_net_socket_af_ax25_sock_dgram_n
- * @brief
- * @scenario
- * @apicovered		socket()
- * @precondition
- * @postcondition
- */
-static void tc_net_socket_af_ax25_sock_dgram_n(void)
-{
-	int fd = -1;
-	fd = socket(AF_AX25, SOCK_DGRAM, 0);
 	TC_ASSERT_EQ_CLEANUP("socket", fd, -1, close(fd))
 	TC_SUCCESS_RESULT()
 }
