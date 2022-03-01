dfb0ccd2c08eb29b2b9b482b4b9a179ab08ee608
Abhishek Akkabathula <a.akkabathul@samsung.com>
2022-03-01 13:33:01 +0530

    fs/procfs : extend procfs to support log dump feature
    Signed-off-by: Abhishek Akkabathula <a.akkabathul@samsung.com>

--- os/fs/procfs/fs_procfs.c
+++ os/fs/procfs/fs_procfs.c
@@ -96,6 +96,9 @@ extern const struct procfs_operations proc_operations;
 extern const struct procfs_operations cpuload_operations;
 extern const struct procfs_operations uptime_operations;
 extern const struct procfs_operations version_operations;
+#if defined(CONFIG_LOG_DUMP)
+extern const struct procfs_operations logsave_operations;
+#endif
 
 /* This is not good.  These are implemented in drivers/mtd.  Having to
  * deal with them here is not a good coupling.
@@ -127,6 +130,10 @@ static const struct procfs_entry_s g_procfsentries[] = {
 
 #if defined(CONFIG_SCHED_CPULOAD) && !defined(CONFIG_FS_PROCFS_EXCLUDE_CPULOAD)
 	{"cpuload", &cpuload_operations},
+#endif
+
+#if defined(CONFIG_LOG_DUMP)
+	{"logsave", &logsave_operations},
 #endif
 
 #if defined(CONFIG_FS_SMARTFS) && !defined(CONFIG_FS_PROCFS_EXCLUDE_SMARTFS)
