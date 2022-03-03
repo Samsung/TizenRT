7373b1e1ee41317c5785c76ee23e3257ea22a6fa
Amogh Hassija <a.hassija@samsung.com>
2022-03-03 14:15:11 +0530

    apps/examples/smartfs: Revert changes related to append file operation
    - Append File Operation improvements are not guaranteed/indicated with the optimized buffer size.
    - We keep Read File Operation improvements which are significant as a suggestion to the programmer.
    - Modularity is retained considering the possible addition of any future improvement suggestions.
    
    Signed-off-by: Amogh Hassija <a.hassija@samsung.com>

--- apps/examples/smartfs/buffer_optimization/buffer_optimization_main.c
+++ apps/examples/smartfs/buffer_optimization/buffer_optimization_main.c
@@ -42,8 +42,6 @@
 #define SAMPLE_FILE "/mnt/sample_file"
 #define TIMER_DEVNAME "/dev/timer%d"
 #define READ_TEST_ITR 100	//No. of times file will be read for testing
-#define APPEND_TEST_ITR 1	//No. of times file will be appended for testing
-#define TOTAL_APPEND_SIZE 10240	//Amount of data to be appended during the test
 #define DEFAULT_BUF_SIZE_S 512	//Smaller default buffer size used for demonstration testng
 #define DEFAULT_BUF_SIZE_L 1024	//Larger default buffer size used for demonstration testing
 
@@ -116,160 +114,6 @@ error_with_fd:
 }
 
 /****************************************************************************
- * Name: append_test_util
- *
- * Description: Perform appending of data to the file, buffer by buffer
- *
- ****************************************************************************/
-
-static int append_test_util(int buf_size)
-{
-	int ret = OK;
-	int fd;
-	int itr = 0;
-	int total = TOTAL_APPEND_SIZE;
-
-	while (itr < APPEND_TEST_ITR) {
-		fd = open(SAMPLE_FILE, O_WROK | O_APPEND);
-		if (fd < 0) {
-			printf("Unable to open sample file %s for appending, Error: %d\n", SAMPLE_FILE, errno);
-			return ERROR;
-		}
-
-		while (total > 0) {
-			if (total < buf_size) {
-				buf_size = total;
-			}
-			ret = write(fd, g_buf, buf_size);
-			if (ret < 0) {
-				printf("Unable to write to file %s, Error: %d\n", SAMPLE_FILE, errno);
-				ret = errno;
-				goto error_with_fd;
-			}
-			total -= ret;
-		}
-		close(fd);
-		itr += 1;
-	}
-
-	ret = OK;
-error_with_fd:
-	close(fd);
-	return ret;
-}
-
-/****************************************************************************
- * Name: append_test
- *
- * Description: Set up the timer and run the append test for different
- *              buffer sizes.
- *
- ****************************************************************************/
-
-static int append_test(int frt_fd, int file_size)
-{
-	int ret;
-	int buf_size;
-	struct timer_status_s time_1, time_2;
-	uint32_t time_append;
-	struct stat st;
-
-
-	ret = ioctl(frt_fd, TCIOC_GETSTATUS, (unsigned long)(uintptr_t)&time_1);
-	if (ret < 0) {
-		fprintf(stderr, "ERROR: Failed to get Free Run Timer status: %d\n", errno);
-		return ret;
-	}
-
-	ret = append_test_util(DEFAULT_BUF_SIZE_S);
-	if (ret != OK) {
-		printf("Appending with buffer size %d failed\n", DEFAULT_BUF_SIZE_S);
-		return ret;
-	}
-
-	ret = ioctl(frt_fd, TCIOC_GETSTATUS, (unsigned long)(uintptr_t)&time_2);
-	if (ret < 0) {
-		fprintf(stderr, "ERROR: Failed to get Free Run Timer status: %d\n", errno);
-		return ret;
-	}
-
-	time_append = time_2.timeleft - time_1.timeleft;
-	print_test_result("appending", TOTAL_APPEND_SIZE, APPEND_TEST_ITR, DEFAULT_BUF_SIZE_S, time_append);
-
-	unlink(SAMPLE_FILE);
-
-	ret = create_test_file(file_size * 1024);
-	if (ret != OK) {
-		printf("Unable to complete test\n");
-		return -1;
-	}
-
-	ret = ioctl(frt_fd, TCIOC_GETSTATUS, (unsigned long)(uintptr_t)&time_1);
-	if (ret < 0) {
-		fprintf(stderr, "ERROR: Failed to get Free Run Timer status: %d\n", errno);
-		return ret;
-	}
-
-	ret = append_test_util(DEFAULT_BUF_SIZE_L);
-	if (ret != OK) {
-		printf("Appending with buffer size %d failed\n", DEFAULT_BUF_SIZE_L);
-		return ret;
-	}
-
-	ret = ioctl(frt_fd, TCIOC_GETSTATUS, (unsigned long)(uintptr_t)&time_2);
-	if (ret < 0) {
-		fprintf(stderr, "ERROR: Failed to get Free Run Timer status: %d\n", errno);
-		return ret;
-	}
-
-	time_append = time_2.timeleft - time_1.timeleft;
-	print_test_result("appending", TOTAL_APPEND_SIZE, APPEND_TEST_ITR, DEFAULT_BUF_SIZE_L, time_append);
-
-	unlink(SAMPLE_FILE);
-
-	ret = create_test_file(file_size * 1024);
-	if (ret != OK) {
-		printf("Unable to complete test\n");
-		return -1;
-	}
-
-	ret = stat("/mnt", &st);
-	if (ret < 0) {
-		printf("Stat to get optimal read buffer size failed, Errno = %d\n", errno);
-		return -1;
-	}
-	buf_size = st.st_blksize;
-
-	ret = ioctl(frt_fd, TCIOC_GETSTATUS, (unsigned long)(uintptr_t)&time_1);
-	if (ret < 0) {
-		fprintf(stderr, "ERROR: Failed to get Free Run Timer status: %d\n", errno);
-		return ret;
-	}
-
-	ret = append_test_util(buf_size);
-	if (ret != OK) {
-		printf("Appending with buffer size %d failed\n", buf_size);
-		return ret;
-	}
-
-	ret = ioctl(frt_fd, TCIOC_GETSTATUS, (unsigned long)(uintptr_t)&time_2);
-	if (ret < 0) {
-		fprintf(stderr, "ERROR: Failed to get Free Run Timer status: %d\n", errno);
-		return ret;
-	}
-
-	time_append = time_2.timeleft - time_1.timeleft;
-	printf("TEST WITH OPTIMIZED BUFFER\n");
-	print_test_result("appending", TOTAL_APPEND_SIZE, APPEND_TEST_ITR, buf_size, time_append);
-
-	unlink(SAMPLE_FILE);
-
-	printf("================================= APPEND TESTS COMPLETE =================================\n\n");
-
-	return OK;
-}
-
-/****************************************************************************
  * Name: read_test_util
  *
  * Description: Perform a back-to-back read of the entire file's contents,
@@ -324,6 +168,8 @@ static int read_test(int frt_fd, int file_size)
 	struct timer_status_s time_1, time_2;
 	uint32_t time_read;
 	struct stat st;
+
+	printf("================================== STARTING READ TESTS ==================================\n\n");
 
 	ret = ioctl(frt_fd, TCIOC_GETSTATUS, (unsigned long)(uintptr_t)&time_1);
 	if (ret < 0) {
@@ -432,18 +278,18 @@ int buffer_optimization_main(int argc, char *argv[])
 	} else {
 		printf("\nCorrect test command usage is: smartfs_buf_opt <Timer Device No.> <File Size in Kb>\n");
 		printf("-> Read test example will read the file back to back, one buffer size at a time\n");
-		printf("-> Append test example will append 10Kb data to the file, one buffer size at a time\n");
 		return -1;
 	}
 
 	snprintf(timer_path, _POSIX_PATH_MAX, TIMER_DEVNAME, timer_devno);
-	printf("Timer path = %s\n", timer_path);
+	printf("Timer path = %s\nCreating Test File\n", timer_path);
 
 	ret = create_test_file(file_size * 1024);
 	if (ret != OK) {
 		printf("Unable to create test file, ret: %d\n", ret);
 		return -1;
 	}
+	printf("Test File Creation Successful\n");
 
 	frt_fd = open(timer_path, O_RDONLY);
 	if (frt_fd < 0) {
@@ -466,20 +312,11 @@ int buffer_optimization_main(int argc, char *argv[])
 	if (ret != OK) {
 		printf("Read test failed on file: %s, ret: %d\n", SAMPLE_FILE, ret);
 		ret = -1;
-		goto error_with_open_timer_dev;
 	}
 
-	ret = append_test(frt_fd, file_size);
-	if (ret != OK) {
-		printf("Append test failed on file: %s, ret: %d\n", SAMPLE_FILE, ret);
-		ret = -1;
-	}
-
-error_with_open_timer_dev:
 	if (ioctl(frt_fd, TCIOC_STOP, 0) < 0) {
 		fprintf(stderr, "ERROR: Failed to stop the Free Run Timer: %d\n", errno);
 		ret = -1;
-		goto error_with_frt_fd;
 	}
 	if (ret == OK) {
 		printf("Test Example Completed Successfully\n");
