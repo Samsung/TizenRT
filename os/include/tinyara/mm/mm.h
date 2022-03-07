041ea71f02972166815f60d56e26185e8c4cfb23
jeongchanKim <jc_.kim@samsung.com>
2022-03-07 14:33:51 +0900

    mm/mm_heap/mm_getheap.c : Add API to get largest free node size
    mm_get_largest_freenode_size() can get the largest free node size based on the passed address heap
    
    Signed-off-by: jeongchanKim <jc_.kim@samsung.com>

--- os/include/tinyara/mm/mm.h
+++ os/include/tinyara/mm/mm.h
@@ -694,6 +694,8 @@ struct mm_heap_s *mm_get_heap_with_index(int index);
 
 int mm_get_heapindex(void *mem);
 
+size_t mm_get_largest_freenode_size(void);
+
 #if defined(CONFIG_APP_BINARY_SEPARATION) && defined(__KERNEL__)
 void mm_initialize_app_heap_q(void);
 void mm_add_app_heap_list(struct mm_heap_s *heap, char *app_name);
