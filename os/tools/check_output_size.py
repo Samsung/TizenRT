11927d8cd0a19944dc9706737ae274fd50651cc7
jeongchanKim <jc_.kim@samsung.com>
2022-03-11 15:21:50 +0900

    os/tools/*.py : Change indentation from tab to 4 spaces
    It is recommended to use Python Coding rule based on PEP 8.
    (PEP 8 : python.org/dev/peps/pep-0008)
    So change the indentations from tab to 4 spaces.
    
    Signed-off-by: jeongchanKim <jc_.kim@samsung.com>

--- os/tools/check_output_size.py
+++ os/tools/check_output_size.py
@@ -34,32 +34,32 @@ output_folder = build_folder + '/output/bin'
 FAIL_TO_BUILD = False
 
 def number_with_comma_align(number):
-	return ("{:10,}".format(number))
+    return ("{:10,}".format(number))
 
 def number_with_comma(number):
-	return ("{:,}".format(number))
+    return ("{:,}".format(number))
 
 def check_binary_size(bin_type, part_size):
-	# Read the binary name from .bininfo
-	bin_name = util.get_binname_from_bininfo(bin_type)
-	if bin_name == 'None' :
-		return
-	output_path = build_folder + '/output/bin/' + bin_name
+    # Read the binary name from .bininfo
+    bin_name = util.get_binname_from_bininfo(bin_type)
+    if bin_name == 'None' :
+        return
+    output_path = build_folder + '/output/bin/' + bin_name
 
-	# Get the partition and binary size
-	BINARY_SIZE=os.path.getsize(output_path)
-	PARTITION_SIZE = part_size
+    # Get the partition and binary size
+    BINARY_SIZE=os.path.getsize(output_path)
+    PARTITION_SIZE = part_size
 
-	# Print each information
-	print(bin_type + "\tBinary Size : " + number_with_comma_align(BINARY_SIZE) + " bytes" + "\tPartition Size : " + number_with_comma_align(PARTITION_SIZE) + " bytes")
+    # Print each information
+    print(bin_type + "\tBinary Size : " + number_with_comma_align(BINARY_SIZE) + " bytes" + "\tPartition Size : " + number_with_comma_align(PARTITION_SIZE) + " bytes")
 
-	# Compare the partition size and its binary size
-	if PARTITION_SIZE < int(BINARY_SIZE) :
-		print("   !!!!!!!! ERROR !!!!!!!")
-		print("   Built " + bin_type + " (" + number_with_comma(BINARY_SIZE) + " bytes) is greater than its partition (" + number_with_comma(PARTITION_SIZE) + " bytes).")
-		print("   " + bin_type + " Binary will be deleted. Need to re-configure the partition using menuconfig and to re-build.")
-		os.remove(output_path)
-		FAIL_TO_BUILD = True
+    # Compare the partition size and its binary size
+    if PARTITION_SIZE < int(BINARY_SIZE) :
+        print("   !!!!!!!! ERROR !!!!!!!")
+        print("   Built " + bin_type + " (" + number_with_comma(BINARY_SIZE) + " bytes) is greater than its partition (" + number_with_comma(PARTITION_SIZE) + " bytes).")
+        print("   " + bin_type + " Binary will be deleted. Need to re-configure the partition using menuconfig and to re-build.")
+        os.remove(output_path)
+        FAIL_TO_BUILD = True
 
 PARTITION_SIZE_LIST = util.get_value_from_file(cfg_file, "CONFIG_FLASH_PART_SIZE=")
 PARTITION_NAME_LIST = util.get_value_from_file(cfg_file, "CONFIG_FLASH_PART_NAME=")
@@ -68,7 +68,7 @@ CONFIG_APP_BINARY_SEPARATION = util.get_value_from_file(cfg_file, "CONFIG_APP_BI
 CONFIG_SUPPORT_COMMON_BINARY = util.get_value_from_file(cfg_file, "CONFIG_SUPPORT_COMMON_BINARY=").rstrip('\n')
 
 if PARTITION_SIZE_LIST == 'None' :
-	sys.exit(0)
+    sys.exit(0)
 
 NAME_LIST = PARTITION_NAME_LIST.replace('"','').split(",")
 SIZE_LIST = PARTITION_SIZE_LIST.replace('"','').split(",")
@@ -82,8 +82,8 @@ APP2_IDX = 0
 
 FLASH_SIZE = 0
 for part in SIZE_LIST :
-	if part.isdigit() == True :
-		FLASH_SIZE += int(part) * 1024
+    if part.isdigit() == True :
+        FLASH_SIZE += int(part) * 1024
 
 KERNEL_PART_TMP_SIZE = FLASH_SIZE
 COMMON_PART_TMP_SIZE = FLASH_SIZE
@@ -91,27 +91,27 @@ APP1_PART_TMP_SIZE = FLASH_SIZE
 APP2_PART_TMP_SIZE = FLASH_SIZE
 
 for name in NAME_LIST :
-	PART_IDX += 1
-	if name == "kernel" or name == "os" :
-		if KERNEL_PART_TMP_SIZE > (int(SIZE_LIST[PART_IDX]) * 1024) :
-			KERNEL_PART_TMP_SIZE = (int(SIZE_LIST[PART_IDX]) * 1024)
-			KERNEL_IDX = PART_IDX
-		continue
-	elif name == "app1" :
-		if APP1_PART_TMP_SIZE > (int(SIZE_LIST[PART_IDX]) * 1024) :
-			APP1_PART_TMP_SIZE = int(SIZE_LIST[PART_IDX]) * 1024
-			APP1_IDX = PART_IDX
-		continue
-	elif name == "app2" :
-		if APP2_PART_TMP_SIZE > (int(SIZE_LIST[PART_IDX]) * 1024) :
-			APP2_PART_TMP_SIZE = int(SIZE_LIST[PART_IDX]) * 1024
-			APP2_IDX = PART_IDX
-		continue
-	elif name == "common" :
-		if COMMON_PART_TMP_SIZE > (int(SIZE_LIST[PART_IDX]) * 1024) :
-			COMMON_PART_TMP_SIZE = int(SIZE_LIST[PART_IDX]) * 1024
-			COMMON_IDX = PART_IDX
-		continue
+    PART_IDX += 1
+    if name == "kernel" or name == "os" :
+        if KERNEL_PART_TMP_SIZE > (int(SIZE_LIST[PART_IDX]) * 1024) :
+            KERNEL_PART_TMP_SIZE = (int(SIZE_LIST[PART_IDX]) * 1024)
+            KERNEL_IDX = PART_IDX
+        continue
+    elif name == "app1" :
+        if APP1_PART_TMP_SIZE > (int(SIZE_LIST[PART_IDX]) * 1024) :
+            APP1_PART_TMP_SIZE = int(SIZE_LIST[PART_IDX]) * 1024
+            APP1_IDX = PART_IDX
+        continue
+    elif name == "app2" :
+        if APP2_PART_TMP_SIZE > (int(SIZE_LIST[PART_IDX]) * 1024) :
+            APP2_PART_TMP_SIZE = int(SIZE_LIST[PART_IDX]) * 1024
+            APP2_IDX = PART_IDX
+        continue
+    elif name == "common" :
+        if COMMON_PART_TMP_SIZE > (int(SIZE_LIST[PART_IDX]) * 1024) :
+            COMMON_PART_TMP_SIZE = int(SIZE_LIST[PART_IDX]) * 1024
+            COMMON_IDX = PART_IDX
+        continue
 
 KERNEL_PARTITION_SIZE = int(SIZE_LIST[KERNEL_IDX]) * 1024
 COMMON_PARTITION_SIZE = int(SIZE_LIST[COMMON_IDX]) * 1024
@@ -122,13 +122,13 @@ APP2_PARTITION_SIZE = int(SIZE_LIST[APP2_IDX]) * 1024
 print("\n========== Size Verification of built Binaries ==========")
 check_binary_size("KERNEL", KERNEL_PARTITION_SIZE)
 if CONFIG_APP_BINARY_SEPARATION == "y" :
-	check_binary_size("APP1", APP1_PARTITION_SIZE)
-	check_binary_size("APP2", APP2_PARTITION_SIZE)
-	if CONFIG_SUPPORT_COMMON_BINARY == "y" :
-		check_binary_size("COMMON", COMMON_PARTITION_SIZE)
+    check_binary_size("APP1", APP1_PARTITION_SIZE)
+    check_binary_size("APP2", APP2_PARTITION_SIZE)
+    if CONFIG_SUPPORT_COMMON_BINARY == "y" :
+        check_binary_size("COMMON", COMMON_PARTITION_SIZE)
 
 if FAIL_TO_BUILD == True :
-	# Stop to build, because there is mismatched size problem.
-	sys.exit(1)
+    # Stop to build, because there is mismatched size problem.
+    sys.exit(1)
 else :
-	print("Size verification is done.")
+    print("Size verification is done.")
--- os/tools/check_package.py
+++ os/tools/check_package.py
@@ -45,98 +45,98 @@ VERIFY_SUCCESS = True
 
 target = sys.argv[1]
 if target == "-h" or target == "--help" :
-	print("Usage :")
-	print("\tpython check_package.py [package_path]")
-	print("\tex) python check_package.py ../../build/output/bin/kernel_XXX_YYMMDD.trpk")
-	sys.exit(0)
+    print("Usage :")
+    print("\tpython check_package.py [package_path]")
+    print("\tex) python check_package.py ../../build/output/bin/kernel_XXX_YYMMDD.trpk")
+    sys.exit(0)
 
 # Read the package header
 with open(target, 'r') as f:
-	data = f.read()
+    data = f.read()
 
 check_signing = struct.unpack('H', data[4:6])
 
 if "kernel_" in target :
-	print("=== Kernel Package Header Information ===")
-	print("< Cannot check the signing >")
-	SIGNING_OFFSET = 0 #For kernel package, signing info is not added at the first. So we will not use the offset for kernel package case.
+    print("=== Kernel Package Header Information ===")
+    print("< Cannot check the signing >")
+    SIGNING_OFFSET = 0 #For kernel package, signing info is not added at the first. So we will not use the offset for kernel package case.
 
-	header_size = struct.unpack('H', data[4:6])
-	package_ver = struct.unpack('I', data[6:10])
-	file_size = struct.unpack('I', data[10:14])
-	secure_size = struct.unpack('H', data[14:16])
+    header_size = struct.unpack('H', data[4:6])
+    package_ver = struct.unpack('I', data[6:10])
+    file_size = struct.unpack('I', data[10:14])
+    secure_size = struct.unpack('H', data[14:16])
 
-	print("\tHeader Size        : " + str(header_size[0]))
-	print("\tPackage Version    : " + str(package_ver[0]))
-	print("\tFile Size          : " + str(file_size[0]))
-	print("\tSecure Header Size : " + str(secure_size[0]))
+    print("\tHeader Size        : " + str(header_size[0]))
+    print("\tPackage Version    : " + str(package_ver[0]))
+    print("\tFile Size          : " + str(file_size[0]))
+    print("\tSecure Header Size : " + str(secure_size[0]))
 
 elif "common_" in target :
-	print("=== Common Package Header Information ===")
+    print("=== Common Package Header Information ===")
 
-	if int(check_signing[0]) == COMMON_HEADER_SIZE :
-		print("< Unsigned Common Package >")
-		SIGNING_OFFSET = 0
-	else :
-		print("< Signed Common Package >")
-		SIGNING_OFFSET = SIGNING_SIZE
+    if int(check_signing[0]) == COMMON_HEADER_SIZE :
+        print("< Unsigned Common Package >")
+        SIGNING_OFFSET = 0
+    else :
+        print("< Signed Common Package >")
+        SIGNING_OFFSET = SIGNING_SIZE
 
-	header_size = struct.unpack('H', data[4 + SIGNING_OFFSET:6 + SIGNING_OFFSET])
-	package_ver = struct.unpack('I', data[6 + SIGNING_OFFSET:10 + SIGNING_OFFSET])
-	file_size = struct.unpack('I', data[10 + SIGNING_OFFSET:14 + SIGNING_OFFSET])
+    header_size = struct.unpack('H', data[4 + SIGNING_OFFSET:6 + SIGNING_OFFSET])
+    package_ver = struct.unpack('I', data[6 + SIGNING_OFFSET:10 + SIGNING_OFFSET])
+    file_size = struct.unpack('I', data[10 + SIGNING_OFFSET:14 + SIGNING_OFFSET])
 
-	print("\tHeader Size     : " + str(header_size[0]))
-	print("\tPackage Version : " + str(package_ver[0]))
-	print("\tFile Size       : " + str(file_size[0]))
+    print("\tHeader Size     : " + str(header_size[0]))
+    print("\tPackage Version : " + str(package_ver[0]))
+    print("\tFile Size       : " + str(file_size[0]))
 
 elif "app_" in target :
-	print("=== App Package Header Information ===")
+    print("=== App Package Header Information ===")
 
-	if int(check_signing[0]) == APP_HEADER_SIZE :
-		print("< Unsigned App Package >")
-		SIGNING_OFFSET = 0
-	else :
-		print("< Signed App Package >")
-		SIGNING_OFFSET = SIGNING_SIZE
+    if int(check_signing[0]) == APP_HEADER_SIZE :
+        print("< Unsigned App Package >")
+        SIGNING_OFFSET = 0
+    else :
+        print("< Signed App Package >")
+        SIGNING_OFFSET = SIGNING_SIZE
 
-	header_size = struct.unpack('H', data[4 + SIGNING_OFFSET:6 + SIGNING_OFFSET])
-	package_type = struct.unpack('B', data[6 + SIGNING_OFFSET:7 + SIGNING_OFFSET])
-	main_prio = struct.unpack('B', data[7 + SIGNING_OFFSET:8 + SIGNING_OFFSET])
-	loading_prio = struct.unpack('B', data[8 + SIGNING_OFFSET:9 + SIGNING_OFFSET])
-	file_size = struct.unpack('I', data[9 + SIGNING_OFFSET:13 + SIGNING_OFFSET])
-	package_name = struct.unpack('ccc', data[13 + SIGNING_OFFSET:16 + SIGNING_OFFSET])
-	package_ver = struct.unpack('I', data[29 + SIGNING_OFFSET:33 + SIGNING_OFFSET])
-	ram_size = struct.unpack('I', data[33 + SIGNING_OFFSET:37 + SIGNING_OFFSET])
-	stk_size = struct.unpack('I', data[37 + SIGNING_OFFSET:41 + SIGNING_OFFSET])
-	kernel_ver = struct.unpack('I', data[41 + SIGNING_OFFSET:45 + SIGNING_OFFSET])
+    header_size = struct.unpack('H', data[4 + SIGNING_OFFSET:6 + SIGNING_OFFSET])
+    package_type = struct.unpack('B', data[6 + SIGNING_OFFSET:7 + SIGNING_OFFSET])
+    main_prio = struct.unpack('B', data[7 + SIGNING_OFFSET:8 + SIGNING_OFFSET])
+    loading_prio = struct.unpack('B', data[8 + SIGNING_OFFSET:9 + SIGNING_OFFSET])
+    file_size = struct.unpack('I', data[9 + SIGNING_OFFSET:13 + SIGNING_OFFSET])
+    package_name = struct.unpack('ccc', data[13 + SIGNING_OFFSET:16 + SIGNING_OFFSET])
+    package_ver = struct.unpack('I', data[29 + SIGNING_OFFSET:33 + SIGNING_OFFSET])
+    ram_size = struct.unpack('I', data[33 + SIGNING_OFFSET:37 + SIGNING_OFFSET])
+    stk_size = struct.unpack('I', data[37 + SIGNING_OFFSET:41 + SIGNING_OFFSET])
+    kernel_ver = struct.unpack('I', data[41 + SIGNING_OFFSET:45 + SIGNING_OFFSET])
 
-	print("\tHeader Size       : " + str(header_size[0]))
-	if package_type[0] == PACKAGE_TYPE_ELF :
-		print("\tPackage Type      : ELF(%d)" %package_type[0])
-	else :
-		print("\tPackage Type Invalid : %d" %package_type[0])
-		VERIFY_SUCCESS = False
-	print("\tMain Priority     : " + str(main_prio[0]))
-	if loading_prio[0] == LOADING_LOW :
-		print("\tLoading Priority  : LOADING_LOW(%d)" %LOADING_LOW)
-	elif loading_prio[0] == LOADING_MID :
-		print("\tLoading Priority  : LOADING_MID(%d)" %LOADING_MID)
-	elif loading_prio[0] == LOADING_HIGH :
-		print("\tLoading Priority  : LOADING_HIGH(%d)" %LOADING_HIGH)
-	else :
-		print("\tLoading Priority : Invalid(%d)" %loading_prio[0])
-		VERIFY_SUCCESS = False
-	print("\tFile Size         : " + str(file_size[0]))
-	print("\tPackage Name      : " + str(package_name[0]) + str(package_name[1]) + str(package_name[2]))
-	print("\tPackage Version   : " + str(package_ver[0]))
-	print("\tRAM Size          : " + str(ram_size[0]))
-	print("\tStack Size        : " + str(stk_size[0]))
-	print("\tKernel Version    : " + str(kernel_ver[0]))
+    print("\tHeader Size       : " + str(header_size[0]))
+    if package_type[0] == PACKAGE_TYPE_ELF :
+        print("\tPackage Type      : ELF(%d)" %package_type[0])
+    else :
+        print("\tPackage Type Invalid : %d" %package_type[0])
+        VERIFY_SUCCESS = False
+    print("\tMain Priority     : " + str(main_prio[0]))
+    if loading_prio[0] == LOADING_LOW :
+        print("\tLoading Priority  : LOADING_LOW(%d)" %LOADING_LOW)
+    elif loading_prio[0] == LOADING_MID :
+        print("\tLoading Priority  : LOADING_MID(%d)" %LOADING_MID)
+    elif loading_prio[0] == LOADING_HIGH :
+        print("\tLoading Priority  : LOADING_HIGH(%d)" %LOADING_HIGH)
+    else :
+        print("\tLoading Priority : Invalid(%d)" %loading_prio[0])
+        VERIFY_SUCCESS = False
+    print("\tFile Size         : " + str(file_size[0]))
+    print("\tPackage Name      : " + str(package_name[0]) + str(package_name[1]) + str(package_name[2]))
+    print("\tPackage Version   : " + str(package_ver[0]))
+    print("\tRAM Size          : " + str(ram_size[0]))
+    print("\tStack Size        : " + str(stk_size[0]))
+    print("\tKernel Version    : " + str(kernel_ver[0]))
 
 else :
-	print("!!!Not Supported Package. Please Check the package!!!")
-	VERIFY_SUCCESS = False
-	sys.exit(1)
+    print("!!!Not Supported Package. Please Check the package!!!")
+    VERIFY_SUCCESS = False
+    sys.exit(1)
 
 # Verify the package by calculating the crc and comparing it with the value of the header.
 
@@ -150,21 +150,21 @@ checksum_calc = zlib.crc32(data[calc_start_idx:calc_end_idx]) & 0xFFFFFFFF
 checksum_header = struct.unpack('I', data[0 + SIGNING_OFFSET:4 + SIGNING_OFFSET])[0]
 
 if checksum_header == checksum_calc :
-	print("\t* Checksum verification Done.")
+    print("\t* Checksum verification Done.")
 else :
-	print("\t* Checksum is invalid, header : " + str(checksum_header) + ", calc : " + str(checksum_calc));
-	VERIFY_SUCCESS = False
+    print("\t* Checksum is invalid, header : " + str(checksum_header) + ", calc : " + str(checksum_calc));
+    VERIFY_SUCCESS = False
 
 
 # Verify the package version between header and the file name
 file_name_ver = os.path.basename(target).split("_")[2].split(".")[0]
 
 if package_ver[0] == int(file_name_ver) :
-	print("\t* Version Matched between filename and header.")
+    print("\t* Version Matched between filename and header.")
 else :
-	print("\t* Version NOT MATCHED!!! between filename(" + file_name_ver + ") and header(" + str(package_ver[0]) + ")")
-	VERIFY_SUCCESS = False
+    print("\t* Version NOT MATCHED!!! between filename(" + file_name_ver + ") and header(" + str(package_ver[0]) + ")")
+    VERIFY_SUCCESS = False
 
 # Return exit(1) if there is a validation failure.
 if VERIFY_SUCCESS == False :
-	sys.exit(1)
+    sys.exit(1)
