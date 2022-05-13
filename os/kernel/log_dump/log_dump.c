ee78e65cadf73aecf580627fa8ac751194265401
sangamanatha <sangam.swami@samsung.com>
2022-05-13 00:45:16 -0400

    kernel/log_dump : Add compression for log_dump
    Support debug log compression with miniz/lzma library.
    
    1. Format of the data read by application contains:
    |<- 4 byte length ->|<- compressed data ->|<- 4 byte length ->|<- compressed data ->|...
    
    "4 byte length" contains the size of the "compressed data".
    
    2. After reading 4 byte length field, app get the length of compressed data chunk to be uncompressed.
    
    3. User/app should use lzma library to uncompress the compressed log data.
    -----------------------
    Ex 1(MINIZ) :
        CONFIG_LOG_DUMP_CHUNK_SIZE -> set to 4Kby default
    
    	int writesize = CONFIG_LOG_DUMP_CHUNK_SIZE;
        int size = writesize;
        unsigned char *out_buf = (unsigned char *)malloc(CONFIG_LOG_DUMP_CHUNK_SIZE);
    
    	ret = mz_uncompress(out_buffer, writesize, &commpressed_data[0], *size);
    
        The uncompressed data will be at out_buf with writesize lenth.
    
    Ex 2(LZMA) :
        CONFIG_LOG_DUMP_CHUNK_SIZE -> set to 4Kby default
        #define LZMA_PROPS_SIZE 5  -> set in LzmaLib.h
    
        int writesize = CONFIG_LOG_DUMP_CHUNK_SIZE;
        int size = writesize - (LZMA_PROPS_SIZE);
        unsigned char *out_buf = (unsigned char *)malloc(CONFIG_LOG_DUMP_CHUNK_SIZE);
    
            ret = LzmaUncompress(&out_buf[0], &writesize, &compressed_data[LZMA_PROPS_SIZE],     &size, &compressed_data[0], LZMA_PROPS_SIZE);
    
        The uncompressed data will be at out_buf with writesize lenth.
    -----------------------
    
    Signed-off-by: sangamanatha <sangam.swami@samsung.com>
    Co-authored-by: Abhishek Akkabathula <a.akkabathul@samsung.com>

--- os/kernel/log_dump/log_dump.c
+++ os/kernel/log_dump/log_dump.c
@@ -24,6 +24,7 @@
 #include <tinyara/mm/mm.h>
 #include <tinyara/sched.h>
 
+#include <mqueue.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -34,6 +35,12 @@
 
 #include <tinyara/log_dump/log_dump.h>
 #include <tinyara/log_dump/log_dump_internal.h>
+#if defined(CONFIG_LIB_MINIZ)
+#include "miniz/miniz.h"
+#endif
+#if defined(CONFIG_LIB_LZMA)
+#include <tinyara/lzma/LzmaLib.h>
+#endif
 
 /****************************************************************************
  * Pre-processor Definitions
@@ -47,14 +54,13 @@
 #define MAX(a, b)		   (((a) > (b)) ? (a) : (b))
 #endif
 
-#define LOG_DUMP_OK			 0
-#define LOG_DUMP_OK			0
-#define LOG_DUMP_OK_FSZ		1
+#define LOG_DUMP_OK			0	/* Used for successful return check */
+#define LOG_DUMP_OK_HEAPCHK		1	/* To check heap available after success return */
 #define LOG_DUMP_MEM_FAIL		-1
-#define LOG_DUMP_HEAP_FAIL		-2
-#define LOG_DUMP_SIZE_FAIL		-3
-#define LOG_DUMP_OPT_FAIL		-4
-#define LOG_DUMP_STR_FREEMEM	14
+#define LOG_DUMP_COMPHEAD_OFFSET	-1
+#define LOG_DUMP_OPT_FAIL		-2
+#define LOG_DUMP_STR_FREEHEAP		15	/* String size to display Free heap size */
+#define LOG_DUMP_COMPRESS_NODESZ	5	/* use 4 char to store compressed node size */
 
 #define LOG_CHUNK_SIZE			sizeof(struct log_dump_chunk_s)
 
@@ -66,7 +72,8 @@ static bool is_started_to_save;
 
 struct log_dump_chunk_s {
 	struct log_dump_chunk_s *flink;
-	char arr[CONFIG_LOG_DUMP_CHUNK_SIZE];
+	char arr[CONFIG_LOG_DUMP_CHUNK_SIZE];	/* to store the compressed log */
+	int comp_head;				/* starting index of the 1st compressed block in the chunk */
 };
 
 /****************************************************************************
@@ -82,6 +89,18 @@ static int curbytes = 0;		/* number of bytes in the current chunk that were fill
 static int current_ptr = 0;		/* current pointer for read */
 static size_t log_dump_size = 0;
 static struct log_dump_chunk_s *read_node;
+static signed char *uncomp_array;
+static int uncomp_curbytes = 0;
+static char comp_size[LOG_DUMP_COMPRESS_NODESZ];
+#if defined(CONFIG_LIB_LZMA)
+static unsigned int propsSize = LZMA_PROPS_SIZE;
+#endif
+static bool set_comp_head = false;
+static mqd_t g_logdump_mq_fd;
+static size_t writesize = CONFIG_LOG_DUMP_CHUNK_SIZE;
+static unsigned char *out_buf;
+static bool current_compression;
+static int compress_ret;
 
 /****************************************************************************
  * Private Functions
@@ -91,6 +110,11 @@ static struct log_dump_chunk_s *read_node;
  * Public Functions
  ****************************************************************************/
 
+mqd_t log_dump_get_mqfd(void)
+{
+	return g_logdump_mq_fd;
+}
+
 int log_dump_init(void)
 {
 	sq_init(&log_dump_chunks);
@@ -98,8 +122,16 @@ int log_dump_init(void)
 	struct log_dump_chunk_s *node = (struct log_dump_chunk_s *)kmm_malloc(LOG_CHUNK_SIZE);
 
 	if (node == NULL) {
+		lldbg("memory allocation failure\n");
 		return LOG_DUMP_MEM_FAIL;
 	}
+	uncomp_array = (signed char *)kmm_malloc(CONFIG_LOG_DUMP_CHUNK_SIZE);
+	if (uncomp_array == NULL) {
+		lldbg("memory allocation failure\n");
+		kmm_free(node);
+		return LOG_DUMP_MEM_FAIL;
+	}
+	node->comp_head = 0;		/* since first node */
 
 	curbytes = 0;
 	current_ptr = 0;
@@ -112,10 +144,75 @@ int log_dump_init(void)
 	return LOG_DUMP_OK;
 }
 
-void log_dump_read_init(void)
+unsigned char *last_comp_block;
+size_t last_comp_block_size;
+int last_comp_block_ptr;
+bool last_block_compression;
+
+int log_dump_read_wake(void)
+{
+	last_block_compression = true;
+	struct log_dump_msg_s msg;
+	int ret;
+
+	msg.comp = true;
+	msg.ch = 'c';
+	ret = mq_send(log_dump_get_mqfd(), (const char *)&msg, sizeof(struct log_dump_msg_s), 100);
+	if (ret < 0) {
+		lldbg("failed mq_send ret = %d\n", ret);
+		return ERROR;
+	}
+
+	while (last_block_compression) ;
+	return 0;
+}
+
+int log_dump_read_init(void)
 {
 	read_node = (struct log_dump_chunk_s *)sq_peek(&log_dump_chunks);	/* reset the read node to head */
-	current_ptr = 0;							/* reset the read pointer */
+
+	if (read_node == NULL) {
+		lldbg("empty read node\n");
+		return LOG_DUMP_MEM_FAIL;
+	}
+
+	if (read_node->comp_head < 0) {
+		current_ptr = curbytes;
+	} else {
+		current_ptr = read_node->comp_head;
+	}
+
+	last_comp_block_size = CONFIG_LOG_DUMP_CHUNK_SIZE;
+
+#if defined(CONFIG_LIB_LZMA)
+	last_comp_block = (unsigned char *)kmm_malloc(4 + CONFIG_LOG_DUMP_CHUNK_SIZE + LZMA_PROPS_SIZE);
+#elif defined(CONFIG_LIB_MINIZ)
+	last_comp_block = (unsigned char *)kmm_malloc(4 + CONFIG_LOG_DUMP_CHUNK_SIZE);
+#endif
+	if (last_comp_block == NULL) {
+		lldbg("memory allocation failure\n");
+		return LOG_DUMP_MEM_FAIL;
+	}
+
+#if defined(CONFIG_LIB_LZMA)
+	int ret = LzmaCompress(&last_comp_block[4 + LZMA_PROPS_SIZE], &last_comp_block_size, uncomp_array, uncomp_curbytes, &last_comp_block[4], &propsSize, 0, 1 << 13, -1, -1, -1, -1, 1);
+#elif defined(CONFIG_LIB_MINIZ)
+	int ret = mz_compress(&last_comp_block[4], &last_comp_block_size, uncomp_array, uncomp_curbytes);
+#endif
+	if (ret != Z_OK) {
+		lldbg("Fail to compress ret = %d\n", ret);
+		kmm_free(last_comp_block);
+		return -ret;
+	}
+
+	snprintf(comp_size, LOG_DUMP_COMPRESS_NODESZ, "%04d", last_comp_block_size);
+	for (int i = 0; i < LOG_DUMP_COMPRESS_NODESZ - 1; i++) {
+		last_comp_block[i] = comp_size[i];
+	}
+
+	last_comp_block_size += 4;
+	last_comp_block_ptr = 0;
+	return 0;
 }
 
 static void log_dump_mem_check(size_t max_size)
@@ -125,10 +222,10 @@ static void log_dump_mem_check(size_t max_size)
 		size_t extra_chunk_size = log_dump_size - max_size;
 		int extra_chunks_count = extra_chunk_size / LOG_CHUNK_SIZE;
 		struct log_dump_chunk_s *next_chunk;
-		
+
 		for (int i = 0; i < extra_chunks_count; i++) {
 			next_chunk = (struct log_dump_chunk_s *)sq_remfirst(&log_dump_chunks);
-			current_ptr = 0; /* reset the read pointer as the head is removed */
+			current_ptr = 0;	/* reset the read pointer as the head is removed */
 			kmm_free(next_chunk);
 		}
 	}
@@ -148,7 +245,7 @@ int log_dump_set(FAR const char *buffer, size_t buflen)
 	return LOG_DUMP_OK;
 }
 
-int log_dump_tobuffer(char ch, size_t *free_size)
+static int log_dump_tobuffer(char ch, size_t *free_size)
 {
 	/* need to check if the current chunks size is over max_log_size or greater than x% of free heap */
 
@@ -166,16 +263,15 @@ int log_dump_tobuffer(char ch, size_t *free_size)
 #endif
 	size_t percent_free_size = (*free_size * CONFIG_LOG_DUMP_MAX_FREE_HEAP) / 100;
 
-	if(percent_free_size < CONFIG_LOG_DUMP_MAX_SIZE) {
-		max_size = LOG_CHUNK_SIZE;
+	if (percent_free_size < CONFIG_LOG_DUMP_MAX_SIZE) {
+		max_size = LOG_CHUNK_SIZE;	/* setting the lower limit */
 	} else {
-		max_size = MIN(percent_free_size, CONFIG_LOG_DUMP_MAX_SIZE);			/* setting the upper limit */
-		max_size = MAX(max_size, CONFIG_LOG_DUMP_MIN_SIZE);						/* checking the lower limit */
+		max_size = CONFIG_LOG_DUMP_MAX_SIZE;	/* setting the upper limit */
 	}
 
-	if (curbytes == CONFIG_LOG_DUMP_CHUNK_SIZE) { /* last chunk is full */
+	if (curbytes == CONFIG_LOG_DUMP_CHUNK_SIZE) {	/* last chunk is full */
 
-		if (log_dump_size + LOG_CHUNK_SIZE < max_size) {	
+		if (log_dump_size + LOG_CHUNK_SIZE < max_size) {
 			/* memory available, so fetch a new buffer */
 			struct log_dump_chunk_s *node = (struct log_dump_chunk_s *)kmm_malloc(LOG_CHUNK_SIZE);
 
@@ -185,26 +281,46 @@ int log_dump_tobuffer(char ch, size_t *free_size)
 
 			curbytes = 1;
 			node->arr[0] = ch;
-			sq_addlast((sq_entry_t *)node, &log_dump_chunks);
+			sq_addlast((sq_entry_t *) node, &log_dump_chunks);
+			if (set_comp_head) {	/* new node, so compressed block starts at 0 */
+				node->comp_head = 0;
+				set_comp_head = false;
+			} else {
+				node->comp_head = LOG_DUMP_COMPHEAD_OFFSET;
+				set_comp_head = false;
+			}
 			log_dump_size += LOG_CHUNK_SIZE;
 
 		} else {
 			/* logs reached memory limit, reuse the head and free extra logs */
 			struct log_dump_chunk_s *node = (struct log_dump_chunk_s *)sq_remfirst(&log_dump_chunks);
 			curbytes = 1;
-			current_ptr = 0; /* reset the read pointer as head is modified */
+			current_ptr = 0;	/* reset the read pointer as head is modified */
 			node->arr[0] = ch;
-			sq_addlast((sq_entry_t *)node, &log_dump_chunks);
+			sq_addlast((sq_entry_t *) node, &log_dump_chunks);
+			if (set_comp_head) {	/* new node, so compressed block starts at 0 */
+				node->comp_head = 0;
+				set_comp_head = false;
+			} else {
+				node->comp_head = LOG_DUMP_COMPHEAD_OFFSET;
+				set_comp_head = false;
+			}
 			log_dump_mem_check(max_size);
 		}
 
-	} else { /* there is place in tail */
+	} else {					/* there is place in tail */
 		struct log_dump_chunk_s *log_dump_tail = (struct log_dump_chunk_s *)sq_tail(&log_dump_chunks);
 		log_dump_tail->arr[curbytes] = ch;
 		curbytes += 1;
+		if (set_comp_head) {	/* new node, so compressed block starts at 0 */
+			if (log_dump_tail->comp_head < 0) {	/* compressed node not present */
+				log_dump_tail->comp_head = curbytes - 1;
+			}
+			set_comp_head = false;
+		}
 		if (curbytes % CONFIG_LOG_DUMP_MEMCHECK_SIZE == 0) {
 			log_dump_mem_check(max_size);
-			return LOG_DUMP_OK_FSZ;
+			return LOG_DUMP_OK_HEAPCHK;
 		}
 	}
 
@@ -219,17 +335,64 @@ int log_dump_save(char ch)
 
 	size_t free_size = 0;
 	int ret = 0;
-	char fsz_array[LOG_DUMP_STR_FREEMEM];
+	uncomp_array[uncomp_curbytes] = ch;
+	uncomp_curbytes++;
+	if (uncomp_curbytes == CONFIG_LOG_DUMP_CHUNK_SIZE) {
+		/* compress the block, add it to the nodes, reset the uncomp_array */
+		set_comp_head = true;
+#if defined(CONFIG_LIB_LZMA)
+		out_buf = (unsigned char *)kmm_malloc(CONFIG_LOG_DUMP_CHUNK_SIZE + LZMA_PROPS_SIZE);
+#elif defined(CONFIG_LIB_MINIZ)
+		out_buf = (unsigned char *)kmm_malloc(CONFIG_LOG_DUMP_CHUNK_SIZE);
+#endif
+		if (out_buf == NULL) {
+			lldbg("memory allocation failure\n");
+			return LOG_DUMP_MEM_FAIL;
+		}
 
-	ret = log_dump_tobuffer(ch, &free_size);
-	if (ret == LOG_DUMP_OK_FSZ)
-	{
-		snprintf(fsz_array, sizeof(size_t), "<fsz:%d>", free_size);
-		for(int i = 0; i < sizeof(fsz_array); i++)
-		{
-			ret = log_dump_tobuffer(fsz_array[i], &free_size);
-			if(ret < 0)
+		current_compression = true;
+
+		struct log_dump_msg_s msg;
+		msg.comp = false;
+		msg.ch = 'c';
+		mq_send(log_dump_get_mqfd(), (const char *)&msg, sizeof(struct log_dump_msg_s), 100);
+
+		while (current_compression) ;
+
+		if (compress_ret != Z_OK) {
+			lldbg("Fail to compress ret = %d\n", compress_ret);
+			kmm_free(out_buf);
+			return -compress_ret;
+		}
+		uncomp_curbytes = 0;	/* reset */
+		snprintf(comp_size, LOG_DUMP_COMPRESS_NODESZ, "%04d", writesize);
+		for (int i = 0; i < LOG_DUMP_COMPRESS_NODESZ - 1; i++) {
+			ret = log_dump_tobuffer(comp_size[i], &free_size);
+			if (ret < 0) {
+				kmm_free(out_buf);
 				return ret;
+			}
+		}
+
+		for (int i = 0; i < writesize; i++) {
+			ret = log_dump_tobuffer(out_buf[i], &free_size);
+			if (ret < 0) {
+				kmm_free(out_buf);
+				return ret;
+			}
+		}
+		kmm_free(out_buf);
+		ret = LOG_DUMP_OK_HEAPCHK;
+
+	} else {
+		ret = LOG_DUMP_OK;
+	}
+
+	if (ret == LOG_DUMP_OK_HEAPCHK) {
+		char fsz_array[LOG_DUMP_STR_FREEHEAP];	/* For Free heap memory string */
+		snprintf(fsz_array, sizeof(size_t), "<fsz:%08d>", free_size);
+		for (int i = 0; i < sizeof(fsz_array); i++) {
+			uncomp_array[uncomp_curbytes++] = fsz_array[i];
 		}
 	}
 	return ret;
@@ -239,23 +402,29 @@ size_t log_dump_read(FAR char *buffer, size_t buflen)
 {
 	size_t ret = 0;
 
-	sched_lock(); /* to ensure that the read is not disturbed by add_char */
+	sched_lock();	/* to ensure that the read is not disturbed by add_char */
 
 	while (ret < buflen) {
-		if (read_node == (struct log_dump_chunk_s *)sq_tail(&log_dump_chunks)) { /* last chunk */
-			if (current_ptr == curbytes) { /* all data has been read */
-				break;
+		if (read_node == (struct log_dump_chunk_s *)sq_tail(&log_dump_chunks)) {	/* last chunk */
+			if (current_ptr == curbytes) {	/* all data has been read */
+				if (last_comp_block_ptr < last_comp_block_size) {
+					buffer[ret] = last_comp_block[last_comp_block_ptr];
+					ret++;
+					last_comp_block_ptr++;
+				} else {
+					break;
+				}
 			} else {
 				buffer[ret] = read_node->arr[current_ptr];
 				current_ptr++;
 				ret++;
 			}
-		} else { /* reading a filled chunk */
+		} else {	/* reading a filled chunk */
 
 			buffer[ret] = read_node->arr[current_ptr];
 			current_ptr++;
 			ret++;
-			if (current_ptr == CONFIG_LOG_DUMP_CHUNK_SIZE) { /* completed reading a chunk */
+			if (current_ptr == CONFIG_LOG_DUMP_CHUNK_SIZE) {	/* completed reading a chunk */
 				current_ptr = 0;
 				read_node = (struct log_dump_chunk_s *)sq_next((sq_entry_t *)read_node);
 			}
@@ -265,4 +434,43 @@ size_t log_dump_read(FAR char *buffer, size_t buflen)
 	sched_unlock();
 
 	return ret;
+}
+
+int log_dump(int argc, char *argv[])
+{
+	struct mq_attr attr;
+
+	attr.mq_maxmsg = 32;
+	attr.mq_msgsize = sizeof(struct log_dump_msg_s);
+	attr.mq_flags = 0;
+
+	/* Create log dump message queue */
+	g_logdump_mq_fd = mq_open("logdump_s", O_RDWR | O_CREAT, 0666, &attr);
+	if (g_logdump_mq_fd == (mqd_t) ERROR) {
+		lldbg("Fail to open mq, errno %d. EXIT!\n", errno);
+		return 0;
+	}
+
+	log_dump_init();
+
+	struct log_dump_msg_s msg;
+	int nbytes;
+
+	while (true) {
+		nbytes = mq_receive(g_logdump_mq_fd, (char *)&msg, sizeof(struct log_dump_msg_s), NULL);
+		if (nbytes <= 0) {
+			continue;
+		}
+		if (msg.comp) {
+			log_dump_read_init();
+			last_block_compression = false;
+		} else {
+#if defined(CONFIG_LIB_LZMA)
+			compress_ret = LzmaCompress(&out_buf[LZMA_PROPS_SIZE], &writesize, uncomp_array, CONFIG_LOG_DUMP_CHUNK_SIZE, out_buf, &propsSize, 0, 1 << 13, -1, -1, -1, -1, 1);
+#elif defined(CONFIG_LIB_MINIZ)
+			compress_ret = mz_compress(out_buf, &writesize, uncomp_array, CONFIG_LOG_DUMP_CHUNK_SIZE);
+#endif
+			current_compression = false;
+		}
+	}
 }
