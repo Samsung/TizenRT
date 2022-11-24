5abe95edff52408609cc7041af1ae0e6ac4abed0
vboxuserroot <s.nayak@samsung.com>
2022-11-24 16:39:13 +0530

    os/include/debug.h: Change the conditionals for mfdbg from dbg to lldbg
    This patch is for change the mfdbg to use lldbg instead of dbg in flat build
    The logs get stuck when there is heap currption
    Change the mfdbg to use lldbg, when in abort mode or in interrupt context.
    With the change the logs are getting displayed correctly.
    
    WITH DBG -
    mm_check_heap_corruption: ################################################
    mm_check_heap_corruption: ERROR: Heap n ( logs are missing here)
    
    WITH LLDBG -
    mm_check_heap_corruption: ################################################
    mm_check_heap_corruption: ERROR: Heap node corruption detected.
    mm_check_heap_corruption: ================================================
    mm_check_heap_corruption: Possible corruption scenario 1:
    mm_check_heap_corruption: ...........

--- os/include/debug.h
+++ os/include/debug.h
@@ -499,8 +499,11 @@ int get_errno(void);
 #define mllvdbg(...)
 #endif
 
-#if defined(CONFIG_MM_ASSERT_ON_FAIL) && defined(CONFIG_APP_BINARY_SEPARATION) && defined (__KERNEL__)
-#define mfdbg(format, ...) lldbg(format, ##__VA_ARGS__)
+#if defined(CONFIG_BUILD_FLAT) || defined(__KERNEL__)
+#define mfdbg(format, ...) if (abort_mode || up_interrupt_context()) \
+                                                             lldbg(format, ##__VA_ARGS__); \
+                                                        else \
+                                                             dbg(format, ##__VA_ARGS__);
 #else
 #define mfdbg(format, ...) dbg(format, ##__VA_ARGS__)
 #endif
