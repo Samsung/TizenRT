efd34ca315ceafa19d475ab97a2614b38093b62f
Vibhor Mahajan <vibhor.m@samsung.com>
2023-02-14 12:40:04 +0530

    apps/examples/hello_tfmicro: Add Tensor Flow sample application.

--- apps/examples/hello_tfmicro/constants.h
+++ apps/examples/hello_tfmicro/constants.h
@@ -31,40 +31,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef TENSORFLOW_LITE_MICRO_KERNELS_ACTIVATION_UTILS_H_
-#define TENSORFLOW_LITE_MICRO_KERNELS_ACTIVATION_UTILS_H_
+#ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_CONSTANTS_H_
+#define TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_CONSTANTS_H_
 
-#include <math.h>
-#include <cmath>
+/* This constant represents the range of x values our model was trained on, which is from 0 to (2 * Pi). We approximate Pi to avoid requiring additional libraries. */
+const float kXrange = 2.f * 3.14159265359f;
 
-#include "tensorflow/lite/c/builtin_op_data.h"
+/* This constant determines the number of inferences to perform across the range of x values defined above. Since each inference takes time, the higher this number, the more time it will take to run through the entire range. The value of this constant can be tuned so that one full cycle takes a desired amount of time. Since different devices take different amounts of time to perform inference, this value should be defined per-device. */
+extern const int kInferencesPerCycle;
 
-namespace tflite {
-namespace ops {
-namespace micro {
+#endif  /* TENSORFLOW_LITE_MICRO_EXAMPLES_HELLO_WORLD_CONSTANTS_H_ */
 
-// Returns the floating point value for a fused activation:
-inline float ActivationValFloat(TfLiteFusedActivation act, float a) {
-  switch (act) {
-    case kTfLiteActNone:
-      return a;
-    case kTfLiteActRelu:
-      return fmax(0.0f, a);
-    case kTfLiteActRelu1:
-      return fmax(-1.0f, fmin(a, 1.0f));
-    case kTfLiteActRelu6:
-      return fmax(0.0f, fmin(a, 6.0f));
-    case kTfLiteActTanh:
-      return tanh(a);
-    case kTfLiteActSignBit:
-      return signbit(a);
-    case kTfLiteActSigmoid:
-      return 1.0f / (1.0f + std::exp(-a));
-  }
-}
-
-}  // namespace micro
-}  // namespace ops
-}  // namespace tflite
-
-#endif  // TENSORFLOW_LITE_MICRO_KERNELS_ACTIVATION_UTILS_H_
