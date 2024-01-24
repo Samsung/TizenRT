95d97af0673095403ba1add985e1d0fd1caa7221
ziyik <https://ziyik@github.com>
2024-01-24 15:50:11 +0800

    os/board/rtl8730e: Disable BT Classic in BT GAP lib

--- os/board/rtl8730e/src/libs/version.h
+++ os/board/rtl8730e/src/libs/version.h
@@ -1,12 +1,12 @@
 #define VERSION_MAJOR            0
 #define VERSION_MINOR            0
 #define VERSION_REVISION         0
-#define VERSION_BUILDNUM         798
-#define VERSION_GCID             0xf72136c3
-#define VERSION_GCIDH            0x093f720a
-#define VERSION_PGCID            0x37b3aab9
-#define VERSION_PGCIDH           0xb773e46f
-#define LIB_NAME                 "ambsmt_ca32"
+#define VERSION_BUILDNUM         845
+#define VERSION_GCID             0xceba1b9b
+#define VERSION_GCIDH            0x164433b8
+#define VERSION_PGCID            0x45133a36
+#define VERSION_PGCIDH           0xf0a75ba7
+#define LIB_NAME                 "ambsmart_le"
 #define CUSTOMER_NAME            lib
 #define CN_1                     'l'
 #define CN_2                     'i'
@@ -16,14 +16,14 @@
 #define CN_6                     '#'
 #define CN_7                     '#'
 #define CN_8                     '#'
-#define BUILDING_TIME            "Thu Oct 19 13:59:35 2023"
+#define BUILDING_TIME            "Tue Jan 23 13:59:23 2024"
 #define NAME2STR(a)              #a
 #define CUSTOMER_NAME_S          #NAME2STR(CUSTOMER_NAME)
 #define NUM4STR(a,b,c,d)         #a "." #b "." #c "." #d
 #define VERSIONBUILDSTR(a,b,c,d) NUM4STR(a,b,c,d)
 #define VERSION_BUILD_STR        VERSIONBUILDSTR(VERSION_MAJOR,VERSION_MINOR,VERSION_REVISION,VERSION_BUILD)
-#define COMMIT                   f72136c3093f
-#define BUILDING_TIME_STR        Thu_2023_10_19_13_59_35
+#define COMMIT                   ceba1b9b1644
+#define BUILDING_TIME_STR        Tue_2024_01_23_13_59_23
 #define BUILDER                  server
 #define BUILDER_STR              server
 #define TO_STR(R) NAME2STR(R)
