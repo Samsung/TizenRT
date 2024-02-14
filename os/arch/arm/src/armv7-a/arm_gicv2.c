3b2f5a62957a08ff3d740fcd5bb4c8d4d82ce432
edwakuwaku <edwardong@realtek-sg.com>
2024-02-14 18:00:45 +0800

    os/arch/arm/src/armv7-a: Refine arch code for cortex-A bootup flow - Added customization for CPU gating when undergoing flash operation

--- os/arch/arm/src/armv7-a/arm_gicv2.c
+++ os/arch/arm/src/armv7-a/arm_gicv2.c
@@ -126,6 +126,9 @@ void arm_gic0_initialize(void)
 
   DEBUGVERIFY(irq_attach(GIC_IRQ_SGI1, arm_start_handler, NULL));
   DEBUGVERIFY(irq_attach(GIC_IRQ_SGI2, arm_pause_handler, NULL));
+#ifdef CONFIG_CPU_GATING
+  DEBUGVERIFY(irq_attach(GIC_IRQ_SGI3, arm_flash_handler, NULL));
+#endif
 #endif
 
   arm_gic_dump("Exit arm_gic0_initialize", true, 0);
