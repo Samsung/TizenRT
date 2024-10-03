06219546e258007df1107df778e82593bb0226ab
Vibhor Mahajan <vibhor.m@samsung.com>
2024-10-03 10:17:21 +0530

    Implementation to get keyword data as per new flow
    2 new APIs added to get keyword size and data

--- framework/src/media/voice/HardwareKeywordDetector.h
+++ framework/src/media/voice/HardwareKeywordDetector.h
@@ -36,6 +36,8 @@ public:
 	void detectKeyword(void) override;
 	bool isKeywordDetectStarted(void) override;
 	void registerKeywordResultCallback(SpeechResultListener speechResultCallback) override;
+	bool getKeywordBufferSize(uint32_t *bufferSize) override;
+	bool getKeywordData(uint8_t *buffer) override;
 
 private:
 	/* AUDIO_DEVICE_PROCESS_TYPE_NONE card, device id */
