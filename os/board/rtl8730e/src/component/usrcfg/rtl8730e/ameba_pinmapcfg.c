1a978c5caead8e11023001d1b38cc58959ae45df
sin <sinzhenbei@realtek-sg.com>
2024-12-17 14:30:02 +0800

    os/board/rtl8730e: Modify PinMode AILite
    Change Notes:
    The current pinmapcfg is based on the AILite modules (consolidated AILite, AILite+ and AILite+W).
    Samsung requires modifications to the PuPd for both cases: Func PuPd and Sleep PuPd.
    Refer to the UM0602_RTL8730E_pinmux_v1.3(85339)_241212.xls.

--- os/board/rtl8730e/src/component/usrcfg/rtl8730e/ameba_pinmapcfg.c
+++ os/board/rtl8730e/src/component/usrcfg/rtl8730e/ameba_pinmapcfg.c
@@ -30,47 +30,47 @@ const PMAP_TypeDef pmap_func[] = {
 //Pin Name		Func PU/PD					Slp PU/PD
 	{_PA_0,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_1,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PA_2,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PA_3,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PA_4,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
+	{_PA_2,			GPIO_PuPd_UP,			GPIO_PuPd_UP},			 //
+	{_PA_3,			GPIO_PuPd_UP,			GPIO_PuPd_UP},			 //
+	{_PA_4,			GPIO_PuPd_UP,			GPIO_PuPd_UP},			 //
 	{_PA_5,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_6,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_7,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_8,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PA_9,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
-	{_PA_10,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
+	{_PA_9,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
+	{_PA_10,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PA_11,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
 	{_PA_12,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_13,			GPIO_PuPd_UP,			GPIO_PuPd_UP},			 //
-	{_PA_14,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
+	{_PA_14,			GPIO_PuPd_UP,			GPIO_PuPd_UP},			 //
 	{_PA_15,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PA_16,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 	// QA board need pull up
+	{_PA_16,			GPIO_PuPd_NOPULL,		GPIO_PuPd_NOPULL},			 	// QA board need pull up
 	{_PA_17,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_18,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
 	{_PA_19,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_20,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_21,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PA_22,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
-	{_PA_23,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
-	{_PA_24,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
-	{_PA_25,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
+	{_PA_22,			GPIO_PuPd_NOPULL,		GPIO_PuPd_NOPULL},			 //
+	{_PA_23,			GPIO_PuPd_DOWN,				GPIO_PuPd_DOWN},			 //
+	{_PA_24,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
+	{_PA_25,			GPIO_PuPd_UP,			GPIO_PuPd_UP},			 //
 	{_PA_26,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PA_27,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PA_28,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PA_29,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
-	{_PA_30,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
-	{_PA_31,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
+	{_PA_28,			GPIO_PuPd_UP,			GPIO_PuPd_UP},			 //
+	{_PA_29,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
+	{_PA_30,			GPIO_PuPd_DOWN,				GPIO_PuPd_DOWN},			 //
+	{_PA_31,			GPIO_PuPd_DOWN,				GPIO_PuPd_DOWN},			 //
 	{_PB_0,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
 	{_PB_1,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
 	{_PB_2,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
 	{_PB_3,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PB_4,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PB_5,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
+	{_PB_5,			GPIO_PuPd_UP,			GPIO_PuPd_UP},			 //
 	{_PB_6,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PB_7,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
 	{_PB_8,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
 	{_PB_9,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
-	{_PB_10,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
+	{_PB_10,			GPIO_PuPd_NOPULL,			GPIO_PuPd_NOPULL},			 //
 	{_PB_11,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PB_12,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
 	{_PB_13,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
@@ -79,19 +79,19 @@ const PMAP_TypeDef pmap_func[] = {
 	{_PB_16,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
 	{_PB_17,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PB_18,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PB_19,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
+	{_PB_19,			GPIO_PuPd_DOWN,				GPIO_PuPd_DOWN},			 //
 	{_PB_20,			GPIO_PuPd_DOWN,			GPIO_PuPd_DOWN},			 //
-	{_PB_21,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 // boot sel
+	{_PB_21,			GPIO_PuPd_NOPULL,			GPIO_PuPd_NOPULL},			 // boot sel
 	{_PB_22,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PB_23,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PB_24,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 // PB24 internal pull high(80k)
-	{_PB_25,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
+	{_PB_25,			GPIO_PuPd_DOWN,				GPIO_PuPd_DOWN},			 //
 	{_PB_26,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PB_27,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PB_28,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
-	{_PB_29,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
-	{_PB_30,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
-	{_PB_31,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //
+	{_PB_29,			GPIO_PuPd_NOPULL,			GPIO_PuPd_NOPULL},			 //
+	{_PB_30,			GPIO_PuPd_NOPULL,			GPIO_PuPd_NOPULL},			 //
+	{_PB_31,			GPIO_PuPd_DOWN,				GPIO_PuPd_DOWN},			 //
 	{_PC_0,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PC_1,			GPIO_PuPd_UP,				GPIO_PuPd_UP},			 //
 	{_PC_2,			GPIO_PuPd_UP,				GPIO_PuPd_DOWN},			 //flash clk pull down for PG or 50uA
