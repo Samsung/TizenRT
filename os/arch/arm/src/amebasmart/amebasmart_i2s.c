08963b37f81f2f64ad21d7dfc4696f23b48137bb
lhenry-realtek <176550561+lhenry-realtek@users.noreply.github.com>
2024-12-23 17:22:28 +0800

    os/board/rtl8730e: working I2S TDM implementation with DMA
    This commit completes the DMA portion of the I2S implementation and allows for receive of data based on AIS25BA sensor TDM signal specification
    
    Please note that if TDM board v1 (Dec 2024) is used, rework is required!
    
    To test, build with AIS25BA sensor example and use "sensor" command in TASH

--- os/arch/arm/src/amebasmart/amebasmart_i2s.c
+++ os/arch/arm/src/amebasmart/amebasmart_i2s.c
@@ -191,11 +191,10 @@ struct amebasmart_i2s_s {
 
 #if defined(I2S_HAVE_RX) && (0 < I2S_HAVE_RX)
 	uint8_t i2s_rx_buf[(I2S_DMA_PAGE_NUM + 1) * I2S_DMA_PAGE_SIZE]__attribute__((aligned(64))); /* Allocate buffer for use in RX, must I2S_DMA_PAGE_NUM+1 for zero buffer */
+	uint8_t i2s_rx_buf_ext[(I2S_DMA_PAGE_NUM + 1) * I2S_DMA_PAGE_SIZE]__attribute__((aligned(64))); /* Allocate buffer for use in RX, must I2S_DMA_PAGE_NUM+1 for zero buffer */
 	//uint8_t* i2s_rx_buf;
 	i2s_irq_handler rx_isr_handler;
 #if defined(I2S_HAVE_TDM) && (0 < I2S_HAVE_TDM)
-	//uint8_t* i2s_rx_buf_ext;
-	uint8_t i2s_rx_buf_ext[(I2S_DMA_PAGE_NUM + 1) * I2S_DMA_PAGE_SIZE]__attribute__((aligned(64))); /* Allocate buffer for use in RX, must I2S_DMA_PAGE_NUM+1 for zero buffer */
 	i2s_irq_handler rx_isr_handler_ext;
 #endif
 #endif
@@ -209,12 +208,14 @@ struct amebasmart_i2s_s {
 	uint8_t txenab : 1; /* True: TX transfers enabled */
 #if defined(I2S_HAVE_TDM) && (0 < I2S_HAVE_TDM)
 	uint8_t tdmenab : 1; /* True: TDM Enabled (this will disable MULTIIO)*/
+	
+	struct ap_buffer_s *apb_rx; /* Pointer to application RX audio buffer for DMA to populate */
 #endif
 
 	int sample_rate;			/*!< I2S sample rate */
 	int channel_num;			/*!< Number of channels */
 	int bits_per_sample;		/*!< Bits per sample */
-	struct ap_buffer_s *apb_tx; /* Pointer to application TX audio buffer to track current TX byte count */
+	struct ap_buffer_s *apb_tx;
 
 #if defined(I2S_HAVE_RX) && (0 < I2S_HAVE_RX)
 	struct amebasmart_transport_s rx; /* RX transport state */
@@ -265,11 +266,11 @@ static const struct amebasmart_i2s_config_s amebasmart_i2s3_config = {
 #if defined(I2S_HAVE_TDM) && (0 < I2S_HAVE_TDM)
 /* currently support I2S2 */
 static const struct amebasmart_i2s_config_s amebasmart_i2s2_tdm_config = {
-	.i2s_mclk_pin = PB_22,	// PU
-	.i2s_sclk_pin = PB_21,	// PU
-	.i2s_ws_pin = PA_16,	// PU
-	.i2s_sd_tx_pin = PB_10,	// PU
-	.i2s_sd_rx_pin = PB_29,	// PU (REWORK)
+	.i2s_mclk_pin = PB_22, // PU
+	.i2s_sclk_pin = PB_21, // PU
+	.i2s_ws_pin = PA_16,  // PU
+	.i2s_sd_tx_pin = PB_10, // PU
+	.i2s_sd_rx_pin = PB_29, // PU (REWORK)
 
 	.i2s_idx = I2S_NUM_2,
 	.rxenab = 1,
@@ -345,6 +346,9 @@ static int i2s_receive(struct i2s_dev_s *dev, struct ap_buffer_s *apb, i2s_callb
 static int i2s_send(struct i2s_dev_s *dev, struct ap_buffer_s *apb, i2s_callback_t callback, void *arg, uint32_t timeout);
 static int i2s_err_cb_register(struct i2s_dev_s *dev, i2s_err_cb_t cb, void *arg);
 
+/* tdm methods */
+static int i2s_tdm_receive(struct i2s_dev_s *dev, struct ap_buffer_s *apb, i2s_callback_t callback, void *arg, uint32_t timeout);
+
 /****************************************************************************
  * External functions
  ****************************************************************************/
@@ -384,7 +388,7 @@ static const struct i2s_ops_s g_i2stdm_ops = {
 
 	.i2s_rxsamplerate = i2s_samplerate,
 	.i2s_rxdatawidth = i2s_rxdatawidth,
-	.i2s_receive = i2s_receive,
+	.i2s_receive = i2s_tdm_receive,
 
 	/* Transmitter methods */
 
@@ -859,9 +863,22 @@ static int i2s_rxdma_prep(struct amebasmart_i2s_s *priv, struct amebasmart_buffe
 	apb->nbytes = 0;
 	apb->curbyte = 0;
 
-	priv->i2s_rx_buf = apb->samp;
-	i2s_set_dma_buffer(priv->i2s_object, NULL, (char *)priv->i2s_rx_buf, I2S_DMA_PAGE_NUM, I2S_DMA_PAGE_SIZE);
+	//priv->i2s_rx_buf = apb->samp;
+#if defined(I2S_HAVE_TDM) && (0 < I2S_HAVE_TDM)
+	if (priv->tdmenab) {
+		i2sinfo("tdm: set dma buffer! rxbuf: %p\n", priv->i2s_rx_buf);
 
+		i2s_tdm_set_dma_buffer(priv->i2s_object, NULL, (char *)priv->i2s_rx_buf, I2S_DMA_PAGE_NUM, I2S_DMA_PAGE_SIZE, GDMA_INT, apb->nmaxbytes);
+		if (priv->i2s_object->fifo_num >= SP_RX_FIFO8) {
+			i2sinfo("tdm: set dma buffer for EXT! rxbuf1: %p\n", priv->i2s_rx_buf);
+			i2s_tdm_set_dma_buffer(priv->i2s_object, NULL, (char *)priv->i2s_rx_buf_ext, I2S_DMA_PAGE_NUM, I2S_DMA_PAGE_SIZE, GDMA_EXT, apb->nmaxbytes);
+		}
+	} else
+#else
+	{
+		i2s_set_dma_buffer(priv->i2s_object, NULL, (char *)priv->i2s_rx_buf, I2S_DMA_PAGE_NUM, I2S_DMA_PAGE_SIZE);
+	}
+#endif
 	return 0;
 }
 
@@ -891,8 +908,12 @@ static int i2s_rx_start(struct amebasmart_i2s_s *priv)
 
 		/* Add the container to the list of active DMAs */
 		sq_addlast((sq_entry_t *)bfcontainer, &priv->rx.act);
-
-		i2s_recv_page(priv->i2s_object);
+#if defined(I2S_HAVE_TDM) && (0 < I2S_HAVE_TDM)
+		if(priv->tdmenab == 0)
+#endif
+		{
+			i2s_recv_page(priv->i2s_object);
+		}
 	}
 	leave_critical_section(flags);
 
@@ -1009,7 +1030,8 @@ static void i2s_rx_schedule(struct amebasmart_i2s_s *priv, int result)
 		/* Schedule the TX DMA done processing to occur on the worker thread. */
 		ret = work_queue(HPWORK, &priv->rx.work, i2s_rx_worker, priv, 0);
 		if (ret != 0) {
-			i2serr("ERROR: Failed to queue RX work: %d\n", ret);
+			//i2serr("ERROR: Failed to queue RX work: %d\n", ret);
+			lldbg("ERROR: Failed to queue RX work: %d\n", ret);
 		}
 	}
 }
@@ -1098,7 +1120,6 @@ static int i2s_receive(struct i2s_dev_s *dev, struct ap_buffer_s *apb, i2s_callb
 	struct amebasmart_buffer_s *bfcontainer;
 	irqstate_t flags;
 	int ret;
-	printf("i2s_receive\n");
 	/* Has the RX channel been enabled? */
 	if (!priv->rxenab) {
 		i2serr("ERROR: I2S has no receiver\n");
@@ -1109,7 +1130,7 @@ static int i2s_receive(struct i2s_dev_s *dev, struct ap_buffer_s *apb, i2s_callb
 	DEBUGASSERT(bfcontainer);
 
 	i2s_exclsem_take(priv);
-	printf("RX Exclusive Enter\n");
+	i2sinfo("RX Exclusive Enter\n");
 
 	/* Add a reference to the audio buffer */
 	apb_reference(apb);
@@ -1127,12 +1148,77 @@ static int i2s_receive(struct i2s_dev_s *dev, struct ap_buffer_s *apb, i2s_callb
 	flags = enter_critical_section();
 	sq_addlast((sq_entry_t *)bfcontainer, &priv->rx.pend);
 	leave_critical_section(flags);
-	printf("i2s_rx_start\n");
 	/* Start transfer */
 	ret = i2s_rx_start(priv);
 
 	i2s_exclsem_give(priv);
-	printf("RX Exclusive Exit\n");
+	i2sinfo("RX Exclusive Exit\n");
+
+	return OK;
+
+#else
+	i2serr("ERROR: I2S has no receiver\n");
+	UNUSED(priv);
+	return -ENOSYS;
+#endif
+}
+
+static int i2s_tdm_receive(struct i2s_dev_s *dev, struct ap_buffer_s *apb, i2s_callback_t callback, void *arg, uint32_t timeout)
+{
+	struct amebasmart_i2s_s *priv = (struct amebasmart_i2s_s *)dev;
+
+	i2sinfo("[TDM RX] apb=%p nmaxbytes=%d samp=%p arg=%p timeout=%d\n", apb, apb->nmaxbytes, apb->samp, arg, timeout);
+
+	/* apb samp is the buffer user use to receive, nmaxbyte should be less than I2S_DMA_PAGE_NUM*I2S_DMA_PAGE_SIZE */
+	i2s_init_buffer(apb->samp, apb->nmaxbytes);
+
+#if defined(I2S_HAVE_RX) && (0 < I2S_HAVE_RX)
+	struct amebasmart_buffer_s *bfcontainer;
+	irqstate_t flags;
+	int ret;
+	/* Has the RX channel been enabled? */
+	if (!priv->rxenab) {
+		i2serr("ERROR: I2S has no receiver\n");
+		return -EAGAIN;
+	}
+
+	/* obtain a buffer container */
+	bfcontainer = i2s_buf_rx_allocate(priv);
+	DEBUGASSERT(bfcontainer);
+
+	i2s_exclsem_take(priv);
+	i2sinfo("RX Exclusive Enter\n");
+
+	/* Add a reference to the audio buffer */
+	apb_reference(apb);
+
+	/* Reference the application buffer for DMA operation to copy data into */
+	priv->apb_rx = apb;
+
+	/* Initialize the buffer container structure */
+	bfcontainer->callback = (void *)callback;
+	bfcontainer->timeout = timeout;
+	bfcontainer->arg = arg;
+	bfcontainer->apb = apb;
+	bfcontainer->result = -EBUSY;
+
+	/* Prepare DMA microcode (CHANGE THIS! THIS SHOULD SETUP DMA BUFFERS BEFORE CLOCK STARTS) */
+	i2s_rxdma_prep(priv, bfcontainer);
+
+	flags = enter_critical_section();
+	sq_addlast((sq_entry_t *)bfcontainer, &priv->rx.pend);
+	leave_critical_section(flags);
+	/* Start transfer */
+	ret = i2s_rx_start(priv);
+
+	/* resume the clockgen if master, GDMA-SPORT ISR will trigger the callback to pass memory to app layer */
+	if (priv->i2s_object->role == MASTER) {
+		//i2s_resume(priv, priv->i2s_object->direction);
+		ameba_i2s_tdm_resume(priv->i2s_object);
+	}
+
+	i2s_exclsem_give(priv);
+	i2sinfo("RX Exclusive Exit\n");
 
 	return OK;
 
@@ -1144,23 +1230,97 @@ static int i2s_receive(struct i2s_dev_s *dev, struct ap_buffer_s *apb, i2s_callb
 }
 
 /*!
- * @brief Tx interrupt handler.
+ * @brief Rx interrupt handler.
  *
  * @param base pointer.
  * @param handle Pointer to the sai_handle_t structure.
  */
+extern volatile uint8_t DMA_Done, DMA_Done_1;
 void i2s_transfer_rx_handleirq(void *data, char *pbuf)
 {
-	/* TODO: there is currently crash here as data/pbuf is NULL! */
-	lldbg("handle irq %p %p\n", data, pbuf);
 	struct amebasmart_i2s_s *priv = (struct amebasmart_i2s_s *)data;
 	i2s_t *obj = priv->i2s_object;
+
+#if defined(I2S_HAVE_TDM) && (0 < I2S_HAVE_TDM)
+	if (priv->tdmenab) {
+		/* this callback will be called twice if 2 DMA channels are used, only allow processing when both completion flags are true */
+		if (obj->fifo_num >= SP_RX_FIFO8) {
+			if (DMA_Done && DMA_Done_1) {
+				lldbg("rx complete 8CH! stopping clockgen! APB: %p\n", priv->apb_rx);
+
+				/* stop clockgen */
+				ameba_i2s_tdm_pause(obj);
+
+				/* INTERNAL channel will have SLOT 1,2 followed by 5,6 */
+				/* EXTERNAL channel will have SLOT 3,4 followed by 7,8 */
+				u16 word_size;
+				switch(priv->i2s_object->channel_length) {
+					case SP_RXCL_16:
+						word_size = sizeof(u16);
+						break;
+					default:
+						word_size = sizeof(u32);
+						break;
+				}
+
+				if (word_size == sizeof(u16)) {
+					u16 *dst_buf = (u16 *)priv->apb_rx->samp;
+					u32 *rx_int = (u32 *)priv->i2s_rx_buf;
+					u32 *rx_ext = (u32 *)priv->i2s_rx_buf_ext;
+
+					for (u32 i = 0, j = 0; i < priv->apb_rx->nmaxbytes; i += 8, j += 4) {
+						dst_buf[i + 0] = rx_int[j + 0] >> 16;		// slot 1
+						dst_buf[i + 1] = rx_int[j + 1] >> 16;		// slot 2
+						dst_buf[i + 2] = rx_ext[j + 0] >> 16;		// slot 3
+						dst_buf[i + 3] = rx_ext[j + 1] >> 16;		// slot 4
+
+						dst_buf[i + 4] = rx_int[j + 0] & 0xFFFF;	// slot 5
+						dst_buf[i + 5] = rx_int[j + 1] & 0xFFFF;	// slot 6
+						dst_buf[i + 6] = rx_ext[j + 0] & 0xFFFF;	// slot 7
+						dst_buf[i + 7] = rx_ext[j + 1] & 0xFFFF;	// slot 8
+					}
+				} else {
+					u32 *dst_buf = (u32 *)priv->apb_rx->samp;
+					u32 *rx_int = (u32 *)priv->i2s_rx_buf;
+					u32 *rx_ext = (u32 *)priv->i2s_rx_buf_ext;
+
+					/* TODO: Verify correctness of this */
+					for (u32 i = 0, j = 0; i < priv->apb_rx->nmaxbytes; i += 8, j += 4) {
+						dst_buf[i + 0] = rx_int[j + 0];	// slot 1
+						dst_buf[i + 1] = rx_int[j + 1];	// slot 2
+						dst_buf[i + 2] = rx_ext[j + 0];	// slot 3
+						dst_buf[i + 3] = rx_ext[j + 1];	// slot 4
+
+						dst_buf[i + 4] = rx_int[j + 2];	// slot 5
+						dst_buf[i + 5] = rx_int[j + 3];	// slot 6
+						dst_buf[i + 6] = rx_ext[j + 2];	// slot 7
+						dst_buf[i + 7] = rx_ext[j + 3];	// slot 8
+					}
+				}
+
+				/* call upper layer callback */
+				i2s_rxdma_callback(priv, OK);
+			}
+		} else {
+			if (DMA_Done) {
+				lldbg("rx complete! stopping clockgen\n");
+				/* stop clockgen */
+				ameba_i2s_tdm_pause(obj);
+
+				/* copy DMA contents into buffer container ? */
+				// TODO: for single channel DMA populate the buffer
+
+				/* call upper layer callback */
+				i2s_rxdma_callback(priv, OK);
+			}
+		}
+	}
+#else
+
 	/* submit a new page for receive */
 	i2s_recv_page(obj);
-
-	lldbg("rxpage\n");
-
 	i2s_rxdma_callback(priv, OK);
+#endif
 }
 
 #if defined(I2S_HAVE_RX) && (0 < I2S_HAVE_RX)
@@ -1872,7 +2032,7 @@ static void i2s_getdefaultconfig(struct amebasmart_i2s_s *priv)
 		/* word and channel length */	
 		priv->bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
 		priv->i2s_object->word_length = WL_16b; // SP_RXWL_16;
-		priv->i2s_object->channel_length = SP_CL_16;
+		priv->i2s_object->channel_length = SP_RXCL_16;
 
 		/* FIFO size */
 		priv->i2s_object->fifo_num = SP_RX_FIFO8;	// for 8ch tdm
@@ -2096,12 +2256,6 @@ struct i2s_dev_s *amebasmart_i2s_tdm_initialize(uint16_t port, bool is_reinit)
 #if defined(I2S_HAVE_TX) && (0 < I2S_HAVE_TX)
 	i2s_buf_tx_initialize(priv);
 #endif
-	printf("tdmenab: %d \n", priv->config->tdmenab);
-	printf("ChLen: %d \n", priv->i2s_object->channel_length);
-	printf("WLen: %d \n", priv->i2s_object->word_length);
-	printf("ChNum: %d \n", priv->i2s_object->channel_num);
-	printf("SR: %d \n", priv->i2s_object->sampling_rate);
-	printf("FIFOnum: %d \n", priv->i2s_object->fifo_num);
 
 	//Init_Params.chn_cnt = (sp_obj.tdmmode + 1) * 2;
 	/* I2s object initialization */
@@ -2109,13 +2263,8 @@ struct i2s_dev_s *amebasmart_i2s_tdm_initialize(uint16_t port, bool is_reinit)
 
 	/* Initialize buffering */
 #if defined(I2S_HAVE_TX) && (0 < I2S_HAVE_TX)
-	i2s_set_dma_buffer(priv->i2s_object, (char *)priv->i2s_tx_buf, NULL, I2S_DMA_PAGE_NUM, I2S_DMA_PAGE_SIZE); /* Allocate DMA Buffer for TX */
+	i2s_tdm_set_dma_buffer(priv->i2s_object, (char *)priv->i2s_tx_buf, NULL, I2S_DMA_PAGE_NUM, I2S_DMA_PAGE_SIZE, GDMA_INT); /* Allocate DMA Buffer for TX */
 #endif
-
-#if defined(I2S_HAVE_RX) && (0 < I2S_HAVE_RX)
-	i2s_set_dma_buffer(priv->i2s_object, NULL, (char *)priv->i2s_rx_buf, I2S_DMA_PAGE_NUM, I2S_DMA_PAGE_SIZE); /* Allocate DMA Buffer for TX */
-#endif
-
 
 	/* configures IRQ */
 #if defined(I2S_HAVE_RX) && (0 < I2S_HAVE_RX)
