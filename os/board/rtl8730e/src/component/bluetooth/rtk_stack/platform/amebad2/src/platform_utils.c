297dcef360b6e9632b2fb9a334c22c9b994b0c05
yeetee179 <yeetee179@gmail.com>
2025-01-17 16:02:37 +0800

    os/board/rtl8730e: added BLE secure parameter setting, passkey display callback and passkey confirmation API - ble_manager_set_secure_param API is added to set BLE secure parameter - ble_server_passkey_display_cb is added inform app layer passkey - ble_manager_passkey_confirm is added to for passkey confirmation

--- os/board/rtl8730e/src/component/bluetooth/rtk_stack/platform/amebad2/src/platform_utils.c
+++ os/board/rtl8730e/src/component/bluetooth/rtk_stack/platform/amebad2/src/platform_utils.c
@@ -2,7 +2,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+extern int TRNG_get_random_bytes(void *dst, uint32_t size);
 uint32_t platform_random(uint32_t max)
 {
-	return rand() % max;
+	uint32_t val;
+	TRNG_get_random_bytes(&val, sizeof(uint32_t));
+	return (val % max); 
 }
