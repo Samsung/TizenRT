bf996bcea35dbde590d141ef0c715a089e6f5b87
shkwak <shkwak@imagis.co.kr>
2025-03-07 15:32:35 +0900

    driver/input: ist415 version command bugfix
    - Delete the return value when calling the ist415_display_version function.
    
    Signed-off-by: sanghoon <shkwak@imagis.co.kr>

--- os/drivers/input/ist415.c
+++ os/drivers/input/ist415.c
@@ -449,7 +449,7 @@ static int ist415_cmd(struct touchscreen_s *upper, int argc, char **argv)
 	} else if(strncmp(argv[1], "jitter_test", 12) == 0) {
 		ret = ist415_jittertest(dev);
 	} else if(strncmp(argv[1], "version", 8) == 0) {
-		ret = ist415_display_version(dev);
+		ist415_display_version(dev);
 	} else {
 		ret = -EINVAL;
 	}
