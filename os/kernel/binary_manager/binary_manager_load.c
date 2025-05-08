58799a4d4241d67410608241d107f02b363e5418
Kishore S N <kishore.sn@samsung.com>
2025-05-08 16:31:36 +0530

    os/kernel/semaphore: Add check for semcount in mutex case
    When a semaphore is being used as a mutex in pthread, then
    we must check that semcount never goes above 1.
    
    Signed-off-by: Kishore S N <kishore.sn@samsung.com>

--- os/kernel/binary_manager/binary_manager_load.c
+++ os/kernel/binary_manager/binary_manager_load.c
@@ -709,6 +709,10 @@ void binary_manager_release_binary_sem(int bin_idx)
 					/* Increase semcount and release itself from holder */
 					sem->semcount++;
 
+					if ((sem->flags & FLAGS_SEM_MUTEX) != 0) {
+						DEBUGASSERT(sem->semcount < 2);
+					}
+
 					/* And after releasing the kernel sem, there can be a task which waits that sem. So unblock the waiting task. */
 					sem_unblock_task(sem, holder->htcb);
 				}
