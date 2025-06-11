6d297c87279fd7000be398a4eb73e0a5993aeba8
jwei5 <jwei5>
2025-06-11 18:40:22 +0800

    apps/examples: Update CSI example
    - Update CSI example for multi-STA
    - Clean up CSI example code, include header file to define BSP related structs for example to use

--- apps/examples/board_specific_demo/ameba_wifi_csi_demo/example_wifi_csi.c
+++ apps/examples/board_specific_demo/ameba_wifi_csi_demo/example_wifi_csi.c
@@ -30,11 +30,13 @@
 #include <tinyara/wifi/rtk/wifi_constants.h>
 #include <sched.h>
 #include <semaphore.h>
+#include <example_wifi_csi.h>
 
 #define IP_ADDR_INVALID "0.0.0.0"
 extern int wifi_csi_config(rtw_csi_action_parm_t *act_param);
 extern int wifi_csi_report(uint32_t buf_len, uint8_t *csi_buf, uint32_t *len);
 extern int wifi_is_running(unsigned char wlan_idx);
+extern int wifi_get_setting(unsigned char wlan_idx, struct _rtw_wifi_setting_t *psetting);
 static sem_t wc_ready_sema;
 
 static unsigned int csi_data_len = 2048;
@@ -52,6 +54,7 @@ int main(int argc, FAR char *argv[])
 int wificsi_main(int argc, char *argv[])
 #endif
 {
+#ifdef CONFIG_ARCH_CHIP_RTL8730E
 	printf("WIFI CSI\n");
 	rtw_csi_action_parm_t act_param = {0};
 	unsigned int len;
@@ -59,13 +62,23 @@ int wificsi_main(int argc, char *argv[])
 	unsigned long long *buff_tmp = NULL; /* to printf csi data*/
 	unsigned int csi_seq_num;
 	unsigned int timestamp;
+	struct _rtw_wifi_setting_t wlan_setting = {0};
+	unsigned char device_0[6] = {0};  /* get mac_addr of AP via wifi_get_setting */
+	/* To test multi-STA CSI, uncomment and set the mac addr of the other devices */
+	// unsigned char device_1[6] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};  /* need modify to mac address of associated AP when sta mode */
+	// unsigned char device_2[6] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};  /* need modify to mac address of associated AP when sta mode */
 	act_param.group_num = 0;
-	act_param.mode = 0;  /* 0: rx normal 2:rx respon */
+	act_param.mode = 2;  /* 0: rx normal 2:rx respon */
 	act_param.accuracy = 0;
 	act_param.trig_period = 200;  /* ms */
-	act_param.data_rate = 0x80;  /* ofdm 6 mpbs*/
-	act_param.ch_opt = 1; //0: legacy 1: non-legacy
-//	act_param.mac_addr = {0x00, 0xe0, 0x4c, 0x81, 0x92, 0xbb};
+	act_param.data_rate = 0xc;  /* ofdm 6 mpbs*/
+	act_param.ch_opt = 0; //0: legacy 1: non-legacy
+	act_param.trig_frame_mgnt = 0;   /* no need for rx resp mode, default 0*/
+	act_param.trig_frame_ctrl = 0;   /* no need for rx resp mode, default 0*/
+	act_param.trig_frame_data = 0;   /* no need for rx resp mode, default 0*/
+	/* If rx normal mode (mode = 0) is used, set the packet type that will trigger CSI */
+	// act_param.trig_frame_data = CSI_TRIG_QOS_DATA 
+
 	char ipv4_address[4];
 	char ipv4_buf[16];
 	while (1) {
@@ -73,11 +86,15 @@ int wificsi_main(int argc, char *argv[])
 			snprintf(ipv4_buf, 16, "%d.%d.%d.%d", ipv4_address[0], ipv4_address[1], ipv4_address[2], ipv4_address[3]);
 		}
 		if (wifi_is_running(WLAN0_IDX) && ((wifi_get_join_status()) && (strncmp(ipv4_buf, IP_ADDR_INVALID, sizeof(ipv4_buf))))) {
+			/* get mac_addr of connected ap */
+			wifi_get_setting(WLAN0_IDX, &wlan_setting);
+			memcpy(device_0, wlan_setting.bssid, 6);
 			sleep(2); /* 2s */
 			break;
 		}
 		sleep(2); /* 2s */
 	}
+	memcpy(act_param.mac_addr, device_0, 6);
 	/**
 	 * should use semaphore to wait wifi event happen
 	 * the following example shows that we wait for wifi csi ready
@@ -86,16 +103,31 @@ int wificsi_main(int argc, char *argv[])
 	sem_init(&wc_ready_sema, 0, 0);
 
 	/* register wifi event callback function */
-	wifi_reg_event_handler(21, example_wifi_csi_report_cb, NULL);
+	wifi_reg_event_handler(WIFI_CSI_DONE, example_wifi_csi_report_cb, NULL);
 
 	/* csi cfg and csi en */
 	act_param.act = 1;  /* csi cfg */
 	act_param.enable = 0;
 	wifi_csi_config(&act_param);
 
+	/* Enable CSI for device_0 */
+	act_param.trig_flag = 0;
 	act_param.act = 0;  /* csi en */
 	act_param.enable = 1;
 	wifi_csi_config(&act_param);
+
+	/* Uncomment the following to test Multi-STA CSI */
+	/* Enable CSI for device_1 */
+	// act_param.trig_flag = 1;
+	// memcpy(act_param.mac_addr, device_1, 6);
+	// wifi_csi_config(&act_param);
+	
+	/* Uncomment the following to test Multi-STA CSI */
+	/* Enable CSI for device_2 */
+	// act_param.trig_flag = 2;
+	// memcpy(act_param.mac_addr, device_2, 6);
+	// wifi_csi_config(&act_param);
+
 	csi_buf = (unsigned char *)malloc(csi_data_len);
 	if (csi_buf) {
 		while (1) {
@@ -125,10 +157,14 @@ int wificsi_main(int argc, char *argv[])
 
 
 	/* unregister wifi event callback function */
-	wifi_unreg_event_handler(21, example_wifi_csi_report_cb);
+	wifi_unreg_event_handler(WIFI_CSI_DONE, example_wifi_csi_report_cb);
 
 	sem_destroy(&wc_ready_sema);
 
 	return 0;
+#else
+	printf("\n wificsi example only supported on RTL8730E!\n");
+	return 0;
+#endif //#ifdef CONFIG_ARCH_CHIP_RTL8730E
 
 }
