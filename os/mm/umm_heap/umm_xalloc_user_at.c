cac055e673d2fa8d9d9962d7b8b84062dfec0aee
Aniket <a.nagpure@partner.samsung.com>
2025-06-24 10:59:01 +0530

    os/mm : Removed declarations of ARCH_GET_RET_ADDRESS and removed unnecessary files.
    1.Replacing the ARCH_GET_RET_ADDRESS api with builtin_return_address api
    2.Previous has limitation to get proper address. (must be used at start of a function), new one does not have.
    3.New one only has a limitation (working on specific toolchain), but we support the docker so that no issue on this.

--- os/mm/umm_heap/umm_xalloc_user_at.c
+++ os/mm/umm_heap/umm_xalloc_user_at.c
@@ -43,8 +43,7 @@ void *calloc_user_at(struct mm_heap_s *heap, size_t n, size_t elem_size)
 		return NULL;
 	}
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
-	mmaddress_t caller_retaddr;
-	ARCH_GET_RET_ADDRESS(caller_retaddr)	
+	mmaddress_t caller_retaddr = (mmaddress_t)__builtin_return_address(0);
 	return mm_calloc(heap, n, elem_size, caller_retaddr);
 #else
 	return mm_calloc(heap, n, elem_size);
@@ -66,8 +65,7 @@ void *memalign_user_at(struct mm_heap_s *heap, size_t alignment, size_t size)
 		return NULL;
 	}
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
-	mmaddress_t caller_retaddr;
-	ARCH_GET_RET_ADDRESS(caller_retaddr)	
+	mmaddress_t caller_retaddr = (mmaddress_t)__builtin_return_address(0);
 	return mm_memalign(heap, alignment, size, caller_retaddr);
 #else
 	return mm_memalign(heap, alignment, size);
@@ -96,8 +94,7 @@ void *malloc_user_at(struct mm_heap_s *heap, size_t size)
 		return NULL;
 	}
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
-	mmaddress_t caller_retaddr = 0;
-	ARCH_GET_RET_ADDRESS(caller_retaddr)	
+	mmaddress_t caller_retaddr = (mmaddress_t)__builtin_return_address(0);
 	return mm_malloc(heap, size, caller_retaddr);
 #else
 	return mm_malloc(heap, size);
@@ -116,13 +113,13 @@ void *malloc_user_at(struct mm_heap_s *heap, size_t size)
 void *realloc_user_at(struct mm_heap_s *heap, void *oldmem, size_t newsize)
 {
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
-	mmaddress_t caller_retaddr = 0;
-	ARCH_GET_RET_ADDRESS(caller_retaddr)
+	mmaddress_t caller_retaddr = (mmaddress_t)__builtin_return_address(0);
 #endif
 	if (newsize == 0) {
 		free_user_at(heap, oldmem);
 		return NULL;
 	}
+
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
 	return mm_realloc(heap, oldmem, newsize, caller_retaddr);
 #else
@@ -145,10 +142,10 @@ void *zalloc_user_at(struct mm_heap_s *heap, size_t size)
 		return NULL;
 	}
 #ifdef CONFIG_DEBUG_MM_HEAPINFO
-	mmaddress_t caller_retaddr = 0;
-	ARCH_GET_RET_ADDRESS(caller_retaddr)	
+	mmaddress_t caller_retaddr = (mmaddress_t)__builtin_return_address(0);
 	return mm_zalloc(heap, size, caller_retaddr);
 #else
 	return mm_zalloc(heap, size);
 #endif
+
 }
