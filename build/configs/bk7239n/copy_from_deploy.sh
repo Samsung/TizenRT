e469c83f4cb98503c449aaf86ea58c563dae1877
Poly-J <jun.pan@aclsemi.com>
2025-10-29 16:11:03 +0800

    build/config: Add BK7239N project
    This directory contains the project's config file, linker scripts, and some other packaging scripts.
    and there are currently two projects:
    1.the hello project uses the FLAT compilation mode,
    2.the loadable_app project uses the PROTECT compilation mode.
    
    Signed-off-by: Poly-J <jun.pan@aclsemi.com>

---
+++ build/configs/bk7239n/copy_from_deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+THIS_PATH=`test -d ${0%/*} && cd ${0%/*}; pwd`
+TOP_PATH=${THIS_PATH}/../../..
+CONFIG=${TOP_PATH}/os/.config
+BUILDDIR=${TOP_PATH}/build
+BINDIR=${BUILDDIR}/output/bin
+TOOL_PATH=${THIS_PATH}/../../tools/armino
+
+function copy_from_deploy()
+{
+    echo "Copying from deploy..."
+	local deploy_output_dir="${BUILDDIR}/tools/armino/beken_utils/output_dir"
+	cp -rf ${deploy_output_dir}/bootloader.bin ${BINDIR}/bootloader.bin
+	cp -rf ${deploy_output_dir}/kernel.bin ${BINDIR}/kernel.bin
+	cp -rf ${deploy_output_dir}/all-app.bin ${BINDIR}/all-app.bin
+	cp -rf ${deploy_output_dir}/app1 ${BINDIR}/app1
+	cp -rf ${deploy_output_dir}/app2 ${BINDIR}/app2
+    cp -rf ${deploy_output_dir}/*.trpk ${BINDIR}/
+}
+
+copy_from_deploy
