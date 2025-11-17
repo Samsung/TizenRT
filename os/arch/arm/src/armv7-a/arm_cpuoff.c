e22c81ea2b368f35f217736b4af93c86047546d7
Eugene Ang <eugeneang654871@gmail.com>
2025-11-17 09:38:43 +0800

    os/arch/arm/src/amebasmart: Revert SMP systick ISR when PM handling
    This commit reverts 6e46e19. Core 0 will be the only core handling g_system_timer throughout the lifetime of the application.
    Hence the following code is redundant and has been removed.

--- os/arch/arm/src/armv7-a/arm_cpuoff.c
+++ os/arch/arm/src/armv7-a/arm_cpuoff.c
@@ -258,9 +258,6 @@ int up_cpu_off(int cpu)
 		return -EBUSY;
 	}
 
-	/* Ensure that hotplug target CPU's systick PPI stops firing to prevent deadlock condition */
-	up_systimer_pause(cpu);
-
 	/* Lock spinlock to synchronize with the target CPU */
 	spin_lock(&g_cpu_hotpluged[cpu]);
 
