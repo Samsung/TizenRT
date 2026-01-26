12530b4c0de0fed16807bd7c993ee2ade07b9609
ziliguo <zili.guo@aclsemi.com>
2026-01-26 16:52:23 +0800

    build/configs/bk7239n: add common partition, xip and loadable_all project
    - adapt common partition for compile and package script, with CONFIG_SUPPORT_COMMON_BINARY
    - update flash partition
    - add add xip project, loadable_all project

--- build/configs/bk7239n/copy_from_deploy.sh
+++ build/configs/bk7239n/copy_from_deploy.sh
@@ -6,17 +6,25 @@ CONFIG=${TOP_PATH}/os/.config
 BUILDDIR=${TOP_PATH}/build
 BINDIR=${BUILDDIR}/output/bin
 TOOL_PATH=${THIS_PATH}/../../tools/armino
+source ${CONFIG}
 
 function copy_from_deploy()
 {
     echo "Copying from deploy..."
 	local deploy_output_dir="${BUILDDIR}/tools/armino/beken_utils/output_dir"
 	cp -rf ${deploy_output_dir}/bootloader.bin ${BINDIR}/bootloader.bin
-	cp -rf ${deploy_output_dir}/bootloader_ota.bin ${BINDIR}/bootloader_ota.bin
+	if [ "$CONFIG_BL2_UPDATE_WITH_PC" = "y" ]; then
+		cp -rf ${deploy_output_dir}/bootloader_ota.bin ${BINDIR}/bootloader_ota.bin
+	fi
 	cp -rf ${deploy_output_dir}/kernel.bin ${BINDIR}/kernel.bin
-	cp -rf ${deploy_output_dir}/all-app.bin ${BINDIR}/all-app.bin
+    if [ "${CONFIG_BK7239N_MP}" = "y" ]; then
+	    cp -rf ${deploy_output_dir}/all-app.bin ${BINDIR}/all-app.bin
+    else
+        cp -rf ${deploy_output_dir}/all-app.bin ${BINDIR}/mpw_all-app.bin
+    fi
 	cp -rf ${deploy_output_dir}/app1 ${BINDIR}/app1
 	cp -rf ${deploy_output_dir}/app2 ${BINDIR}/app2
+	cp -rf ${deploy_output_dir}/common ${BINDIR}/common
     cp -rf ${deploy_output_dir}/*.trpk ${BINDIR}/
 }
 
