e817d44f8747db77d0502a69edfba0cb5206298b
Eunwoo Nam <eunwoo.nam@samsung.com>
2026-02-09 17:11:18 +0900

    os: replace dbg with lldbg in CPU on/off functions
    CPU on/off operations are typically performed inside enter_critical_section.
    Using dbg causes context switching which must be avoided in critical sections.
    Replace smpdbg/smpvdbg/slldbg with smplldbg/smpllvdbg to prevent
    unintended context switches during CPU power state transitions.
    
    Signed-off-by: Eunwoo Nam <eunwoo.nam@samsung.com>

--- os/arch/arm/src/armv7-a/arm_cpuoff.c
+++ os/arch/arm/src/armv7-a/arm_cpuoff.c
@@ -248,7 +248,7 @@ int up_cpu_off(int cpu)
 	struct tcb_s * tcb;
 	int ret = OK;
 
-	smpvdbg("Disabling CPU%d\n", cpu);
+	smpllvdbg("Disabling CPU%d\n", cpu);
 
 	DEBUGASSERT(cpu > 0 && cpu < CONFIG_SMP_NCPUS && cpu != this_cpu());
 
@@ -282,20 +282,20 @@ int up_cpu_off(int cpu)
 
 	/* Check if hotplug failed */
 	if (ret < 0) {
-		smpdbg("Secondary core hotplug failed CPU%d\n", cpu);
+		smplldbg("Secondary core hotplug failed CPU%d\n", cpu);
 		return ret;
 	}
 
-	smpvdbg("Secondary core hotplug complete CPU%d\n", cpu);
+	smpllvdbg("Secondary core hotplug complete CPU%d\n", cpu);
 
 	/* Power down the core completely */
 	ret = up_cpu_down(cpu);
 	if (ret < 0) {
-		smpdbg("Failed to powerdown secondary core CPU%d\n", cpu);
+		smplldbg("Failed to powerdown secondary core CPU%d\n", cpu);
 		return ret;
 	}
 
-	smpvdbg("Secondary core powerdown complete CPU%d\n", cpu);
+	smpllvdbg("Secondary core powerdown complete CPU%d\n", cpu);
 
 	return ret;
 }
