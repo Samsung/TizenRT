ba3707aa5ef0fb50dab04a6b897488c7fbd3d070
jun.pan <jun.pan@aclsemi>
2026-03-26 13:56:25 +0800

    build/configs/bk7239n: Optimize deployment script

--- build/configs/bk7239n/bk7239n_post.sh
+++ build/configs/bk7239n/bk7239n_post.sh
@@ -8,10 +8,14 @@ BUILDDIR=${TOP_PATH}/build
 source ${CONFIG}
 
 if [ "${CONFIG_BINARY_SIGNING}" = "y" ]; then
-    python3 -B ${BUILDDIR}/tools/armino/beken_utils/scripts/copy_build_files.py ${TOPDIR} ${BUILDDIR}/output/bin ${BUILDDIR}/tools/armino/beken_utils/input_dir
-    export BK_DOCKER_MODE=1
-    ${TOP_PATH}/build/tools/armino/beken_utils/deployment_main.sh ${BUILDDIR}/tools/armino/beken_utils/input_dir ${BUILDDIR}/tools/armino/beken_utils/output_dir
-    ${THIS_PATH}/copy_from_deploy.sh
+    if [ ! -f "${BUILDDIR}/tools/armino/beken_utils/deployment_main.sh" ]; then
+        python3 -B ${BUILDDIR}/configs/bk7239n/copy_build_files.py ${TOPDIR} ${BUILDDIR}/output/bin ${BUILDDIR}/output/bin/install_for_deployment
+    else
+        python3 -B ${BUILDDIR}/configs/bk7239n/copy_build_files.py ${TOPDIR} ${BUILDDIR}/output/bin ${BUILDDIR}/tools/armino/beken_utils/input_dir
+        export BK_DOCKER_MODE=1
+        ${TOP_PATH}/build/tools/armino/beken_utils/deployment_main.sh ${BUILDDIR}/tools/armino/beken_utils/input_dir ${BUILDDIR}/tools/armino/beken_utils/output_dir
+        ${THIS_PATH}/copy_from_deploy.sh
+    fi
 else
     python3 -B ${BUILDDIR}/tools/armino/bkfil_pack_tool/bkfil_bin_pack.py ${TOPDIR} ${BUILDDIR}/output/bin
 fi
