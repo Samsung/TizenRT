1834ecf587ee0dd42c507c895fdcabde5ac294fd
jaeyong1-lee <jaeyong1.lee@samsung.com>
2026-04-13 20:16:56 +0900

    os/*: Remove APP_BINARY_SEPARATION dependency from signing configs
    - Remove APP_BINARY_SEPARATION dependency from USER_SIGN_PREPEND_SIZE
    - Remove CONFIG_APP_BINARY_SEPARATION guard from up_verify_usersignature()
      in all architecture-specific signature files
    
    This allows BINARY_SIGNING to be used independently of APP_BINARY_SEPARATION,
    enabling resource binary signing without requiring application binary separation.
    
    Affected files:
    - os/board/*/Kconfig.signing: Updated USER_SIGN_PREPEND_SIZE dependency
    - os/arch/arm/src/*/signature.c: Removed CONFIG_APP_BINARY_SEPARATION guard
    - os/include/tinyara/signature.h: Removed CONFIG_APP_BINARY_SEPARATION guard
    
    Signed-off-by: jaeyong1-lee <jaeyong1.lee@samsung.com>

--- os/arch/arm/src/amebalite/amebalite_signature.c
+++ os/arch/arm/src/amebalite/amebalite_signature.c
@@ -28,9 +28,7 @@ int up_verify_kernelsignature(uint32_t address)
 	return SIGNATURE_VAILD;
 }
 
-#ifdef CONFIG_APP_BINARY_SEPARATION
 int up_verify_usersignature(uint32_t address)
 {
 	return SIGNATURE_VAILD;
 }
-#endif
