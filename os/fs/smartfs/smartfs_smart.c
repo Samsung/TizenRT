a1374495a398b0f0b27dd8a9033f5da23affaca2
tjkwon <tj80.kwon@samsung.com>
2026-08-05 10:53:41 +0900

    fs/smartfs : Update sector buffer after read When smartfs read some sectors, sector buffer holds first sector info. Because of this, append happened after read, some sectors points same sector as a next sector To prevent this, update sector buffer for every read operation

--- os/fs/smartfs/smartfs_smart.c
+++ os/fs/smartfs/smartfs_smart.c
@@ -543,6 +543,18 @@ static ssize_t smartfs_read(FAR struct file *filep, char *buffer, size_t buflen)
 			goto errout_with_semaphore;
 		}
 
+#ifdef CONFIG_SMARTFS_USE_SECTOR_BUFFER
+		/* Sync data to prevent commit old data in sf->bflags.
+		 * Ex. Read->append->sync will commit wrong value of chain header.
+		 */
+		if (sf->bflags & SMARTFS_BFLAG_DIRTY) {
+			/* This must not be happened! */
+			fdbg("BUG!!! Flag is dirty!!\n", sf->currsector);
+		} else {
+			memcpy(sf->buffer, fs->fs_rwbuffer, fs->fs_llformat.availbytes);
+		}
+#endif
+
 		/* Point header to the read data to get used byte count */
 
 		header = (struct smartfs_chain_header_s *)fs->fs_rwbuffer;
